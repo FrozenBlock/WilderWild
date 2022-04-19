dcf73e3c92e8679ec955e58322f20b58565dc1aa
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-04-19 09:41:13 -0700

    Waterlogged Cattails!

---
+++ src/main/java/net/frozenblock/wilderwild/block/CattailBlock.java
@@ -0,0 +1,44 @@
+package net.frozenblock.wilderwild.block;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
+
+public class CattailBlock extends TallFlowerBlock implements Waterloggable {
+    public static final BooleanProperty WATERLOGGED;
+
+    public CattailBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState( this.getDefaultState().with(WATERLOGGED, false));
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(WATERLOGGED);
+    }
+
+    static {
+        WATERLOGGED = Properties.WATERLOGGED;
+    }
+}
