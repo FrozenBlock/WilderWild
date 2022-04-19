3b442144726f001e1cac28f5ff35c30a8d0f9ca4
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-04-19 10:59:27 -0700

    bruh

--- src/main/java/net/frozenblock/wilderwild/mixin/ExampleMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		WilderWild.LOGGER.info("This line is printed by an example mod mixin!");
+		WilderWild.LOGGER.info("bruh");
 	}
 }
