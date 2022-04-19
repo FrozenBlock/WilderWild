c1c54e83454560a0b2178432c3c25d20e2d3c482
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-04-19 18:31:26 -0400

    Pillars Have A Rare Chance To Rotate

--- src/main/java/net/frozenblock/wilderwild/block/SculkBoneBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/SculkBoneBlock.java
@@ -1,5 +1,6 @@
 package net.frozenblock.wilderwild.block;
 
+import net.frozenblock.wilderwild.noise.EasyNoiseSampler;
 import net.frozenblock.wilderwild.registry.NewProperties;
 import net.frozenblock.wilderwild.registry.RegisterBlocks;
 import net.minecraft.block.*;
@@ -17,7 +18,7 @@ public class SculkBoneBlock extends PillarBlock implements SculkSpreadable {
 
     public SculkBoneBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(HEIGHT_LEFT, 0).with(AXIS, Direction.Axis.Y));
+        this.setDefaultState(this.stateManager.getDefaultState().with(HEIGHT_LEFT, 4).with(AXIS, Direction.Axis.Y));
     }
 
     public static final IntProperty HEIGHT_LEFT = NewProperties.PILLAR_HEIGHT_LEFT;
@@ -29,19 +30,26 @@ public class SculkBoneBlock extends PillarBlock implements SculkSpreadable {
             BlockPos blockPos = cursor.getPos();
             boolean bl = blockPos.isWithinDistance(catalystPos, spreadManager.getMaxDistance());
             if (!bl) {
+                Direction direction = world.getBlockState(blockPos).get(AXIS).getType().random(EasyNoiseSampler.simpleRandom);
+                if (direction==Direction.DOWN) { direction=Direction.UP; }
                 int pillarHeight = world.getBlockState(blockPos).get(SculkBoneBlock.HEIGHT_LEFT);
-                BlockPos topPos = getTop(world, blockPos, pillarHeight);
+                BlockPos topPos = getTop(world, blockPos, pillarHeight, direction);
                 if (topPos != null) {
                     pillarHeight = world.getBlockState(topPos).get(SculkBoneBlock.HEIGHT_LEFT);
-                    if (world.getBlockState(topPos.up()).isAir() || world.getBlockState(topPos.up()).getBlock() == Blocks.SCULK_VEIN) {
-                        BlockState blockState = RegisterBlocks.SCULK_BONE.getDefaultState().with(SculkBoneBlock.HEIGHT_LEFT, pillarHeight - 1);
+                    direction = world.getBlockState(topPos).get(AXIS).getType().random(EasyNoiseSampler.simpleRandom);
+                    if (direction==Direction.DOWN) { direction=Direction.UP; }
+                    if (world.getBlockState(topPos.offset(direction)).isAir() || world.getBlockState(topPos.offset(direction)).getBlock() == Blocks.SCULK_VEIN) {
+                        BlockState blockState = RegisterBlocks.SCULK_BONE.getDefaultState().with(SculkBoneBlock.HEIGHT_LEFT, pillarHeight-1);
+                        if (EasyNoiseSampler.atomicRandom.nextInt(26)==0) {
+                            blockState = RegisterBlocks.SCULK_BONE.getDefaultState().with(SculkBoneBlock.HEIGHT_LEFT, pillarHeight-1).with(AXIS, Direction.Axis.pickRandomAxis(EasyNoiseSampler.simpleRandom));
+                        }
                         if (pillarHeight==1) {
                             blockState = RegisterBlocks.SCULK_ECHOER.getDefaultState();
                             if (random.nextInt(11) == 0) {
                                 blockState = Blocks.SCULK_CATALYST.getDefaultState();
                             }
                         }
-                        world.setBlockState(topPos.up(), blockState, 3);
+                        world.setBlockState(topPos.offset(direction), blockState, 3);
                         world.playSound(null, blockPos, blockState.getSoundGroup().getPlaceSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
                         return Math.max(0, i - 1);
                     }
@@ -50,20 +58,13 @@ public class SculkBoneBlock extends PillarBlock implements SculkSpreadable {
         } return i;
     }
 
-    public static BlockPos getTop(WorldAccess world, BlockPos pos, int max) {
+    public static BlockPos getTop(WorldAccess world, BlockPos pos, int max, Direction direction) {
         for (int i=0; i<max; i++) {
             Block block = world.getBlockState(pos).getBlock();
             if (block!=RegisterBlocks.SCULK_BONE) { return null; }
-            if (world.getBlockState(pos.up()).isAir() || world.getBlockState(pos.up()).getBlock() == Blocks.SCULK_VEIN) {return pos;}
-            pos=pos.up();
+            if (world.getBlockState(pos.offset(direction)).isAir() || world.getBlockState(pos.offset(direction)).getBlock() == Blocks.SCULK_VEIN) {return pos;}
+            pos=pos.offset(direction);
         } return null;
-    }
-
-    public static int getHeight(WorldAccess world, BlockPos pos) {
-        for (int i=0; i<13; i++) {
-            pos=pos.down();
-            if (world.getBlockState(pos).getBlock() == Blocks.SCULK) {return i;}
-        } return 99;
     }
 
     public int getDecay(int oldDecay) {
