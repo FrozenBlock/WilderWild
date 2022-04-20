198d32835252c2235c40eb22f69058bee16f2b89
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-04-19 21:26:15 -0400

    fix again ALSO GO TO A DEEP DARK

--- src/main/java/net/frozenblock/wilderwild/registry/NewProperties.java
+++ src/main/java/net/frozenblock/wilderwild/registry/NewProperties.java
@@ -13,6 +13,6 @@ public class NewProperties extends Properties {
 
     public static final IntProperty PILLAR_HEIGHT_LEFT = IntProperty.of("sculk_bone_pillar_height_left_to_grow_until_it_reaches_the_top", 0, 15);
     public static final BooleanProperty UPSIDE_DOWN = BooleanProperty.of("is_the_current_sculk_bone_block_pointing_downwards_quation_mark");
-    public static final IntProperty TOTAL_HEIGHT = IntProperty.of("sculk_bone_pillar_height_total", 0, 15);
+    public static final IntProperty TOTAL_HEIGHT = IntProperty.of("sculk_bone_pillar_height_total", 0, 16);
 
 }
