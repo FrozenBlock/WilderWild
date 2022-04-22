e4ce20161520db57c1b0061d1dfbc56ebeb119de
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-04-22 08:40:08 -0700

    OK I MADE ACCESSWIDENSER I THINK just, put the code in the SculkVeinGrowCheckerMixin

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/SculkVeinGrowCheckerMixin.java
@@ -0,0 +1,7 @@
+package net.frozenblock.wilderwild.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = "net.minecraft.block.SculkVeinBlock$SculkVeinGrowChecker")
+public class SculkVeinGrowCheckerMixin {
+}
