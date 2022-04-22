117cc4e42d2385b53115b34b1a1da404fd64efc1
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-04-22 08:52:55 -0700

    nvm

--- src/main/java/net/frozenblock/wilderwild/mixin/SculkVeinGrowCheckerMixin.java
+++
@@ -1,7 +0,0 @@
-package net.frozenblock.wilderwild.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(targets = "net.minecraft.block.SculkVeinBlock$SculkVeinGrowChecker")
-public class SculkVeinGrowCheckerMixin {
-}
