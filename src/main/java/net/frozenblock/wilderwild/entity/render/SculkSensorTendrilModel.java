8c91ad615ddfabc7682a0594a1afb62a1f925a24
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-04-24 15:50:39 -0500

    KILL ME

--- src/main/java/net/frozenblock/wilderwild/entity/render/SculkSensorTendrilModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/SculkSensorTendrilModel.java
@@ -21,10 +21,10 @@ public class SculkSensorTendrilModel<T extends SculkSensorTendrilEntity> extends
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild("northeast", ModelPartBuilder.create().uv(0,8).cuboid(-4.0F, -8.0F, 0.0F, 8.0F, 8.0F, 0.002F), ModelTransform.pivot(-5.0F,24.0F,-5.0F));
-        modelPartData.addChild("northwest", ModelPartBuilder.create().uv(0,8).cuboid(0.0F, -8.0F, -4.0F, 0.002F, 8.0F, 8.0F), ModelTransform.pivot(5.0F,24.0F,-5.0F));
-        modelPartData.addChild("southeast", ModelPartBuilder.create().uv(0,8).cuboid(0.0F, -8.0F, -4.0F, 0.002F, 8.0F, 8.0F), ModelTransform.pivot(-5.0F,24.0F,5.0F));
-        modelPartData.addChild("southwest", ModelPartBuilder.create().uv(0,8).cuboid(-4.0F, -8.0F, 0.0F, 8.0F, 8.0F, 0.002F), ModelTransform.pivot(5.0F,24.0F,5.0F));
+        modelPartData.addChild("northeast", ModelPartBuilder.create().uv(0,0).cuboid(-4.0F, -8.0F, 0.0F, 8.0F, 8.0F, 0.002F), ModelTransform.pivot(-5.0F,24.0F,-5.0F));
+        modelPartData.addChild("northwest", ModelPartBuilder.create().uv(0,0).cuboid(0.0F, -8.0F, -4.0F, 0.002F, 8.0F, 8.0F), ModelTransform.pivot(5.0F,24.0F,-5.0F));
+        modelPartData.addChild("southeast", ModelPartBuilder.create().uv(0,0).cuboid(0.0F, -8.0F, -4.0F, 0.002F, 8.0F, 8.0F), ModelTransform.pivot(-5.0F,24.0F,5.0F));
+        modelPartData.addChild("southwest", ModelPartBuilder.create().uv(0,0).cuboid(-4.0F, -8.0F, 0.0F, 8.0F, 8.0F, 0.002F), ModelTransform.pivot(5.0F,24.0F,5.0F));
         return TexturedModelData.of(modelData,64,64);
     }
     public void setAngles(SculkSensorTendrilEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
@@ -37,10 +37,10 @@ public class SculkSensorTendrilModel<T extends SculkSensorTendrilEntity> extends
     }
     private void setTendrilPitches(SculkSensorTendrilEntity tendril, float animationProgress, float tickDelta) {
         float f = tendril.getTendrilPitch(tickDelta) * (float)(Math.cos((double)animationProgress * 2.25D) * 3.141592653589793D * 0.10000000149011612D);
-        this.northeast.roll = f;
-        this.northwest.pitch = f;
-        this.southeast.pitch = f;
-        this.southwest.roll = f;
+        this.northeast.pitch = f;
+        this.northwest.roll = f;
+        this.southeast.roll = f;
+        this.southwest.pitch = f;
     }
 
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
