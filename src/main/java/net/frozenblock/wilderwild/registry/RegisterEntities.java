9938d54240644eb5ab1facefede91a8f135e4deb
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-04-24 23:56:40 -0500

    tendril entity is now 0 in size

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterEntities.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterEntities.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class RegisterEntities {
-    public static final EntityType<SculkSensorTendrilEntity> TENDRIL_ENTITY = Registry.register(Registry.ENTITY_TYPE, new Identifier(WilderWild.MOD_ID, "sculk_sensor_tendril"), FabricEntityTypeBuilder.create(SpawnGroup.MISC, SculkSensorTendrilEntity::new).dimensions(EntityDimensions.fixed(1.0f, 0.0f)).build());
+    public static final EntityType<SculkSensorTendrilEntity> TENDRIL_ENTITY = Registry.register(Registry.ENTITY_TYPE, new Identifier(WilderWild.MOD_ID, "sculk_sensor_tendril"), FabricEntityTypeBuilder.create(SpawnGroup.MISC, SculkSensorTendrilEntity::new).dimensions(EntityDimensions.fixed(0.0f, 0.0f)).build());
 
 
     public static void init() {
