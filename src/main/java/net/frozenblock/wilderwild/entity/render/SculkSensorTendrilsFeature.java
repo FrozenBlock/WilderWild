544434945335c0a76ef184fa86bb0824ab45035a
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-04-24 22:44:41 -0500

    nvm they dont react to light ._.

--- src/main/java/net/frozenblock/wilderwild/entity/render/SculkSensorTendrilsFeature.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/SculkSensorTendrilsFeature.java
@@ -10,6 +10,7 @@ import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.LightType;
 
 public class SculkSensorTendrilsFeature extends EyesFeatureRenderer<SculkSensorTendrilEntity, SculkSensorTendrilModel<SculkSensorTendrilEntity>> {
 
@@ -23,12 +24,15 @@ public class SculkSensorTendrilsFeature extends EyesFeatureRenderer<SculkSensorT
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, SculkSensorTendrilEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         //if (entity.shouldRender) { /* REPLACE WITH CLIENT SETTINGS CHECK */
             VertexConsumer vertexConsumer = vertexConsumers.getBuffer(this.TENDRILS);
-            this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, calculateColors(entity), calculateColors(entity), calculateColors(entity), 1.0f);
+            this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, calculateColors(entity), calculateColors(entity), calculateColors(entity), 0.0f);
         //}
     }
 
     private static float calculateColors(SculkSensorTendrilEntity tendrils) {
-        if (tendrils.activeTicksLeft>0) { return 1F; }
+        int a = tendrils.world.getLightLevel(LightType.BLOCK, tendrils.getBlockPos());
+        int b = tendrils.world.getLightLevel(LightType.SKY, tendrils.getBlockPos());
+        int o = Math.max(a, b);
+        if (tendrils.activeTicksLeft>0) { return o; }
         return 0F;
     }
 
