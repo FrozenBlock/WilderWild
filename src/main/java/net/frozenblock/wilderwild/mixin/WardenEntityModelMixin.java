56abb04a4d3ae8e7ea31617fbccfe56229e233b3
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-04-27 19:02:24 -0500

    tweaked warden tendrils again again

--- src/main/java/net/frozenblock/wilderwild/mixin/WardenEntityModelMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/WardenEntityModelMixin.java
@@ -21,9 +21,16 @@ public class WardenEntityModelMixin<T extends WardenEntity> {
     private void setTendrilPitches(T warden, float animationProgress, float tickDelta) {
         float f = warden.getTendrilPitch(tickDelta) * (float)(Math.cos((double)animationProgress * 2.25D) * 3.141592653589793D * 0.10000000149011612D);
         float g = warden.getTendrilPitch(tickDelta) * (float)(-Math.sin((double)animationProgress * 2.25D) * 3.141592653589793D * 0.12500000149011612D);
+
+    //hecc yeah we're using all axes for this one >:3 -merp
+
         this.leftTendril.pitch = f;
         this.rightTendril.pitch = f;
-        this.leftTendril.yaw = g;
-        this.rightTendril.yaw = -g;
+
+        this.leftTendril.yaw = g / 2f;
+        this.rightTendril.yaw = -g / 2f;
+
+        this.leftTendril.roll = f / 2f;
+        this.rightTendril.roll = -f / 2f;
     }
 }
