b1b3b7dd6e54f77ff9665a25c12ae47322762c64
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-04-28 10:03:38 -0400

    Ancient Goat Horn (Use In Deep Dark)

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterSounds.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterSounds.java
@@ -24,6 +24,8 @@ public class RegisterSounds {
 
     public static final SoundEvent BLOCK_SCULK_ECHOER_RECEIVE_VIBRATION = new SoundEvent(new Identifier(WilderWild.MOD_ID, "block.sculk_echoer.receive_vibration"));
 
+    public static final SoundEvent ANCIENT_GOAT_HORN_CALL_0 = new SoundEvent(new Identifier(WilderWild.MOD_ID, "item.ancient_goat_horn.sound.0"));
+
     public static final SoundEvent MUSIC_DISC_BENEATH = new SoundEvent(new Identifier(WilderWild.MOD_ID, "music_disc.beneath"));
     public static final SoundEvent MUSIC_DISC_GOATHORN_SYMPHONY = new SoundEvent(new Identifier(WilderWild.MOD_ID, "music_disc.goathorn_symphony"));
 
