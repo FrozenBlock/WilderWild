aaeefaa3851675e08dc913e114c6871d8e5bc56a
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-04-29 13:31:07 -0400

    Plants Vs. Zombies: Garden Warfare Damage Falloff In Minecraft

--- src/main/java/net/frozenblock/wilderwild/entity/AncientHornProjectileEntity.java
+++ src/main/java/net/frozenblock/wilderwild/entity/AncientHornProjectileEntity.java
@@ -261,9 +261,10 @@ public class AncientHornProjectileEntity extends PersistentProjectileEntity {
         }
     }
     public double getDamage() {
-        double distance = this.getBlockPos().getSquaredDistance(new Vec3d(this.vecX, this.vecY, this.vecZ));
-        distance = MathHelper.clamp(distance*distance, 7.8, 45);
-        return 20*Math.sin((distance*Math.PI)/50);
+        double distance = Math.sqrt(this.getBlockPos().getSquaredDistance(new Vec3d(this.vecX, this.vecY, this.vecZ)));
+        distance = MathHelper.clamp(distance, 7.8, 45);
+        System.out.println(distance);
+        return 15*Math.sin((distance*Math.PI)/50);
     }
     protected float getDragInWater() { return 1.0F; }
     public boolean hasNoGravity() { return true; }
