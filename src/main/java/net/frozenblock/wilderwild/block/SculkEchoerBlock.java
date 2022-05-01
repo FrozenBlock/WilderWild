1311bcdffb90a898f367ff8a1e78f1c95e21ba57
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-01 17:49:43 -0400

    Shut up while you're drowning.

--- src/main/java/net/frozenblock/wilderwild/block/SculkEchoerBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/SculkEchoerBlock.java
@@ -199,7 +199,9 @@ public class SculkEchoerBlock extends BlockWithEntity implements Waterloggable {
             world.setBlockState(pos, state.with(SCULK_ECHOER_PHASE, SculkEchoerPhase.ACTIVE), 3);
             world.createAndScheduleBlockTick(pos, state.getBlock(), bubbles+additionalCooldown);
             updateNeighbors(world, pos);
-            world.playSound(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, RegisterSounds.BLOCK_SCULK_ECHOER_ECHO, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 0.9F);
+            if (!state.get(WATERLOGGED)) {
+                world.playSound(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, RegisterSounds.BLOCK_SCULK_ECHOER_ECHO, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.1F + 0.9F);
+            }
         }
     }
 
