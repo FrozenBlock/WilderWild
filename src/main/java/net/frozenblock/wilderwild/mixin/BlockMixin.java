6af872ac91d45dd4a524feb793e7b8bf510657a3
Merp <27739930+MerpisMe@users.noreply.github.com>
2022-05-04 10:03:06 -0500

    fix maybe

--- src/main/java/net/frozenblock/wilderwild/mixin/BlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/BlockMixin.java
@@ -1,10 +1,10 @@
 package net.frozenblock.wilderwild.mixin;
 
 import net.minecraft.core.Registry;
-import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
