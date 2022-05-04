440b3ffec000685422e627d028b821d9d2f4ca65
Merp <27739930+MerpisMe@users.noreply.github.com>
2022-05-04 09:44:24 -0500

    i hope this works

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/BlockMixin.java
@@ -0,0 +1,34 @@
+package net.frozenblock.wilderwild.mixin;
+
+import net.minecraft.core.Registry;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public abstract class MixinBlock {
+	@Shadow protected abstract Block asBlock();
+
+	private static final SoundType MUSHROOMS = new SoundType(1.0F, 1.0F, RegisterSounds.BLOCK_MUSHROOM_BREAK, RegisterSounds.BLOCK_MUSHROOM_STEP, RegisterSounds.BLOCK_MUSHROOM_PLACE, RegisterSounds.BLOCK_MUSHROOM_HIT, RegisterSounds.BLOCK_MUSHROOM_FALL);
+  
+	@Inject(method = "getSoundType", at = @At("TAIL"), cancellable = true)
+	private void getSoundGroupMixin(CallbackInfoReturnable<SoundType> cir){
+		String blockId = Registry.BLOCK.getKey(this.asBlock()).getPath();
+		cir.setReturnValue(
+				switch (blockId){
+					case
+							"red_mushroom",
+							"brown_mushroom"
+							-> MUSHROOMS;
+          
+					default -> cir.getReturnValue();
+				}
+		);
+	}
+}
