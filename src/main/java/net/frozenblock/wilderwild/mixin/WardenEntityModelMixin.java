6951953f76e49ad89f397f6f5a8e5fa09e61cba7
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-05-07 15:46:51 -0500

    maybe better warden head looking

--- src/main/java/net/frozenblock/wilderwild/mixin/WardenEntityModelMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/WardenEntityModelMixin.java
@@ -12,13 +12,55 @@ import org.spongepowered.asm.mixin.Shadow;
 public class WardenEntityModelMixin<T extends WardenEntity> {
     @Final
     @Shadow
+    protected ModelPart head;
+    @Final
+    @Shadow
+    protected ModelPart body;
+    @Final
+    @Shadow
+    protected ModelPart leftArm;
+    @Final
+    @Shadow
+    protected ModelPart rightArm;
+    @Final
+    @Shadow
     protected ModelPart rightTendril;
     @Final
     @Shadow
     protected ModelPart leftTendril;
     /**
      * @author FrozenBlock
-     * @reason yay pitches
+     * @reason better head looking???
+     */
+    @Overwrite
+    private void setHeadAngle(float yaw, float pitch) {
+
+        float headX = pitch * 0.017453292F;
+        float headY = yaw * 0.017453292F;
+        float r = (float) (180 / Math.PI);
+
+        this.head.pitch = headX;
+        this.head.yaw = headY;
+        this.head.roll = headY / 2F;
+
+        this.body.pitch = headY / 2F;
+        this.body.yaw = Math.abs(headY / 2) + (headX / 2);
+        this.body.pivotX = r * -(headY / 32);
+        this.body.pivotZ = r * (-Math.abs(headY / 16) - (headX / 16));
+
+        this.rightArm.pitch = Math.abs(headY / 2) + Math.abs(headX / 2);
+        this.rightArm.roll = Math.abs(headY / 5) + Math.abs(headX / 5);
+        this.rightArm.pivotZ = r * -(headY / 32);
+
+        this.leftArm.pitch = Math.abs(headY / 2) + Math.abs(headX / 2);
+        this.leftArm.roll = -Math.abs(headY / 5) - Math.abs(headX / 5);
+        this.leftArm.pivotZ = r * -(headY / 32);
+
+    }
+
+    /**
+     * @author FrozenBlock
+     * @reason yay tendrils
      */
     @Overwrite
     private void setTendrilPitches(T warden, float animationProgress, float tickDelta) {
