457edcd7a777dd963562bb4374596e334374c273
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-12 08:01:36 -0400

    Update SculkJawBlock.java

--- src/main/java/net/frozenblock/wilderwild/block/SculkJawBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/SculkJawBlock.java
@@ -62,7 +62,7 @@ public class SculkJawBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof LivingEntity && !state.get(ACTIVE) && !(entity instanceof WardenEntity)) {
-            float damage = entity instanceof PlayerEntity ? 2.5f : 5f;
+            float damage = entity instanceof PlayerEntity ? 2.5f : 8f;
                 world.setBlockState(pos, state.with(ACTIVE, true), 3);
                 entity.damage(NewDamageSource.SCULK_JAW, damage);
                 world.createAndScheduleBlockTick(pos, state.getBlock(), 60);
