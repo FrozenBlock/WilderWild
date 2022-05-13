f41a56b8991167b6e95de928876b8cf0cb8a5c78
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-13 11:26:47 -0400

    Testing Goat Spawns In Taiga

--- src/main/java/net/frozenblock/wilderwild/world/gen/WildWorldGen.java
+++ src/main/java/net/frozenblock/wilderwild/world/gen/WildWorldGen.java
@@ -1,6 +1,11 @@
 package net.frozenblock.wilderwild.world.gen;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.frozenblock.wilderwild.world.gen.treedecorators.WildTreeDecorators;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.BiomeKeys;
 
 public class WildWorldGen {
     public static void generateWildWorldGen() {
@@ -13,6 +18,8 @@ public class WildWorldGen {
         WildTreeDecorators.generateTreeDecorators();
         WildTreesGeneration.generateTrees();
         WildMushroomGeneration.generateMushroom();
+
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.TAIGA), SpawnGroup.CREATURE, EntityType.GOAT, 2, 3, 6);
     }
 }
 
