8b517917904c4db05f48a6dc86a412a4a55bdcf4
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-13 11:41:50 -0400

    Apparently It's Literally Impossible For Goats To Spawn In Taigas

--- src/main/java/net/frozenblock/wilderwild/world/gen/WildWorldGen.java
+++ src/main/java/net/frozenblock/wilderwild/world/gen/WildWorldGen.java
@@ -1,11 +1,6 @@
 package net.frozenblock.wilderwild.world.gen;
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.frozenblock.wilderwild.world.gen.treedecorators.WildTreeDecorators;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.BiomeKeys;
 
 public class WildWorldGen {
     public static void generateWildWorldGen() {
@@ -18,8 +13,6 @@ public class WildWorldGen {
         WildTreeDecorators.generateTreeDecorators();
         WildTreesGeneration.generateTrees();
         WildMushroomGeneration.generateMushroom();
-
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.TAIGA), SpawnGroup.CREATURE, EntityType.GOAT, 2, 3, 6);
     }
 }
 
