8f9bb54e5850301b1f51283cbd26f285bbccccf4
Xfrtrex <80720298+BluePhoenixLOL@users.noreply.github.com>
2022-05-15 12:29:37 -0700

    we need to fix the renderer

--- src/main/java/net/frozenblock/wilderwild/entity/render/FireflyEntityRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/FireflyEntityRenderer.java
@@ -13,11 +13,10 @@ import net.minecraft.util.math.MathHelper;
 @Environment(EnvType.CLIENT)
 public class FireflyEntityRenderer extends MobEntityRenderer<FireflyEntity, FireflyEntityModel<FireflyEntity>> {
     private static final Identifier TEXTURE = new Identifier(WilderWild.MOD_ID, "textures/entity/firefly/firefly_off.png");
-    private static final Identifier LIGHT_LAYER = new Identifier(WilderWild.MOD_ID, "textures/entity/firefly/firefly_on.png");
 
     public FireflyEntityRenderer(EntityRendererFactory.Context context) {
         super(context, new FireflyEntityModel<>(context.getPart(WildClientMod.MODEL_FIREFLY_LAYER)), 0.3F);
-        //this.addFeature(new FireflyFeatureRenderer<>(this, LIGHT_LAYER, (firefly, tickDelta, animationProgress) -> Math.max(1.0F, MathHelper.cos(animationProgress * 0.1F) * 0.25F), FireflyEntityModel::getMain));
+        this.addFeature(new FireflyFeatureRenderer(this));
     }
 
     @Override
--- src/main/java/net/frozenblock/wilderwild/entity/render/FireflyFeatureRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/FireflyFeatureRenderer.java
@@ -2,17 +2,40 @@ package net.frozenblock.wilderwild.entity.render;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.frozenblock.wilderwild.WilderWild;
 import net.frozenblock.wilderwild.entity.FireflyEntity;
-import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-
-import java.util.List;
+import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
-public class FireflyFeatureRenderer {
+public class FireflyFeatureRenderer extends EyesFeatureRenderer<FireflyEntity, FireflyEntityModel<FireflyEntity>> {
+    public RenderLayer GLOW;
+
+    public FireflyFeatureRenderer(FeatureRendererContext<FireflyEntity, FireflyEntityModel<FireflyEntity>> featureRendererContext) {
+        super(featureRendererContext);
+        GLOW = RenderLayer.getEyes(new Identifier(WilderWild.MOD_ID, "textures/entity/firefly/firefly_on.png"));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, FireflyEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(this.GLOW);
+            this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, calcGlow(entity), calcGlow(entity), calcGlow(entity), 1.0f);
+    }
+
+    private static float calcGlow(FireflyEntity firefly) {
+        //help i dont know this stuff please fix
+        float d = (float) ((float) Math.cos((1*Math.PI)/(20)));
+        return MathHelper.clamp(d,0,1);
+    }
+
+    public RenderLayer getEyesTexture() {
+        return this.GLOW;
+    }
 }
