3cb295395ce6a8aba612cc000dbda3b32b47b9cb
Treetrain1 <treetrain1@gmail.com>
2022-05-15 14:09:41 -0500

    sus firefly

--- src/main/java/net/frozenblock/wilderwild/entity/FireflyEntity.java
+++ src/main/java/net/frozenblock/wilderwild/entity/FireflyEntity.java
@@ -1,28 +1,32 @@
 package net.frozenblock.wilderwild.entity;
 
-import net.frozenblock.wilderwild.entity.ai.FloatWanderGoal;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Flutterer;
 import net.minecraft.entity.ai.control.FlightMoveControl;
 import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.ai.pathing.BirdNavigation;
-import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.server.network.DebugInfoSender;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
-public class FireflyEntity extends AnimalEntity implements Flutterer {
-    public FireflyEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+public class FireflyEntity extends FlyingEntity implements Flutterer {
+    public FireflyEntity(EntityType<? extends FlyingEntity> entityType, World world) {
         super(entityType, world);
         this.moveControl = new FlightMoveControl(this, 20, true);
+        this.experiencePoints = 1;
+        this.setPathfindingPenalty(PathNodeType.UNPASSABLE_RAIL, 0.0F);
         this.setPathfindingPenalty(PathNodeType.DANGER_FIRE, -1.0F);
         this.setPathfindingPenalty(PathNodeType.WATER, -1.0F);
         this.setPathfindingPenalty(PathNodeType.WATER_BORDER, 16.0F);
@@ -35,8 +39,21 @@ public class FireflyEntity extends AnimalEntity implements Flutterer {
         return !this.onGround;
     }
 
-    public static DefaultAttributeContainer.Builder createFireflyAttributes() {
-        return HostileEntity.createHostileAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 1D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.005F).add(EntityAttributes.GENERIC_FLYING_SPEED, 0.005F);
+    public static DefaultAttributeContainer.Builder addAttributes() {
+        return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 1D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.005F).add(EntityAttributes.GENERIC_FLYING_SPEED, 0.005F);
+    }
+
+    protected boolean canStartRiding(Entity entity) {
+        return false;
+    }
+
+    public boolean disablesShield() {
+        return false;
+    }
+
+    protected void sendAiDebugData() {
+        super.sendAiDebugData();
+        DebugInfoSender.sendBrainDebugData(this);
     }
 
     protected void initGoals() {
@@ -47,12 +64,6 @@ public class FireflyEntity extends AnimalEntity implements Flutterer {
     @Override
     public void tickMovement() {
         super.tickMovement();
-    }
-
-    @Nullable
-    @Override
-    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null;
     }
 
     @Override
