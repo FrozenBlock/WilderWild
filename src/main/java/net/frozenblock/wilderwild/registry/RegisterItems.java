10602b4536c319337f75282426c7a5dcf8b1f3c3
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-05-18 16:10:50 -0500

    @AViewFromTheTop yo read desc how do we add music to vanilla biomes lol also, you HAVE to listen to serene sonder

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterItems.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterItems.java
@@ -18,6 +18,7 @@ public class RegisterItems {
     public static final MilkweedPodItem MILKWEED_POD = new MilkweedPodItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(64));
     public static final MusicDiscItem MUSIC_DISC_BENEATH = MusicDiscItemInvoker.invokeConstructor(15, RegisterSounds.MUSIC_DISC_BENEATH, new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE));
     public static final MusicDiscItem MUSIC_DISC_GOATHORN_SYMPHONY = MusicDiscItemInvoker.invokeConstructor(15, RegisterSounds.MUSIC_DISC_GOATHORN_SYMPHONY, new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE));
+    public static final MusicDiscItem MUSIC_DISC_THE_OTHER_SIDE = MusicDiscItemInvoker.invokeConstructor(15, RegisterSounds.MUSIC_DISC_THE_OTHER_SIDE, new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE));
     public static final Item FIREFLY_SPAWN_EGG = new SpawnEggItem(RegisterEntities.FIREFLY, Integer.parseInt("2A2E2B", 16), Integer.parseInt("AAF644", 16), new FabricItemSettings().group(ItemGroup.MISC));
 
 
@@ -26,6 +27,7 @@ public class RegisterItems {
         Registry.register(Registry.ITEM, new Identifier(WilderWild.MOD_ID, "milkweed_pod"), MILKWEED_POD);
         Registry.register(Registry.ITEM, new Identifier(WilderWild.MOD_ID, "music_disc_beneath"), MUSIC_DISC_BENEATH);
         Registry.register(Registry.ITEM, new Identifier(WilderWild.MOD_ID, "music_disc_goathorn_symphony"), MUSIC_DISC_GOATHORN_SYMPHONY);
+        Registry.register(Registry.ITEM, new Identifier(WilderWild.MOD_ID, "music_disc_the_other_side"), MUSIC_DISC_THE_OTHER_SIDE);
         Registry.register(Registry.ITEM, new Identifier(WilderWild.MOD_ID, "firefly_spawn_egg"), FIREFLY_SPAWN_EGG);
     }
 }
--- src/main/java/net/frozenblock/wilderwild/registry/RegisterSounds.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterSounds.java
@@ -49,8 +49,11 @@ public class RegisterSounds {
     public static final SoundEvent ANCIENT_HORN_CALL= register("item.ancient_horn.call");
     public static final SoundEvent ANCIENT_HORN_VIBRATION_DISSAPATE = register("entity.ancient_horn_projectile.dissipate");
 
+    public static final SoundEvent MUSIC_OVERWORLD_FORESTS = register("music.overworld.forests");
+
     public static final SoundEvent MUSIC_DISC_BENEATH = register("music_disc.beneath");
     public static final SoundEvent MUSIC_DISC_GOATHORN_SYMPHONY = register("music_disc.goathorn_symphony");
+    public static final SoundEvent MUSIC_DISC_THE_OTHER_SIDE = register("music_disc.the_other_side");
 
     public static SoundEvent register(String string) {
         return Registry.register(Registry.SOUND_EVENT, new Identifier(WilderWild.MOD_ID, string), new SoundEvent(new Identifier(WilderWild.MOD_ID, string)));
