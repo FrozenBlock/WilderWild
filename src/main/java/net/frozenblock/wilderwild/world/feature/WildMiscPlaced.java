de924ac2f25446faaa52da7a342d94754f656c12
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-20 12:46:26 -0400

    Frequency Increased

--- src/main/java/net/frozenblock/wilderwild/world/feature/WildMiscPlaced.java
+++ src/main/java/net/frozenblock/wilderwild/world/feature/WildMiscPlaced.java
@@ -20,6 +20,6 @@ public class WildMiscPlaced {
     static {
         FOREST_ROCK_TAIGA = PlacedFeatures.register("forest_rock_taiga", MiscConfiguredFeatures.FOREST_ROCK, new PlacementModifier[]{RarityFilterPlacementModifier.of(7), SquarePlacementModifier.of(), PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of()});
         DISK_MUD = PlacedFeatures.register("disk_mud", WildMiscConfigured.DISK_MUD, new PlacementModifier[]{CountPlacementModifier.of(1), SquarePlacementModifier.of(), PlacedFeatures.OCEAN_FLOOR_WG_HEIGHTMAP, RandomOffsetPlacementModifier.vertically(ConstantIntProvider.create(-1)), BlockFilterPlacementModifier.of(BlockPredicate.matchingBlocks(new Block[]{Blocks.GRASS_BLOCK, Blocks.DIRT})), BiomePlacementModifier.of()});
-        COARSE_PATH = PlacedFeatures.register("coarse_path", WildMiscConfigured.COARSE_PATH, RarityFilterPlacementModifier.of(3), SquarePlacementModifier.of(), PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP, BiomePlacementModifier.of());
+        COARSE_PATH = PlacedFeatures.register("coarse_path", WildMiscConfigured.COARSE_PATH, RarityFilterPlacementModifier.of(1), SquarePlacementModifier.of(), PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP, BiomePlacementModifier.of());
     }
 }
