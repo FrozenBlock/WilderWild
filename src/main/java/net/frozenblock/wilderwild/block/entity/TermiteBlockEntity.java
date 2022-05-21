f9b7020e956d19e10e5445094672ede460011d58
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-20 21:50:01 -0400

    Test 2

--- src/main/java/net/frozenblock/wilderwild/block/entity/TermiteBlockEntity.java
+++ src/main/java/net/frozenblock/wilderwild/block/entity/TermiteBlockEntity.java
@@ -1,24 +1,28 @@
 package net.frozenblock.wilderwild.block.entity;
 
 import com.mojang.logging.LogUtils;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.frozenblock.wilderwild.registry.RegisterBlockEntityType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class TermiteBlockEntity extends BlockEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
     ArrayList<Termite> termites = new ArrayList<>();
-    IntArrayList termiteX = new IntArrayList();
-    IntArrayList termiteY = new IntArrayList();
-    IntArrayList termiteZ = new IntArrayList();
-    IntArrayList termiteDestroyPower = new IntArrayList();
 
     public TermiteBlockEntity(BlockPos pos, BlockState state) {
         super(RegisterBlockEntityType.TERMITE, pos, state);
@@ -26,52 +30,36 @@ public class TermiteBlockEntity extends BlockEntity {
 
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
-        this.termites.clear();
-        this.termiteX = IntArrayList.wrap(nbt.getIntArray("termiteX"));
-        this.termiteY = IntArrayList.wrap(nbt.getIntArray("termiteY"));
-        this.termiteZ = IntArrayList.wrap(nbt.getIntArray("termiteZ"));
-        this.termiteDestroyPower = IntArrayList.wrap(nbt.getIntArray("termiteDestroyPower"));
-        for (int x : termiteX) {
-            int index = termiteX.indexOf(x);
-            int y = termiteY.getInt(index);
-            int z = termiteZ.getInt(index);
-            int power = termiteDestroyPower.getInt(index);
-            this.termites.add(new Termite(x,y, z, power));
+        if (nbt.contains("termites", 9)) {
+            this.termites.clear();
+            DataResult<?> var10000 = Termite.CODEC.listOf().parse(new Dynamic<>(NbtOps.INSTANCE, nbt.getList("termites", 10)));
+            Logger var10001 = LOGGER;
+            Objects.requireNonNull(var10001);
+            Optional<List> list = (Optional<List>) var10000.resultOrPartial(var10001::error);
+            if (list.isPresent()) {
+                List termitesAllAllAll = list.get();
+                int i = Math.min(termitesAllAllAll.size(), 32);
+
+                for (int j = 0; j < i; ++j) {
+                    this.termites.add((Termite) termitesAllAllAll.get(j));
+                }
+            }
         }
     }
 
-    public void removeTermite(Termite termite) {
-        int index = termites.indexOf(termite);
-        termites.remove(termite);
-        termiteX.removeInt(index);
-        termiteY.removeInt(index);
-        termiteZ.removeInt(index);
-        termiteDestroyPower.removeInt(index);
+    protected void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        DataResult<?> var10000 = Termite.CODEC.listOf().encodeStart(NbtOps.INSTANCE, this.termites);
+        Logger var10001 = LOGGER;
+        Objects.requireNonNull(var10001);
+        var10000.resultOrPartial(var10001::error).ifPresent((cursorsNbt) -> {
+            nbt.put("termites", (NbtElement) cursorsNbt);
+        });
     }
 
     public void addTermite(BlockPos pos) {
         Termite termite = new Termite(pos.getX(), pos.getY(), pos.getZ(), 0);
         this.termites.add(termite);
-    }
-
-    protected void writeNbt(NbtCompound nbt) {
-        super.writeNbt(nbt);
-        if (!this.termites.isEmpty()) {
-            this.termiteX.clear();
-            this.termiteY.clear();
-            this.termiteZ.clear();
-            this.termiteDestroyPower.clear();
-            for (Termite termite : termites) {
-                termiteX.add(termite.pos.getX());
-                termiteY.add(termite.pos.getY());
-                termiteZ.add(termite.pos.getZ());
-                termiteDestroyPower.add(termite.blockDestroyPower);
-            }
-        }
-        nbt.putIntArray("termiteX", this.termiteX);
-        nbt.putIntArray("termiteY", this.termiteY);
-        nbt.putIntArray("termiteZ", this.termiteZ);
-        nbt.putIntArray("termiteDestroyPower", this.termiteDestroyPower);
     }
 
     public void tick(World world, BlockPos pos, BlockState state) {
@@ -81,13 +69,28 @@ public class TermiteBlockEntity extends BlockEntity {
         this.addTermite(pos.up());
     }
 
-    public class Termite {
+    public static class Termite {
         public BlockPos pos;
         public int blockDestroyPower;
+        public static final Codec<Termite> CODEC = RecordCodecBuilder.create((instance) -> {
+            return instance.group(BlockPos.CODEC.fieldOf("pos").forGetter(Termite::getPos),
+                    Codec.intRange(0, 10000).fieldOf("blockDestroyPower").orElse(0).forGetter(Termite::getPower)).apply(instance, Termite::new);
+        });
 
         public Termite(int x, int y, int z, int blockDestroyPower) {
             this.pos = new BlockPos(x,y,z);
             this.blockDestroyPower=blockDestroyPower;
+        }
+        public Termite(BlockPos pos, int blockDestroyPower) {
+            this.pos = pos;
+            this.blockDestroyPower=blockDestroyPower;
+        }
+
+        public BlockPos getPos() {
+            return this.pos;
+        }
+        public int getPower() {
+            return this.blockDestroyPower;
         }
 
     }
