172a39a866d89e2ed9e23de5990c52c834d39e95
Merp <27739930+MerpisMe@users.noreply.github.com>
2022-05-23 10:27:29 -0500

    like this kinda @AViewFromTheTop

--- src/main/java/net/frozenblock/wilderwild/entity/render/AncientHornProjectileModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/AncientHornProjectileModel.java
@@ -13,10 +13,10 @@ import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
 public class AncientHornProjectileModel extends Model {
-    public static final Identifier TEXTURE = new Identifier(WilderWild.MOD_ID, "textures/entity/ancient_horn_projectile.png");
+    public static final Identifier TEXTURE = new Identifier(WilderWild.MOD_ID, "textures/particle/floating_sculk_bubble_4.png");
     private final ModelPart root;
     public float merp = (float) (90 * (Math.PI/180));
-    public float merp2 = (float) (-15 * (Math.PI/180));
+    public float merp2 = (float) (-90 * (Math.PI/180));
     public AncientHornProjectileModel(ModelPart root) {
         super(RenderLayer::getEntityTranslucentEmissive);
         this.root = root;
@@ -30,9 +30,9 @@ public class AncientHornProjectileModel extends Model {
     }
 
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha, float tickDelta, AncientHornProjectileEntity entity) {
-        matrices.scale(0.6f,0.6f,0.6f);
+        matrices.scale(1.0f,1.0f,1.0f);
         float f = MathHelper.sin(((float)entity.aliveTicks + tickDelta - 6.2831855F) * 0.05F) * 2.0F;
-        this.root.yaw = f + merp;
+        this.root.yaw = merp; //f + merp
         this.root.pitch = merp2;
         this.root.render(matrices, vertices, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
     }
