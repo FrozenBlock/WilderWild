eff1fd7b0152d96061db1fd045e6daac2df2498d
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-23 15:00:00 -0400

    yay
    @MerpisMe You'll have to do the textures and UV but i think this is good

--- src/main/java/net/frozenblock/wilderwild/entity/render/AncientHornProjectileModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/AncientHornProjectileModel.java
@@ -15,31 +15,44 @@ public class AncientHornProjectileModel extends Model {
     public static final Identifier TEXTURE = new Identifier(WilderWild.MOD_ID, "textures/entity/ancient_horn_projectile.png");
     private final ModelPart root;
     private final ModelPart body;
+    private final ModelPart body2;
+    private final ModelPart body3;
     public float merp = (float) (90 * (Math.PI/180));
     public float merp2 = (float) (-90 * (Math.PI/180));
     public AncientHornProjectileModel(ModelPart root) {
         super(RenderLayer::getEntityTranslucentEmissive);
         this.root = root;
         this.body = root.getChild("body");
+        this.body2 = root.getChild("body2");
+        this.body3 = root.getChild("body3");
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData modelPartData2 = modelPartData.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, -8.0F, 0.0F, 16.0F, 16.0F, 0.001F), ModelTransform.NONE);
+        ModelPartData modelPartData3 = modelPartData.addChild("body2", ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, -8.0F, 8.0F, 16.0F, 16.0F, 0.001F), ModelTransform.NONE);
+        ModelPartData modelPartData4 = modelPartData.addChild("body3", ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, -8.0F, 16.0F, 16.0F, 16.0F, 0.001F), ModelTransform.NONE);
         return TexturedModelData.of(modelData, 32, 32);
     }
 
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha, float tickDelta, AncientHornProjectileEntity entity) {
         matrices.scale(1.0f,1.0f,1.0f);
-        this.body.yaw = merp;
-        this.body.pitch = merp2;
-
+        this.root.yaw = merp;
+        this.root.pitch = merp2;
         float pulse = (float) ((Math.sin(((entity.aliveTicks + tickDelta)*Math.PI)/5)/4) + 0.5);
         this.body.xScale = pulse;
         this.body.yScale = pulse;
-        this.body.zScale = pulse;
-        this.body.render(matrices, vertices, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+
+        float pulse2 = (float) ((Math.sin((((entity.aliveTicks+(8/1.5)) + tickDelta)*Math.PI)/5)/4) + 0.5);
+        this.body2.xScale = pulse2;
+        this.body2.yScale = pulse2;
+
+        float pulse3 = (float) ((Math.sin((((entity.aliveTicks+(8/3)) + tickDelta)*Math.PI)/5)/4) + 0.5);
+        this.body3.xScale = pulse3;
+        this.body3.yScale = pulse3;
+
+        this.root.render(matrices, vertices, light, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     @Override
