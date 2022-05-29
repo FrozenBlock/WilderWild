61147480f94cb0e7c7ca29b0b77c924cfb7baa32
LIUKRAST <73425030+LIUKRAST@users.noreply.github.com>
2022-05-30 00:15:16 +0200

    Base BaobabTrunkPlacer & some mathematics cause yeah Btw fuck soro your 3x3 tree looks weird

---
+++ src/main/java/net/frozenblock/api/mathematics/AdvancedMath.java
@@ -0,0 +1,20 @@
+package net.frozenblock.api.mathematics;
+
+public class AdvancedMath {
+    /**
+     * ADVANCED MATH
+     * <p>
+     * Adds more math operations
+     * <p>
+     * Only for FrozenBlock Modders, ALL RIGHTS RESERVED
+     * <p>
+     *
+     *
+     * @author      LiukRast (2021-2022)
+     * @since 4.0
+     *
+     */
+    public static float range(float min, float max, float number) {
+        return (number * max) + min;
+    }
+}
--- src/main/java/net/frozenblock/wilderwild/world/gen/trunk/BaobabTrunkPlacer.java
+++ src/main/java/net/frozenblock/wilderwild/world/gen/trunk/BaobabTrunkPlacer.java
@@ -5,6 +5,8 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import java.util.List;
 import java.util.function.BiConsumer;
+
+import net.frozenblock.api.mathematics.AdvancedMath;
 import net.frozenblock.wilderwild.WilderWild;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
@@ -16,9 +18,8 @@ import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 
 public class BaobabTrunkPlacer extends TrunkPlacer {
-    public static final Codec<net.frozenblock.wilderwild.world.gen.trunk.BaobabTrunkPlacer> CODEC = RecordCodecBuilder.create((instance) -> {
-        return fillTrunkPlacerFields(instance).apply(instance, net.frozenblock.wilderwild.world.gen.trunk.BaobabTrunkPlacer::new);
-    });
+    public static final Codec<net.frozenblock.wilderwild.world.gen.trunk.BaobabTrunkPlacer> CODEC = RecordCodecBuilder.create((instance) ->
+            fillTrunkPlacerFields(instance).apply(instance, BaobabTrunkPlacer::new));
 
     public BaobabTrunkPlacer(int i, int j, int k) {
         super(i, j, k);
@@ -28,54 +29,57 @@ public class BaobabTrunkPlacer extends TrunkPlacer {
         return WilderWild.BAOBAB_TRUNK_PLACER;
     }
 
+    @Override
     public List<FoliagePlacer.TreeNode> generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, int height, BlockPos startPos, TreeFeatureConfig config) {
         BlockPos blockPos = startPos.down();
-        setToDirt(world, replacer, random, blockPos, config);
-        setToDirt(world, replacer, random, blockPos.east(), config);
-        setToDirt(world, replacer, random, blockPos.east().east(), config);
-        setToDirt(world, replacer, random, blockPos.east().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south(), config);
-        setToDirt(world, replacer, random, blockPos.south().south(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().south(), config);
-        setToDirt(world, replacer, random, blockPos.south().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().east().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().east().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().south().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().south().east().east(), config);
-        setToDirt(world, replacer, random, blockPos.south().south().south().east().east().east(), config);
+
+        // generate dirt below tree's base
+        setToDirt(world, replacer, random, blockPos, config); // CENTER
+        setToDirt(world, replacer, random, blockPos.east(), config); // EAST
+        setToDirt(world, replacer, random, blockPos.west(), config); // WEST
+        setToDirt(world, replacer, random, blockPos.north(), config); // NORTH
+        setToDirt(world, replacer, random, blockPos.north().east(), config); // NORTH-EAST
+        setToDirt(world, replacer, random, blockPos.north().west(), config); // NORTH-WEST
+        setToDirt(world, replacer, random, blockPos.south(), config); // SOUTH
+        setToDirt(world, replacer, random, blockPos.south().east(), config); // SOUTH-EAST
+        setToDirt(world, replacer, random, blockPos.south().west(), config); // SOUTH-WEST
         BlockPos.Mutable mutable = new BlockPos.Mutable();
 
-        for(int i = 0; i < height; ++i) {
-            this.setLog(world, replacer, random, mutable, config, startPos, 0, i, 0);
-            if (i < height - 1) {
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 0);
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 1);
-                this.setLog(world, replacer, random, mutable, config, startPos, 0, i, 1);
-                this.setLog(world, replacer, random, mutable, config, startPos, 2, i, 0);
-                this.setLog(world, replacer, random, mutable, config, startPos, 3, i, 0);
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 2);
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 3);
-                this.setLog(world, replacer, random, mutable, config, startPos, 2, i, 1);
-                this.setLog(world, replacer, random, mutable, config, startPos, 3, i, 1);
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 2);
-                this.setLog(world, replacer, random, mutable, config, startPos, 1, i, 3);
-                this.setLog(world, replacer, random, mutable, config, startPos, 2, i, 2);
-                this.setLog(world, replacer, random, mutable, config, startPos, 2, i, 3);
-                this.setLog(world, replacer, random, mutable, config, startPos, 3, i, 3);
-                this.setLog(world, replacer, random, mutable, config, startPos, 3, i, 2);
-                this.setLog(world, replacer, random, mutable, config, startPos, 0, i, 2);
-                this.setLog(world, replacer, random, mutable, config, startPos, 0, i, 3);
-            }
-        }
+        int min = 6;
+        int max = 8;
 
-        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(height), 3, true));
+
+        //Random Height between min & max
+        int h = (int) AdvancedMath.range(min, max, (float)Math.random());
+        int h_nw = (int)AdvancedMath.range((float)min/3, (float)max/3, (float)Math.random());
+        int h_sw = (int)AdvancedMath.range((float)min/3, (float)max/3, (float)Math.random());
+        int h_ne = (int)AdvancedMath.range((float)min/3, (float)max/3, (float)Math.random());
+        int h_se = (int) AdvancedMath.range((float)min/3, (float)max/3, (float)Math.random());
+
+        for(int i = 0; i < h; i++) {
+            setLog(world, replacer, random, mutable, config, startPos, 0, i, 0); // CENTER
+
+            setLog(world, replacer, random, mutable, config, startPos, 1, i, 0);
+            setLog(world, replacer, random, mutable, config, startPos, -1, i, 0);
+            setLog(world, replacer, random, mutable, config, startPos, 0, i, 1);
+            setLog(world, replacer, random, mutable, config, startPos, 0, i, -1);
+
+            setLog(world, replacer, random, mutable, config, startPos, 1, i, 1, i <= h_nw);
+            setLog(world, replacer, random, mutable, config, startPos, -1, i, 1, i <= h_sw);
+            setLog(world, replacer, random, mutable, config, startPos, 1, i, -1, i <= h_ne);
+            setLog(world, replacer, random, mutable, config, startPos, -1, i, -1, i <= h_se);
+        }
+        return ImmutableList.of(new FoliagePlacer.TreeNode(startPos.up(h), 0, false));
     }
 
-    private void setLog(TestableWorld testableWorld, BiConsumer<BlockPos, BlockState> biConsumer, Random random, BlockPos.Mutable mutable, TreeFeatureConfig treeFeatureConfig, BlockPos blockPos, int i, int j, int k) {
-        mutable.set(blockPos, i, j, k);
-        this.trySetState(testableWorld, biConsumer, random, mutable, treeFeatureConfig);
+    private void setLog(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos.Mutable pos, TreeFeatureConfig config, BlockPos startPos, int x, int y, int z, boolean condition) {
+        if(condition) {
+            pos.set(startPos, x, y, z);
+            trySetState(world, replacer, random, pos, config);
+        }
+    }
+
+    private void setLog(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos.Mutable pos, TreeFeatureConfig config, BlockPos startPos, int x, int y, int z) {
+        setLog(world, replacer, random, pos, config, startPos, x, y, z, true);
     }
 }
