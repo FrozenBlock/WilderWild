8fa73442fdf0729b03899036cec69e89efa68e02
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-30 12:31:34 -0400

    Firefly Swamp Spawning Test

---
+++ src/main/java/net/frozenblock/wilderwild/world/gen/WildSpawns.java
@@ -0,0 +1,16 @@
+package net.frozenblock.wilderwild.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.frozenblock.wilderwild.registry.RegisterEntities;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.BiomeKeys;
+
+public class WildSpawns {
+
+    public static void addFirefliesSwamps() {
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.SWAMP, BiomeKeys.MANGROVE_SWAMP),
+                SpawnGroup.AMBIENT, RegisterEntities.FIREFLY, 4, 4, 8);
+    }
+
+}
