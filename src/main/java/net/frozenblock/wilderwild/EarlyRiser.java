d9234873cdd82588fa138511849679a5f38bd955
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-30 13:57:27 -0400

    WHAT DO YOU MEAN DIFFERING NUMBER OF PARAMETERS

--- src/main/java/net/frozenblock/wilderwild/EarlyRiser.java
+++ src/main/java/net/frozenblock/wilderwild/EarlyRiser.java
@@ -8,7 +8,7 @@ public class EarlyRiser implements Runnable {
     @Override
     public void run() {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
-
+        
         String spawnGroup = remapper.mapClassName("intermediary","net.minecraft.class_1311");
         ClassTinkerers.enumBuilder(spawnGroup).addEnum("FIREFLIES", "fireflies", 30, true, false, 64).build();
     }
