bbae88ec87a589e5429f11afad753eafec76f8df
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-05-30 13:13:34 -0400

    sus jerma man

--- src/main/java/net/frozenblock/wilderwild/EarlyRiser.java
+++ src/main/java/net/frozenblock/wilderwild/EarlyRiser.java
@@ -9,7 +9,7 @@ public class EarlyRiser implements Runnable {
     public void run() {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
 
-        String spawnGroup = remapper.mapClassName("intermediary","net.minecraft.class_1131");
+        String spawnGroup = remapper.mapClassName("intermediary","net.minecraft.class_1311");
         ClassTinkerers.enumBuilder(spawnGroup).addEnum("FIREFLIES", "fireflies", 30, true, false, 64).build();
     }
 }
