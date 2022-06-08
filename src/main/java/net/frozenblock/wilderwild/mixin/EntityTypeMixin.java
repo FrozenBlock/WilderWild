c477ec257772c5f75ad633a0c2eb8830feffcf14
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-06-08 16:24:55 -0400

    tadpoles are so broken just forget about it

--- src/main/java/net/frozenblock/wilderwild/mixin/EntityTypeMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/EntityTypeMixin.java
@@ -15,12 +15,12 @@ import java.util.Objects;
 @Mixin(EntityType.class)
 public class EntityTypeMixin {
 
-    @Inject(at = @At("HEAD"), method = "register", cancellable = true)
+    /*@Inject(at = @At("HEAD"), method = "register", cancellable = true)
     private static <T extends Entity> void register(String id, EntityType.Builder<T> type, CallbackInfoReturnable<EntityType<T>> info) {
         if (Objects.equals(id, "tadpole")) {
             info.setReturnValue(Registry.register(Registry.ENTITY_TYPE, id, ((EntityType.Builder<T>) EntityType.Builder.create(TadpoleEntity::new, SpawnGroup.WATER_CREATURE).setDimensions(TadpoleEntity.WIDTH, TadpoleEntity.HEIGHT).maxTrackingRange(10)).build(id)));
             info.cancel();
         }
-    }
+    }*/
 
 }
