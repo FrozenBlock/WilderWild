559e0935b7e4c73256453b52180d164efee12e97
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-06-08 16:21:13 -0400

    why won't it work

--- src/main/java/net/frozenblock/wilderwild/mixin/EntityTypeMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/EntityTypeMixin.java
@@ -18,8 +18,7 @@ public class EntityTypeMixin {
     @Inject(at = @At("HEAD"), method = "register", cancellable = true)
     private static <T extends Entity> void register(String id, EntityType.Builder<T> type, CallbackInfoReturnable<EntityType<T>> info) {
         if (Objects.equals(id, "tadpole")) {
-            type = (EntityType.Builder<T>) EntityType.Builder.create(TadpoleEntity::new, SpawnGroup.WATER_CREATURE).setDimensions(TadpoleEntity.WIDTH, TadpoleEntity.HEIGHT).maxTrackingRange(10);
-            info.setReturnValue(Registry.register(Registry.ENTITY_TYPE, id, type.build(id)));
+            info.setReturnValue(Registry.register(Registry.ENTITY_TYPE, id, ((EntityType.Builder<T>) EntityType.Builder.create(TadpoleEntity::new, SpawnGroup.WATER_CREATURE).setDimensions(TadpoleEntity.WIDTH, TadpoleEntity.HEIGHT).maxTrackingRange(10)).build(id)));
             info.cancel();
         }
     }
