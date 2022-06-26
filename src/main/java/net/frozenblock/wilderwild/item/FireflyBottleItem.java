7759ceb4ab2e3656ce912a266fbbb4afe6c9fe3c
Treetrain1 <treetrain1@gmail.com>
2022-06-26 11:05:41 -0500

    player no longer drinks the firefly lol

--- src/main/java/net/frozenblock/wilderwild/item/FireflyBottleItem.java
+++ src/main/java/net/frozenblock/wilderwild/item/FireflyBottleItem.java
@@ -71,7 +71,6 @@ public class FireflyBottleItem extends Item {
                 FireflyEntity entity = RegisterEntities.FIREFLY.create(server);
                 if (entity != null) {
                     //TODO: FIREFLY BOTTLE SOUNDS
-                    entity.playSound(SoundEvents.ITEM_BOTTLE_EMPTY, 1.0F, 1.0F);
                     entity.setVelocity(f * 0.7, g * 0.7, h * 0.7);
                     entity.refreshPositionAndAngles(user.getX(), user.getEyeY(), user.getZ(), user.getPitch(), user.getYaw());
                     entity.setFromBottle(true);
