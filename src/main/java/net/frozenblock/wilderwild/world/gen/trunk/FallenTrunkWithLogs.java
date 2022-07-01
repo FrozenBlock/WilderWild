43b0122cccb9b1c34e2664621d3ca63735d707b8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-06-30 21:34:45 -0400

    Fix Fallen Log Generation

--- src/main/java/net/frozenblock/wilderwild/world/gen/trunk/FallenTrunkWithLogs.java
+++ src/main/java/net/frozenblock/wilderwild/world/gen/trunk/FallenTrunkWithLogs.java
@@ -81,6 +81,17 @@ public class FallenTrunkWithLogs extends TrunkPlacer {
                             }
                             ++placedLogs;
                             logsAboveHole += holeAddition;
+                        } else if (config.trunkProvider.getBlockState(random, mutable.set(x, startPos.getY(), z)).contains(Properties.FACING)) {
+                            replacer.accept(mutable.set(x, startPos.getY(), z), config.trunkProvider.getBlockState(random, mutable.set(x, startPos.getY(), z)).with(Properties.FACING, logDir));
+                            if (random.nextFloat() > 0.28) {
+                                logs.add(new BlockPos(x, startPos.getY(), z));
+                            }
+                            if (i < height - 1 && random.nextFloat() < this.logChance && placedLogs < maxLogs) {
+                                Direction direction = random.nextFloat() >= 0.33 ? Direction.Type.HORIZONTAL.random(random) : Direction.Type.VERTICAL.random(random);
+                                this.generateExtraBranch(logs, world, replacer, random, config, mutable, logDir, i, direction);
+                            }
+                            ++placedLogs;
+                            logsAboveHole += holeAddition;
                         } else if (this.getAndSetState(world, replacer, random, mutable.set(x, startPos.getY(), z), config)) {
                             if (random.nextFloat() > 0.28) {
                                 logs.add(new BlockPos(x, startPos.getY(), z));
