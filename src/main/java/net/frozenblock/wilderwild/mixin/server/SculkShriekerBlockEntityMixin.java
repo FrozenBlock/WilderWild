cf05b7d08b4a945844306a8495ad06cff7464058
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-07-15 15:34:56 -0400

    Soulless Shriekers Can't Shriek

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockEntityMixin.java
@@ -5,10 +5,16 @@ import net.frozenblock.wilderwild.registry.RegisterProperties;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.SculkShriekerBlockEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.event.GameEvent;
+import net.minecraft.world.event.listener.GameEventListener;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SculkShriekerBlockEntity.class)
@@ -21,6 +27,23 @@ public class SculkShriekerBlockEntityMixin {
         if (blockState.get(RegisterProperties.SOULS_TAKEN) == 2) {
             WilderWild.log(Blocks.SCULK_SHRIEKER, entity.getPos(), "All Souls Have Already Been Taken, Cannot Warn", WilderWild.DEV_LOGGING);
             info.setReturnValue(false);
+            info.cancel();
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "accepts", cancellable = true)
+    public void accepts(ServerWorld world, GameEventListener listener, BlockPos pos, GameEvent event, GameEvent.Emitter emitter, CallbackInfoReturnable<Boolean> info) {
+        SculkShriekerBlockEntity entity = SculkShriekerBlockEntity.class.cast(this);
+        if (entity.getCachedState().get(RegisterProperties.SOULS_TAKEN) == 2) {
+            info.setReturnValue(false);
+            info.cancel();
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "shriek", cancellable = true)
+    public void shriek(ServerWorld world, @Nullable ServerPlayerEntity player, CallbackInfo info) {
+        SculkShriekerBlockEntity entity = SculkShriekerBlockEntity.class.cast(this);
+        if (entity.getCachedState().get(RegisterProperties.SOULS_TAKEN) == 2) {
             info.cancel();
         }
     }
--- src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockMixin.java
@@ -4,7 +4,10 @@ import net.frozenblock.wilderwild.registry.RegisterProperties;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SculkShriekerBlock;
+import net.minecraft.entity.Entity;
 import net.minecraft.state.StateManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,6 +19,13 @@ public class SculkShriekerBlockMixin {
     @Inject(at = @At("TAIL"), method = "appendProperties", cancellable = true)
     public void appendProperties(StateManager.Builder<Block, BlockState> builder, CallbackInfo info) {
         builder.add(RegisterProperties.SOULS_TAKEN);
+    }
+
+    @Inject(at = @At("HEAD"), method = "onSteppedOn", cancellable = true)
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity, CallbackInfo info) {
+        if (state.get(RegisterProperties.SOULS_TAKEN) == 2) {
+            info.cancel();
+        }
     }
 
 }
