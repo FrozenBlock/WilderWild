71ea63e5419b6a77c5c9227cb69b45b518438dae
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-07-16 10:36:53 -0400

    Ok Does This Work

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SlimeMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SlimeMixin.java
@@ -4,11 +4,14 @@ import net.frozenblock.wilderwild.registry.RegisterBlocks;
 import net.frozenblock.wilderwild.tag.WilderBiomeTags;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +20,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Iterator;
 
 @Mixin(SlimeEntity.class)
-public class SlimeMixin {
+public abstract class SlimeMixin extends MobEntity {
+
+    protected SlimeMixin(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Inject(method = "canSpawn", at = @At("HEAD"), cancellable = true)
     private static void canSpawn(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> info) {
         if (world.getDifficulty() != Difficulty.PEACEFUL) {
-            if (world.getBiome(pos).isIn(WilderBiomeTags.SLIMES_SPAWN_ON_FLOATING_MOSS) && world.getLightLevel(pos) <= random.nextInt(8)) {
-                if (isFloatingMossNearby(world, pos, 3)) {
+            if (world.getBiome(pos).isIn(WilderBiomeTags.SLIMES_SPAWN_ON_FLOATING_MOSS)) {
+                if (isFloatingMossNearby(world, pos, 1)) {
                     info.setReturnValue(true);
                     info.cancel();
                 }
@@ -39,6 +46,11 @@ public class SlimeMixin {
             pos = iterator.next();
         } while(!world.getBlockState(pos).isOf(RegisterBlocks.FLOATING_MOSS));
         return true;
+    }
+
+    @Override
+    public boolean canSpawn(WorldView world) {
+        return world.doesNotIntersectEntities(this);
     }
 
 }
