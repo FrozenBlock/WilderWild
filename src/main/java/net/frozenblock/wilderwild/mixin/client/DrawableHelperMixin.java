9d140e23581aec2f521158ddab985f560ff99514
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-07-17 11:15:34 -0400

    am i going too far

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/DrawableHelperMixin.java
@@ -0,0 +1,24 @@
+package net.frozenblock.wilderwild.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.frozenblock.wilderwild.WilderWild;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(DrawableHelper.class)
+public class DrawableHelperMixin {
+
+    @Inject(at = @At(value = "TAIL"), method = "drawStringWithShadow", cancellable = true)
+    private static void drawStringWithShadow(MatrixStack matrices, TextRenderer textRenderer, String text, int x, int y, int color, CallbackInfo info) {
+         info.cancel();
+         textRenderer.drawWithShadow(matrices, "Minecraft " + WilderWild.snapshotName + "/snapshot", (float) x, (float) y, color);
+    }
+
+}
