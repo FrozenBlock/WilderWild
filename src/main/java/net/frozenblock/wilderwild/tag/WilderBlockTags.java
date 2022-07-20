564cd7462641af623ad2a31c7e8c742617937262
Soro335 <gabeonelove335@gmail.com>
2022-07-21 01:00:20 +0300

    Someone can delete sandy dirt?

--- src/main/java/net/frozenblock/wilderwild/tag/WilderBlockTags.java
+++ src/main/java/net/frozenblock/wilderwild/tag/WilderBlockTags.java
@@ -16,7 +16,7 @@ public class WilderBlockTags {
     public static final TagKey<Block> TERMITE_BREAKABLE = of("termite_breakable");
     public static final TagKey<Block> BLOCKS_TERMITE = of("blocks_termite");
     public static final TagKey<Block> FIREFLY_HIDEABLE_BLOCKS = of("firefly_hideable_blocks");
-    public static final TagKey<Block> SANDY_DIRT_REPLACEABLE = of("sandy_dirt_replaceable");
+    public static final TagKey<Block> PACKED_MUD_REPLACEABLE = of("packed_mud_replaceable");
 
     private WilderBlockTags() {
     }
