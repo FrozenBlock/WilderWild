2a2b12626aa2bd5476fa8163342ba7c3ddcc2adb
Treetrain1 <treetrain1@gmail.com>
2022-07-25 15:48:28 -0500

    please work

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SchemasMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SchemasMixin.java
@@ -2,6 +2,7 @@ package net.frozenblock.wilderwild.mixin.server;
 
 import com.mojang.datafixers.DataFixerBuilder;
 import com.mojang.datafixers.schemas.Schema;
+import net.frozenblock.wilderwild.WilderWild;
 import net.minecraft.datafixer.Schemas;
 import net.minecraft.datafixer.fix.BlockNameFix;
 import net.minecraft.datafixer.schema.IdentifierNormalizingSchema;
@@ -20,18 +21,16 @@ import java.util.function.BiFunction;
 @Debug(export = true)
 public class SchemasMixin {
 
-    @Shadow
-    @Final
-    private static BiFunction<Integer, Schema, Schema> EMPTY;
+    @Shadow @Final private static BiFunction<Integer, Schema, Schema> EMPTY_IDENTIFIER_NORMALIZE;
 
     @Inject(method = "build", at = @At("TAIL"))
     private static void build(DataFixerBuilder builder, CallbackInfo ci) {
-        Schema schema69420 = builder.addSchema(3546, EMPTY);
+        Schema schema69420 = builder.addSchema(3097, EMPTY_IDENTIFIER_NORMALIZE);
         builder.addFixer(
                 BlockNameFix.create(
                         schema69420,
                         "white_dandelion block renamer",
-                        id -> Objects.equals(IdentifierNormalizingSchema.normalize(id), "wilderwild:white_dandelion") ? "wilderwild:blooming_dandelion" : id
+                        id -> Objects.equals(IdentifierNormalizingSchema.normalize(id), WilderWild.MOD_ID + ":" + "white_dandelion") ? WilderWild.MOD_ID + ":" + "blooming_dandelion" : id
                 ));
     }
 }
