3011a2352469bc7d48d646710f6b7c6e19cb3eea
Treetrain1 <treetrain1@gmail.com>
2022-07-25 14:02:30 -0500

    schemas are weird

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SchemasMixin.java
@@ -0,0 +1,35 @@
+package net.frozenblock.wilderwild.mixin.server;
+
+import com.mojang.datafixers.DataFixerBuilder;
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.datafixer.Schemas;
+import net.minecraft.datafixer.fix.BlockNameFix;
+import net.minecraft.datafixer.schema.IdentifierNormalizingSchema;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+import java.util.function.BiFunction;
+
+@Mixin(Schemas.class)
+@Debug(export = true)
+public class SchemasMixin {
+
+    @Shadow @Final private static BiFunction<Integer, Schema, Schema> EMPTY;
+
+    @Inject(method = "build", at = @At("TAIL"))
+    private static void build(DataFixerBuilder builder, CallbackInfo ci) {
+        Schema schema69420 = builder.addSchema(3546, EMPTY);
+        builder.addFixer(
+            BlockNameFix.create(
+                schema69420,
+                "white_dandelion block renamer",
+                id -> Objects.equals(IdentifierNormalizingSchema.normalize(id), "wilderwild:white_dandelion") ? "wilderwild:blooming_dandelion" : id
+        ));
+    }
+}
