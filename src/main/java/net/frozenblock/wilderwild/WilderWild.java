392b3d5bad91dd322b3d4b8528fbb3de02f7221f
Treetrain1 <treetrain1@gmail.com>
2022-07-29 20:46:48 -0500

    asdjhmn

--- src/main/java/net/frozenblock/wilderwild/WilderWild.java
+++ src/main/java/net/frozenblock/wilderwild/WilderWild.java
@@ -263,7 +263,7 @@ public class WilderWild implements ModInitializer {
 
     private static void applyDataFixes() {
         DataFixerBuilder builder = new DataFixerBuilder(SharedConstants.getGameVersion().getWorldVersion());
-        Schema schema = Schemas.getFixer().getSchema(3117);
+        Schema schema = Schemas.getFixer().getSchema(3097);
         wilderBlockItemRenamer(builder, schema, "white_dandelion", "seeding_dandelion");
         wilderBlockItemRenamer(builder, schema, "blooming_dandelion", "seeding_dandelion");
         wilderBlockRenamer(builder, schema, "potted_white_dandelion", "potted_seeding_dandelion");
