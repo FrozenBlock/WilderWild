7b1edcd17dd4cb4bfcdf5836c1e22da31fea5991
Treetrain1 <treetrain1@gmail.com>
2022-07-30 18:09:16 -0500

    idk how to do this lmao

--- src/main/java/net/frozenblock/wilderwild/misc/config/ModMenuIntegration.java
+++ src/main/java/net/frozenblock/wilderwild/misc/config/ModMenuIntegration.java
@@ -11,7 +11,7 @@ public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return screen -> AutoConfig.getConfigScreen(WilderWildConfig.class, screen).get();
+        return screen -> null;//AutoConfig.getConfigScreen(WilderWildConfig.class, screen).get();
     }
 
 }
