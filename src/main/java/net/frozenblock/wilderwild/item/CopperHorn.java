3b0f7e9b7cc2e56d21ef7bb61fe1d68bed063480
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-08-09 16:31:38 -0700

    attempting to get this thing working

--- src/main/java/net/frozenblock/wilderwild/item/CopperHorn.java
+++ src/main/java/net/frozenblock/wilderwild/item/CopperHorn.java
@@ -1,13 +1,16 @@
 package net.frozenblock.wilderwild.item;
 
 import net.frozenblock.wilderwild.WilderWild;
+import net.frozenblock.wilderwild.misc.server.EasyPacket;
 import net.frozenblock.wilderwild.registry.RegisterItems;
+import net.frozenblock.wilderwild.registry.RegisterSounds;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Instrument;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.tag.TagKey;
@@ -20,6 +23,8 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Iterator;
 import java.util.Optional;
@@ -79,14 +84,25 @@ public class CopperHorn extends Item {
         if (optional.isPresent()) {
             Instrument instrument = optional.get().value();
             user.setCurrentHand(hand);
-            SoundEvent soundEvent = instrument.soundEvent();
-            float range = instrument.range() / 16.0F;
-            float soundPitch = (float) Math.pow(2.0D, (Math.round(((user.getPitch() - 90F) * -1) * 0.06666667F) + this.shift) * 0.08333333F);
-            world.playSoundFromEntity(null, user, soundEvent, SoundCategory.RECORDS, range, soundPitch);
+
+            playSound(instrument, user, world, new CallbackInfo("playSound", true));
+
             return TypedActionResult.consume(itemStack);
         } else {
             return TypedActionResult.fail(itemStack);
         }
+    }
+
+    private void playSound(Instrument instrument, PlayerEntity user, World world, CallbackInfo info) {
+        info.cancel();
+        SoundEvent soundEvent = instrument.soundEvent();
+        float range = instrument.range() / 16.0F;
+        float soundPitch = (float) Math.pow(2.0D, (Math.round(((user.getPitch() - 90F) * -1) * 0.06666667F) + this.shift) * 0.08333333F);
+
+        if (!world.isClient) {
+            EasyPacket.createMovingRestrictionLoopingSound(world, user, soundEvent, SoundCategory.RECORDS, range, soundPitch, WilderWild.id("default"));
+        }
+        world.emitGameEvent(GameEvent.INSTRUMENT_PLAY, user.getPos(), GameEvent.Emitter.of(user));
     }
 
     @Override
