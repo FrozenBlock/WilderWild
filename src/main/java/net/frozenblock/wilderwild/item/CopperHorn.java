195b41ab0937ececbb37deef5bc2247456b83b74
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-09 17:16:47 -0400

    Update CopperHorn.java

--- src/main/java/net/frozenblock/wilderwild/item/CopperHorn.java
+++ src/main/java/net/frozenblock/wilderwild/item/CopperHorn.java
@@ -79,7 +79,7 @@ public class CopperHorn extends Item {
             user.setCurrentHand(hand);
             SoundEvent soundEvent = instrument.soundEvent();
             float range = instrument.range() / 16.0F;
-            int soundPitch = (int) Math.pow(2.0D, ((user.getPitch() - 90F) * -1) * 0.06666667F);
+            float soundPitch = (float) Math.pow(2.0D, ((((user.getPitch() - 90F) * -1) * 0.06666667F) - 12 ) * 0.08333333F);
             world.playSoundFromEntity(null, user, soundEvent, SoundCategory.RECORDS, range, soundPitch);
             return TypedActionResult.consume(itemStack);
         } else {
