2c5b7be1f79793245b71d2b4c30c6eaeb5845bac
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-08-09 20:04:09 -0700

    nope

--- src/main/java/net/frozenblock/wilderwild/misc/PVZGWSound/MovingSoundLoopWithRestriction.java
+++ src/main/java/net/frozenblock/wilderwild/misc/PVZGWSound/MovingSoundLoopWithRestriction.java
@@ -8,6 +8,7 @@ import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.entity.Entity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
 public class MovingSoundLoopWithRestriction extends MovingSoundInstance {
@@ -16,16 +17,14 @@ public class MovingSoundLoopWithRestriction extends MovingSoundInstance {
     private final RegisterMovingSoundRestrictions.LoopPredicate<?> predicate;
     private float distance = 0.0F;
 
-    float thisPitch;
-
     public MovingSoundLoopWithRestriction(Entity entity, SoundEvent sound, SoundCategory category, float volume, float pitch, RegisterMovingSoundRestrictions.LoopPredicate<?> predicate) {
         super(sound, category, SoundInstance.createRandom());
         this.entity = entity;
         this.repeat = true;
         this.repeatDelay = 0;
-        this.pitch = pitch;
-        thisPitch = this.pitch;
         this.volume = volume;
+        this.pitch = pitch;
+
         this.x = (float) entity.getX();
         this.y = (float) entity.getY();
         this.z = (float) entity.getZ();
@@ -41,7 +40,6 @@ public class MovingSoundLoopWithRestriction extends MovingSoundInstance {
     }
 
     public void tick() {
-        this.pitch = thisPitch;
         if (this.entity.isRemoved()) {
             this.setDone();
         } else {
