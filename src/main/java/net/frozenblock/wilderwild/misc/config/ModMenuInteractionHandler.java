356a5c9baaf1b2ffe6465ee4dd5fac25938ba05b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-13 20:29:07 -0400

    distant interactions

--- src/main/java/net/frozenblock/wilderwild/misc/config/ModMenuInteractionHandler.java
+++ src/main/java/net/frozenblock/wilderwild/misc/config/ModMenuInteractionHandler.java
@@ -4,55 +4,52 @@ import net.frozenblock.wilderwild.WilderWild;
 
 public class ModMenuInteractionHandler {
 
-    public static void loadConfig() {
-        WilderWildConfigManager.initializeConfig();
-    }
 
     public static boolean modDesertPlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_DESERT_PLACEMENT.getValue();
+            return MMDistantInteractions.modDesertPlacement();
         }
         return true;
     }
 
     public static boolean modBadlandsPlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_BADLANDS_PLACEMENT.getValue();
+            return MMDistantInteractions.modBadlandsPlacement();
         }
         return true;
     }
 
     public static boolean modJunglePlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_JUNGLE_PLACEMENT.getValue();
+            return MMDistantInteractions.modJunglePlacement();
         }
         return true;
     }
 
     public static boolean modSwampPlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_SWAMP_PLACEMENT.getValue();
+            return MMDistantInteractions.modSwampPlacement();
         }
         return true;
     }
 
     public static boolean modMangroveSwampPlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_MANGROVE_SWAMP_PLACEMENT.getValue();
+            return MMDistantInteractions.modMangroveSwampPlacement();
         }
         return true;
     }
 
     public static boolean modWindsweptSavannaPlacement() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MODIFY_WINDSWEPT_SAVANNA_PLACEMENT.getValue();
+            return MMDistantInteractions.modWindsweptSavannaPlacement();
         }
         return true;
     }
 
     public static boolean tendrilsEnabled() {
         if (WilderWild.hasModMenu()) {
-            return WilderWildConfig.MC_LIVE_SENSOR_TENDRILS.getValue();
+            return MMDistantInteractions.tendrilsEnabled();
         }
         return false;
     }
