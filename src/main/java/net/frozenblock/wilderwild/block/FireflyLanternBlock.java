7c7bb79dbc24c866f5a84c32c1dcf8d71070fb61
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-14 20:27:56 -0400

    Less Annoying Firefly Bottle Sounds

--- src/main/java/net/frozenblock/wilderwild/block/FireflyLanternBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/FireflyLanternBlock.java
@@ -90,7 +90,7 @@ public class FireflyLanternBlock extends BlockWithEntity implements Waterloggabl
                         }
                         player.getInventory().offerOrDrop(new ItemStack(Items.GLASS_BOTTLE));
                         world.setBlockState(pos, state.with(FIREFLIES, MathHelper.clamp(lantern.getFireflies().size(), 0, 4)));
-                        world.playSound(null, pos, RegisterSounds.ITEM_BOTTLE_PUT_IN_LANTERN_FIREFLY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                        world.playSound(null, pos, RegisterSounds.ITEM_BOTTLE_PUT_IN_LANTERN_FIREFLY, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.2f + 0.8f);
                         lantern.updateSync();
                         return ActionResult.SUCCESS;
                     }
@@ -103,7 +103,7 @@ public class FireflyLanternBlock extends BlockWithEntity implements Waterloggabl
                         if (optionalItem.isPresent()) {
                             item = optionalItem.get();
                         }
-                        world.playSound(null, pos, RegisterSounds.ITEM_BOTTLE_CATCH_FIREFLY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                        world.playSound(null, pos, RegisterSounds.ITEM_BOTTLE_CATCH_FIREFLY, SoundCategory.BLOCKS, 1.0F, world.random.nextFloat() * 0.2f + 0.8f);
                         if (!player.isCreative()) {
                             player.getStackInHand(hand).decrement(1);
                         }
