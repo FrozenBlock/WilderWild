49996cfbe6c5f2aae37ae07efb8ea40d4004a4a1
Soro335 <gabeonelove335@gmail.com>
2022-08-16 21:32:07 +0300

    Yes

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
@@ -6,10 +6,8 @@ import net.minecraft.item.FoodComponent;
 
 public class RegisterFood {
 
-    public static final FoodComponent PRICKLY_PEAR;
-
-    static {
+    public static final FoodComponent BAOBAB_NUT = new FoodComponent.Builder().hunger(1).saturationModifier(0.2F).statusEffect(new  StatusEffectInstance(StatusEffects.REGENERATION, 100, 0), 0.5F).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 120, 0), 0.7F).statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 150, 0), 0.15F).build();
         /** Goal: Make Prickly pear deal normal damage, playing the Sweet Berry Thorn Sound **/
-        PRICKLY_PEAR = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.3F).statusEffect(new StatusEffectInstance(StatusEffects.WITHER, 20, 1), 1.0F).build();
+    public static final FoodComponent PRICKLY_PEAR = new FoodComponent.Builder().hunger(2).saturationModifier(0.3F).statusEffect(new StatusEffectInstance(StatusEffects.WITHER, 20, 1), 1.0F).build();
+
     }
-}
--- src/main/java/net/frozenblock/wilderwild/registry/RegisterItems.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterItems.java
@@ -31,7 +31,7 @@ public final class RegisterItems {
     public static final Item CYPRESS_SIGN = new SignItem(new FabricItemSettings().group(ItemGroup.DECORATIONS).maxCount(16),
             RegisterBlocks.CYPRESS_SIGN_BLOCK, RegisterBlocks.CYPRESS_WALL_SIGN);
 
-    public static final Item BAOBAB_NUT = new BlockItem(RegisterBlocks.BAOBAB_NUT, (new Item.Settings()).group(ItemGroup.DECORATIONS).food(FoodComponents.SWEET_BERRIES));
+    public static final Item BAOBAB_NUT = new BlockItem(RegisterBlocks.BAOBAB_NUT, (new Item.Settings()).group(ItemGroup.DECORATIONS).food(RegisterFood.BAOBAB_NUT));
     public static final Item PRICKLY_PEAR = new Item((new Item.Settings()).group(ItemGroup.FOOD).food(RegisterFood.PRICKLY_PEAR));
 
     public static final Item FIREFLY_BOTTLE = new FireflyBottle(new FabricItemSettings().group(ItemGroup.MISC).maxCount(32), "on");
