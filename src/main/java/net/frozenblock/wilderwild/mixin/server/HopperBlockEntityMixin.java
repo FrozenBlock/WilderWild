4008e0d7101009e5191fcb34429eba71eec7d9ee
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-16 15:36:22 -0400

    why the frick won't this WORK

--- src/main/java/net/frozenblock/wilderwild/mixin/server/HopperBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/HopperBlockEntityMixin.java
@@ -4,7 +4,9 @@ import net.frozenblock.wilderwild.block.entity.StoneChestBlockEntity;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,6 +29,15 @@ public class HopperBlockEntityMixin {
         if (inv instanceof StoneChestBlockEntity) {
             info.cancel();
             info.setReturnValue(false);
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "getInventoryAt(Lnet/minecraft/world/World;DDD)Lnet/minecraft/inventory/Inventory;", cancellable = true)
+    private static void getInventoryAt(World world, double x, double y, double z, CallbackInfoReturnable<Inventory> info) {
+        Inventory inv = info.getReturnValue();
+        if (world.getBlockEntity(new BlockPos(x, y, z)) instanceof StoneChestBlockEntity) {
+            info.setReturnValue(null);
+            info.cancel();
         }
     }
 
