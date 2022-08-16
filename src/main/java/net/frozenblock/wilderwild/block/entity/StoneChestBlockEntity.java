585f18f3a04e8dca27696a9aa6f25f329d834de5
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-16 16:33:25 -0400

    what is going on

--- src/main/java/net/frozenblock/wilderwild/block/entity/StoneChestBlockEntity.java
+++ src/main/java/net/frozenblock/wilderwild/block/entity/StoneChestBlockEntity.java
@@ -88,11 +88,6 @@ public class StoneChestBlockEntity extends ChestBlockEntity {
                 if (!blockEntity.closing) {
                     blockEntity.closing = true;
                     playSound(world, pos, state, RegisterSounds.BLOCK_STONE_CHEST_CLOSE_START);
-                    for (PlayerEntity player : blockEntity.getInRangeViewers(world, pos)) {
-                        if (player instanceof ServerPlayerEntity serverPlayer) {
-                            serverPlayer.closeHandledScreen();
-                        }
-                    }
                 }
                 if (blockEntity.openProgress <= 0F) {
                     playSound(world, pos, state, RegisterSounds.BLOCK_STONE_CHEST_SLAM);
@@ -132,22 +127,6 @@ public class StoneChestBlockEntity extends ChestBlockEntity {
         if (isLeft(state)) {
             blockEntity.syncLidValues(world, pos, state);
         }
-    }
-
-    public List<PlayerEntity> getInRangeViewers(World world, BlockPos pos) {
-        int i = pos.getX();
-        int j = pos.getY();
-        int k = pos.getZ();
-        Box box = new Box((float) i - 5.0f, (float) j - 5.0f, (float) k - 5.0f, (float) (i + 1) + 5.0f, (float) (j + 1) + 5.0f, (float) (k + 1) + 5.0f);
-        return world.getEntitiesByType(TypeFilter.instanceOf(PlayerEntity.class), box, this::isPlayerViewing);
-    }
-
-    public boolean isPlayerViewing(PlayerEntity player) {
-        if (player.currentScreenHandler instanceof GenericContainerScreenHandler) {
-            Inventory inventory = ((GenericContainerScreenHandler) player.currentScreenHandler).getInventory();
-            return inventory == StoneChestBlockEntity.this || inventory instanceof DoubleInventory && ((DoubleInventory) inventory).isPart(StoneChestBlockEntity.this);
-        }
-        return false;
     }
 
     public boolean canPlayerUse(PlayerEntity player) {
