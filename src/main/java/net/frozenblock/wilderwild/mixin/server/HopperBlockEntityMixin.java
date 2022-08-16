ad87cfa4d5b42a1fe774c8139f73c02c94155d11
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-16 14:52:36 -0400

    Hoppers shouldn't be able to interact with Stone Chests

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/HopperBlockEntityMixin.java
@@ -0,0 +1,26 @@
+package net.frozenblock.wilderwild.mixin.server;
+
+import net.frozenblock.wilderwild.block.entity.StoneChestBlockEntity;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HopperBlockEntity.class)
+public class HopperBlockEntityMixin {
+
+    @Inject(at = @At("RETURN"), method = "getInventoryAt", cancellable = true)
+    private static void getInventoryAt(World world, double x, double y, double z, CallbackInfoReturnable<Inventory> info) {
+        Inventory inv = info.getReturnValue();
+        if (inv != null) {
+            if (inv instanceof StoneChestBlockEntity) {
+                info.setReturnValue(null);
+                info.cancel();
+            }
+        }
+    }
+
+}
