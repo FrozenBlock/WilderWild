6b87c4a6d269db423076f675ee789b945d8a4245
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-17 15:40:38 -0400

    Faster..?

--- src/main/java/net/frozenblock/wilderwild/mixin/server/ItemStackMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/ItemStackMixin.java
@@ -2,6 +2,7 @@ package net.frozenblock.wilderwild.mixin.server;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,8 +15,11 @@ public class ItemStackMixin {
     @Inject(at = @At("HEAD"), method = "inventoryTick")
     public void inventoryTick(World world, Entity entity, int slot, boolean selected, CallbackInfo info) {
         ItemStack stack = ItemStack.class.cast(this);
-        if (stack.getOrCreateNbt().get("wilderwild_is_ancient") != null) {
-            stack.getOrCreateNbt().remove("wilderwild_is_ancient");
+        NbtCompound nbt = stack.getNbt();
+        if (nbt != null) {
+            if (nbt.get("wilderwild_is_ancient") != null) {
+                nbt.remove("wilderwild_is_ancient");
+            }
         }
     }
 
