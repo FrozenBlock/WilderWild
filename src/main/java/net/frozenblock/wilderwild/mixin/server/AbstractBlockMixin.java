11a110a83be08490493510bce88377fa47ae8d33
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-17 16:05:57 -0400

    there now we should have mud farms with dripstone

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/AbstractBlockMixin.java
@@ -0,0 +1,23 @@
+package net.frozenblock.wilderwild.mixin.server;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(AbstractBlock.class)
+public class AbstractBlockMixin {
+
+    @Inject(method = "scheduledTick", at = @At("HEAD"))
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.isOf(Blocks.DIRT)) {
+            world.setBlockState(pos, Blocks.MUD.getDefaultState());
+        }
+    }
+
+}
