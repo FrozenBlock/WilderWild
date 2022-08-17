62e1a3664d26c3aa742b1a75d2d64a5f3e229efd
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-08-16 19:30:54 -0700

    added another stone chest lift sound variant

--- src/main/java/net/frozenblock/wilderwild/block/StoneChestBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/StoneChestBlock.java
@@ -81,7 +81,7 @@ public class StoneChestBlock extends ChestBlock {
                         stoneEntity.openProgress = !player.isCreative() ? stoneEntity.openProgress + 0.025F : stoneEntity.openProgress + 0.05F;
                     }
                     stoneEntity.stillLidTicks = (int) (Math.max((stoneEntity.openProgress), 0.2) * 120);
-                    StoneChestBlockEntity.playSound(world, pos, state, first ? RegisterSounds.BLOCK_STONE_CHEST_OPEN : RegisterSounds.BLOCK_STONE_CHEST_LIFT, first ? 0.5F : 0.2F);
+                    StoneChestBlockEntity.playSound(world, pos, state, first ? RegisterSounds.BLOCK_STONE_CHEST_OPEN : RegisterSounds.BLOCK_STONE_CHEST_LIFT, 0.5F);
                     world.emitGameEvent(player, GameEvent.CONTAINER_OPEN, pos);
                     stoneEntity.updateSync();
                 }
