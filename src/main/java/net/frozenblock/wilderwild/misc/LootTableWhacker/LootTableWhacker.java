7c8bf8f0bedfe8d44701e960c3a408a8e02cf3e0
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-20 15:27:14 -0400

    Update LootTableWhacker.java

--- src/main/java/net/frozenblock/wilderwild/misc/LootTableWhacker/LootTableWhacker.java
+++ src/main/java/net/frozenblock/wilderwild/misc/LootTableWhacker/LootTableWhacker.java
@@ -1,5 +1,6 @@
 package net.frozenblock.wilderwild.misc.LootTableWhacker;
 
+import net.frozenblock.wilderwild.WilderWild;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
@@ -28,6 +29,7 @@ public class LootTableWhacker extends Item {
                 ResourceLocation location = new ResourceLocation(id.substring(0, id.lastIndexOf(":") - 1), id.substring(id.lastIndexOf(":" + 1)));
                 if (level.getBlockEntity(blockPos) instanceof RandomizableContainerBlockEntity loot) {
                     loot.lootTable = location;
+                    WilderWild.log(location, true);
                 }
             }
         }
