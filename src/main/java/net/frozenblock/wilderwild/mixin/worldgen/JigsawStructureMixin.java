a6fb7ee6d034d3a1ea886c52079c71a176368c57
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-23 20:30:19 -0400

    help wanted

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/worldgen/JigsawStructureMixin.java
@@ -0,0 +1,79 @@
+package net.frozenblock.wilderwild.mixin.worldgen;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Mixin(JigsawStructure.class)
+public class JigsawStructureMixin {
+    @Shadow @Final @Mutable
+    private static int MAX_TOTAL_STRUCTURE_RANGE;
+    @Shadow @Final @Mutable
+    private static Codec<JigsawStructure> CODEC;
+
+    @Shadow @Final
+    private Holder<StructureTemplatePool> startPool;
+    @Shadow @Final
+    private Optional<ResourceLocation> startJigsawName;
+    @Shadow @Final
+    private int maxDepth;
+    @Shadow @Final
+    private HeightProvider startHeight;
+    @Shadow @Final
+    private boolean useExpansionHack;
+    @Shadow @Final
+    private Optional<Heightmap.Types> projectStartToHeightmap;
+    @Shadow @Final
+    private int maxDistanceFromCenter;
+
+    @Inject(method = "<clinit>", at = @At("HEAD"))
+    public void classInit(CallbackInfo info) {
+        MAX_TOTAL_STRUCTURE_RANGE = 256;
+        CODEC = RecordCodecBuilder.mapCodec((instance) -> {
+            return instance.group(Structure.StructureSettings.CODEC.forGetter(structure -> ((Structure)structure).settings), StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).startPool;
+            }), ResourceLocation.CODEC.optionalFieldOf("start_jigsaw_name").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).startJigsawName;
+            }), Codec.intRange(0, 7).fieldOf("size").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).maxDepth;
+            }), HeightProvider.CODEC.fieldOf("start_height").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).startHeight;
+            }), Codec.BOOL.fieldOf("use_expansion_hack").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).useExpansionHack;
+            }), Heightmap.Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).projectStartToHeightmap;
+            }), Codec.intRange(1, 256).fieldOf("max_distance_from_center").forGetter((jigsawStructure) -> {
+                return ((JigsawStructure)jigsawStructure).maxDistanceFromCenter;
+            })).apply(instance, JigsawStructure::new);
+        }).flatXmap(verifyRange(), verifyRange()).codec();
+
+    }
+
+    private static Function<JigsawStructure, DataResult<JigsawStructure>> verifyRange() {
+        return (jigsawStructure) -> {
+
+            int i = switch (jigsawStructure.terrainAdaptation()) {
+                case NONE -> 0;
+                case BURY, BEARD_THIN, BEARD_BOX -> 12;
+            };
+            return jigsawStructure.maxDistanceFromCenter + i > 256 ? DataResult.error("Structure size including terrain adaptation must not exceed 256") : DataResult.success(jigsawStructure);
+        };
+    }
+}
---
+++ src/main/java/net/frozenblock/wilderwild/mixin/worldgen/StructuresMixin.java
@@ -0,0 +1,64 @@
+package net.frozenblock.wilderwild.mixin.worldgen;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.data.worldgen.AncientCityStructurePieces;
+import net.minecraft.data.worldgen.Structures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.util.random.WeightedRandomList;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.heightproviders.ConstantHeight;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSpawnOverride;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Mixin(Structures.class)
+public class StructuresMixin {
+
+    @Shadow @Final @Mutable
+    private static Holder<Structure> ANCIENT_CITY = register(BuiltinStructures.ANCIENT_CITY, new JigsawStructure(structure(BiomeTags.HAS_ANCIENT_CITY, Arrays.stream(MobCategory.values()).collect(Collectors.toMap(mobCategory -> mobCategory, mobCategory -> new StructureSpawnOverride(StructureSpawnOverride.BoundingBoxType.STRUCTURE, WeightedRandomList.create()))), GenerationStep.Decoration.UNDERGROUND_DECORATION, TerrainAdjustment.BEARD_BOX), AncientCityStructurePieces.START, Optional.of(new ResourceLocation("city_anchor")), 7, ConstantHeight.of(VerticalAnchor.absolute(-27)), false, Optional.empty(), 116));
+
+
+    @Inject(method = "<clinit>", at = @At("HEAD"))
+    public void init(CallbackInfo info) {
+        ANCIENT_CITY = register(BuiltinStructures.ANCIENT_CITY, new JigsawStructure(structure(BiomeTags.HAS_ANCIENT_CITY, Arrays.stream(MobCategory.values()).collect(Collectors.toMap
+                (mobCategory -> mobCategory, mobCategory -> new StructureSpawnOverride(StructureSpawnOverride.BoundingBoxType.STRUCTURE, WeightedRandomList.create()))),
+                GenerationStep.Decoration.UNDERGROUND_DECORATION, TerrainAdjustment.BEARD_BOX),
+                AncientCityStructurePieces.START, Optional.of(new ResourceLocation("city_anchor")),
+                7, ConstantHeight.of(VerticalAnchor.absolute(-27)), true, Optional.empty(), 250));
+    }
+
+    @Shadow
+    private static Holder<Structure> register(ResourceKey<Structure> resourceKey, Structure structure) {
+        return BuiltinRegistries.register(BuiltinRegistries.STRUCTURES, resourceKey, structure);
+    }
+    @Shadow
+    private static Structure.StructureSettings structure(TagKey<Biome> tagKey, Map<MobCategory, StructureSpawnOverride> map, GenerationStep.Decoration decoration, TerrainAdjustment terrainAdjustment) {
+        return new Structure.StructureSettings(biomes(tagKey), map, decoration, terrainAdjustment);
+    }
+    @Shadow
+    private static HolderSet<Biome> biomes(TagKey<Biome> tagKey) {
+        return BuiltinRegistries.BIOME.getOrCreateTag(tagKey);
+    }
+}
