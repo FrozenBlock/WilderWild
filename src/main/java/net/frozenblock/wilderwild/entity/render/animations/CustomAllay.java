9263db1138b49ef6a55e92b5ebfc8937f1e65936
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-08-24 14:21:55 -0700

    bedrock allay dance

---
+++ src/main/java/net/frozenblock/wilderwild/entity/render/animations/CustomAllay.java
@@ -0,0 +1,9 @@
+package net.frozenblock.wilderwild.entity.render.animations;
+
+import net.minecraft.world.entity.AnimationState;
+
+public interface CustomAllay {
+
+    AnimationState getDancingAnimationState();
+
+}
