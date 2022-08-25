e114908a6a679809e7cf43514b2ae6fa58e1bc76
Treetrain1 <treetrain1@gmail.com>
2022-08-24 21:58:24 -0500

    better instrument mixin

--- src/main/java/net/frozenblock/wilderwild/mixin/server/InstrumentItemMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/InstrumentItemMixin.java
@@ -2,11 +2,11 @@ package net.frozenblock.wilderwild.mixin.server;
 
 import net.frozenblock.lib.sound.FrozenSoundPackets;
 import net.frozenblock.wilderwild.WilderWild;
-import net.minecraft.core.Holder;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.InstrumentItem;
@@ -14,40 +14,28 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Optional;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(InstrumentItem.class)
-public abstract class InstrumentItemMixin {
+public class InstrumentItemMixin {
 
-    @Shadow
-    protected abstract Optional<Holder<Instrument>> getInstrument(ItemStack stack);
-
-    @Inject(method = "use", at = @At("RETURN"))
-    private void use(Level world, Player user, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        ItemStack itemStack = user.getItemInHand(hand);
-        Optional<Holder<Instrument>> optional = this.getInstrument(itemStack);
-        if (optional.isPresent()) {
-            InstrumentItem goatHorn = InstrumentItem.class.cast(this);
-            user.getCooldowns().addCooldown(goatHorn, 0);
-        }
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemCooldowns;addCooldown(Lnet/minecraft/world/item/Item;I)V", shift = At.Shift.AFTER))
+    private void noCooldown(Level level, Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+        player.getCooldowns().removeCooldown(InstrumentItem.class.cast(this));
     }
 
 
-    @Inject(method = "play", at = @At("HEAD"), cancellable = true)
-    private static void play(Level world, Player player, Instrument instrument, CallbackInfo info) {
-        info.cancel();
-        SoundEvent soundEvent = instrument.soundEvent();
-        float f = instrument.range() / 16.0F;
+    @Redirect(method = "play", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
+    private static void play(Level world, Player player, Entity entity, SoundEvent soundEvent, SoundSource soundSource, float f, float g) {
         if (!world.isClientSide) {
-            FrozenSoundPackets.createMovingRestrictionSound(world, player, soundEvent, SoundSource.RECORDS, f, 1.0F, WilderWild.id("horn"));
+            FrozenSoundPackets.createMovingRestrictionSound(world, player, soundEvent, soundSource, f, g, WilderWild.id("horn"));
         }
-        world.gameEvent(GameEvent.INSTRUMENT_PLAY, player.position(), GameEvent.Context.of(player));
     }
 
 }
