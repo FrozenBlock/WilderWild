b07f549e4764110593f3266669b50abb319372e6
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-24 21:12:39 -0400

    TESTING

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/worldgen/ChunkGeneratorMixin.java
@@ -0,0 +1,68 @@
+package net.frozenblock.wilderwild.mixin.worldgen;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.frozenblock.wilderwild.registry.RegisterStructures;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import net.minecraft.ReportedException;
+import net.minecraft.core.Registry;
+import net.minecraft.core.SectionPos;
+import net.minecraft.network.protocol.game.DebugPackets;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Mixin(ChunkGenerator.class)
+public class ChunkGeneratorMixin {
+
+    @Overwrite
+    public void createReferences(WorldGenLevel worldGenLevel, StructureManager structureManager, ChunkAccess chunkAccess) {
+        int i = 16;
+        ChunkPos chunkPos = chunkAccess.getPos();
+        int j = chunkPos.x;
+        int k = chunkPos.z;
+        int l = chunkPos.getMinBlockX();
+        int m = chunkPos.getMinBlockZ();
+        SectionPos sectionPos = SectionPos.bottomOf(chunkAccess);
+        for (int n = j - i; n <= j + i; ++n) {
+            for (int o = k - i; o <= k + i; ++o) {
+                long p = ChunkPos.asLong(n, o);
+                for (StructureStart structureStart : worldGenLevel.getChunk(n, o).getAllStarts().values()) {
+                    try {
+                        if (!structureStart.isValid() || !structureStart.getBoundingBox().intersects(l, m, l + 15, m + 15)) continue;
+                        structureManager.addReferenceForStructure(sectionPos, structureStart.getStructure(), p, chunkAccess);
+                        DebugPackets.sendStructurePacket(worldGenLevel, structureStart);
+                    }
+                    catch (Exception exception) {
+                        CrashReport crashReport = CrashReport.forThrowable(exception, "Generating structure reference");
+                        CrashReportCategory crashReportCategory = crashReport.addCategory("Structure");
+                        Optional<Registry<Structure>> optional = (Optional<Registry<Structure>>) worldGenLevel.registryAccess().registry(Registry.STRUCTURE_REGISTRY);
+                        crashReportCategory.setDetail("Id", () -> optional.map(registry -> registry.getKey(structureStart.getStructure()).toString()).orElse("UNKNOWN"));
+                        crashReportCategory.setDetail("Name", () -> Registry.STRUCTURE_TYPES.getKey(structureStart.getStructure().type()).toString());
+                        crashReportCategory.setDetail("Class", () -> structureStart.getStructure().getClass().getCanonicalName());
+                        throw new ReportedException(crashReport);
+                    }
+                }
+            }
+        }
+    }
+
+}
