c051c1670b61f676d838f00c02373736fe56ba01
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-08-26 09:56:16 -0700

    maybe fix shrieker gargling

--- src/main/java/net/frozenblock/wilderwild/mixin/client/LevelRendererMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/LevelRendererMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ShriekParticleOption;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SculkShriekerBlock;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -23,27 +22,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(LevelRenderer.class)
-public class WorldRendererMixin {
+public class LevelRendererMixin {
 
     @Shadow
     private @Nullable ClientLevel level;
 
     @Inject(method = "globalLevelEvent", at = @At("HEAD"), cancellable = true)
-    private void globalLevelEvent(int eventId, BlockPos pos, int data, CallbackInfo ci) {
-        if (eventId == 3007) {
+    private void globalLevelEvent(int eventId, BlockPos blockPos, int data, CallbackInfo ci) {
+        switch (eventId) {
+            case 3007:
             assert this.level != null;
-            for (int j = 0; j < 10; ++j) {
+            for (int l = 0; l < 10; ++l) {
                 this.level
                         .addParticle(
-                                new ShriekParticleOption(j * 5), false, (double) pos.getX() + 0.5, (double) pos.getY() + SculkShriekerBlock.TOP_Y, (double) pos.getZ() + 0.5, 0.0, 0.0, 0.0
+                                new ShriekParticleOption(l * 5), false, (double) blockPos.getX() + 0.5, (double) blockPos.getY() + SculkShriekerBlock.TOP_Y, (double) blockPos.getZ() + 0.5, 0.0, 0.0, 0.0
                         );
             }
-            if ((this.level.getBlockState(pos).getValue(BlockStateProperties.WATERLOGGED) || this.level.getBlockState(pos.above()).getBlock() == Blocks.WATER) && ModMenuInteractionHandler.shriekerGargling()) {
+            if (ModMenuInteractionHandler.shriekerGargling() && this.level.getBlockState(blockPos).getValue(BlockStateProperties.WATERLOGGED)) {
                 this.level
                         .playLocalSound(
-                                (double) pos.getX() + 0.5,
-                                (double) pos.getY() + SculkShriekerBlock.TOP_Y,
-                                (double) pos.getZ() + 0.5,
+                                (double) blockPos.getX() + 0.5D,
+                                (double) blockPos.getY() + SculkShriekerBlock.TOP_Y,
+                                (double) blockPos.getZ() + 0.5D,
                                 RegisterSounds.BLOCK_SCULK_SHRIEKER_GARGLE,
                                 SoundSource.BLOCKS,
                                 2.0F,
@@ -53,9 +53,9 @@ public class WorldRendererMixin {
             } else {
                 this.level
                         .playLocalSound(
-                                (double) pos.getX() + 0.5,
-                                (double) pos.getY() + SculkShriekerBlock.TOP_Y,
-                                (double) pos.getZ() + 0.5,
+                                (double) blockPos.getX() + 0.5,
+                                (double) blockPos.getY() + SculkShriekerBlock.TOP_Y,
+                                (double) blockPos.getZ() + 0.5,
                                 SoundEvents.SCULK_SHRIEKER_SHRIEK,
                                 SoundSource.BLOCKS,
                                 2.0F,
