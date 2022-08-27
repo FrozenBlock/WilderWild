862bbb7580eebe1c25467e8593f394e78db480c2
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-27 13:43:39 -0400

    render

--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
@@ -63,7 +63,7 @@ public class JellyfishModel<T extends Jellyfish> extends HierarchicalModel<T> {
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int i, int j, float f, float g, float h, float k) {
-
+        this.root().render(poseStack, vertexConsumer, i, j, f, g, h, k);
     }
 
     @Override
