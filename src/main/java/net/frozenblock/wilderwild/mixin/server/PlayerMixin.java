60f3ed6de6d1ffddd80e1d6268d31bee64efb14b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-26 20:39:35 -0400

    Better Hurt Sound Mixin

--- src/main/java/net/frozenblock/wilderwild/mixin/server/PlayerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/PlayerMixin.java
@@ -2,7 +2,6 @@ package net.frozenblock.wilderwild.mixin.server;
 
 import net.frozenblock.wilderwild.registry.RegisterSounds;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,31 +9,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.minecraft.world.damagesource.DamageSource.*;
+import static net.minecraft.world.damagesource.DamageSource.CACTUS;
 
 @Mixin(Player.class)
 public class PlayerMixin {
-    private final Player player = Player.class.cast(this);
-    @Inject(method = "getHurtSound", at = @At("HEAD"))
+
+    @Inject(method = "getHurtSound", at = @At("HEAD"), cancellable = true)
     public void getHurtSound(DamageSource damageSource, CallbackInfoReturnable<SoundEvent> info) {
-
-        boolean unique = (damageSource == ON_FIRE || damageSource == DROWN || damageSource == SWEET_BERRY_BUSH || damageSource == CACTUS || damageSource == FREEZE);
-
-        if (unique) {
-            if (damageSource == ON_FIRE) {
-                player.playSound(SoundEvents.PLAYER_HURT_ON_FIRE); //ON FIRE
-            } else if (damageSource == DROWN) {
-                player.playSound(SoundEvents.PLAYER_HURT_DROWN); //DROWN
-            } else if (damageSource == SWEET_BERRY_BUSH) {
-                player.playSound(SoundEvents.PLAYER_HURT_SWEET_BERRY_BUSH); //SWEET BERRY BUSH
-            } else if (damageSource == CACTUS) {
-                player.playSound(RegisterSounds.PLAYER_HURT_CACTUS); //CACTUS
-            } else if (damageSource == FREEZE) {
-                player.playSound(SoundEvents.PLAYER_HURT_FREEZE); //FREEZE
-            }
+        if (damageSource == CACTUS) {
+            info.cancel();
+            info.setReturnValue(RegisterSounds.PLAYER_HURT_CACTUS);
         }
-            else {
-                player.playSound(SoundEvents.PLAYER_HURT); //UNASSIGNED
-            }
     }
+    
 }
