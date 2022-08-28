679ea6b51e62fd9c98c91ca523b3d44877c10760
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-28 19:06:56 -0400

    welp targets don't work

--- src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
@@ -329,10 +329,10 @@ public class Jellyfish extends AbstractFish {
     @Override
     protected void registerGoals() {
         //super.registerGoals();
-        //this.goalSelector.addGoal(0, new PanicGoal(this, 1.25));
-        //this.goalSelector.addGoal(2, new AvoidEntityGoal<>(this, Player.class, 8.0f, 1.6, 1.4, EntitySelector.NO_SPECTATORS::test));
+        this.goalSelector.addGoal(0, new PanicGoal(this, 1.25));
+        this.goalSelector.addGoal(2, new AvoidEntityGoal<>(this, Player.class, 8.0f, 1.6, 1.4, EntitySelector.NO_SPECTATORS::test));
         this.goalSelector.addGoal(0, new JellySwimGoal(this));
-        this.goalSelector.addGoal(2, new JellyToTargetGoal(this));
+        //this.goalSelector.addGoal(2, new JellyToTargetGoal(this));
     }
 
     static class JellySwimGoal
