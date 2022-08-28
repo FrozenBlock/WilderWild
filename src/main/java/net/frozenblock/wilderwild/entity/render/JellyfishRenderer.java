bd50b2c69abd7804efded272acdb27bdf7a0c97e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-28 15:33:34 -0400

    better..?

--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
@@ -137,8 +137,8 @@ public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jel
         JellyfishModel<Jellyfish> model = this.getModel();
         model.xRot = Mth.lerp(h, jelly.xRot1, jelly.xBodyRot);
         model.zRot = Mth.lerp(h, jelly.zRot1, jelly.zBodyRot);
-        model.tentXRot = Mth.lerp(h, jelly.xRot3, jelly.xRot2);
-        model.tentZRot = Mth.lerp(h, jelly.zRot3, jelly.zRot2);
+        model.tentXRot = Mth.lerp(h, jelly.xRot4, jelly.xRot3);
+        model.tentZRot = Mth.lerp(h, jelly.zRot4, jelly.zRot3);
     }
 
     @Override
