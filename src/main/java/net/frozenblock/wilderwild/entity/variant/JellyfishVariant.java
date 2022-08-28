43852000fee38f9a2e7a853644558be0b01b2c87
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-28 19:41:34 -0400

    fix

--- src/main/java/net/frozenblock/wilderwild/entity/variant/JellyfishVariant.java
+++ src/main/java/net/frozenblock/wilderwild/entity/variant/JellyfishVariant.java
@@ -6,7 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public record JellyfishVariant(ResourceLocation texture) {
     public static final JellyfishVariant PALE_BLUE = JellyfishVariant.register("pale_blue", "textures/entity/jellyfish/pale_blue.png");
-    public static final JellyfishVariant PINK = JellyfishVariant.register("pale_blue", "textures/entity/jellyfish/pink.png");
+    public static final JellyfishVariant PINK = JellyfishVariant.register("pink", "textures/entity/jellyfish/pink.png");
 
     private static JellyfishVariant register(String string, String string2) {
         return Registry.register(WilderWild.JELLYFISH_VARIANTS, string, new JellyfishVariant(WilderWild.id(string2)));
