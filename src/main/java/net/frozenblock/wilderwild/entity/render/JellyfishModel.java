998616f136685736af0ca369c316dd043d2b267b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-08-28 15:35:40 -0400

    oh

--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
@@ -129,7 +129,7 @@ public class JellyfishModel<T extends Jellyfish> extends HierarchicalModel<T> {
         this.body.zScale = xZScale;
         float yCos = 1.25F + (cos * 0.75F);
         this.body.yScale = yCos;
-        this.body.y = -13.5F - (yCos * 2);
+        this.body.y = -(yCos * 2);
     }
 
     @Override
