9d214187e5a451e2fa562b9b7dff9c7a1f046ad5
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-01 15:17:12 -0400

    just a cutout for now

--- src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
+++ src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
@@ -103,7 +103,7 @@ public final class WilderWildClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.HOLLOWED_OAK_LOG, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.HOLLOWED_SPRUCE_LOG, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.MESOGLEA, RenderType.translucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.NEMATOCYST, translucentCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.NEMATOCYST, RenderType.cutout());
 
         ClientSpriteRegistryCallback.event(InventoryMenu.BLOCK_ATLAS).register((atlasTexture, registry) -> {
             registry.register(WilderWild.id("particle/floating_sculk_bubble_0"));
