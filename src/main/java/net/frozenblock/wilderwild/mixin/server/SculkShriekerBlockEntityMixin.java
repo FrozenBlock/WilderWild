76a63d525b6d24c1a892f0e42c7c8ebb51fa7512
Merp <27739930+MerpisMe@users.noreply.github.com>
2022-09-02 11:17:56 -0700

    can i please get some help with this @AViewFromTheTop @Treetrain1

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SculkShriekerBlockEntityMixin.java
@@ -51,7 +51,7 @@ public class SculkShriekerBlockEntityMixin {
         }
         if (entity.getBlockState().getValue(BlockStateProperties.WATERLOGGED)) {
             if (entity.getLevel() instanceof ServerLevel server) {
-                for (var bubbles = 0; bubbles < 50; ++bubbles) {
+                if (entity.getBlockState().getValue(BlockStateProperties.SHRIEKING) == TRUE) {
                     EasyPacket.EasyFloatingSculkBubblePacket.createParticle(server, Vec3.atCenterOf(entity.getBlockPos()), Math.random() > 0.7 ? 1 : 0, 20 + WilderWild.random().nextInt(40), 0.05, server.random.nextIntBetweenInclusive(1, 3));
                 }
             }
