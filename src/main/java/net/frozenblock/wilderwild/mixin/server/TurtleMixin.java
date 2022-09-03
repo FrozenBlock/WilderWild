55317cfc4696f1cc0ed2311bb178c780ec12410f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-03 11:13:48 -0400

    i want to think straight please

--- src/main/java/net/frozenblock/wilderwild/mixin/server/TurtleMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/TurtleMixin.java
@@ -6,13 +6,15 @@ import net.minecraft.world.entity.animal.Turtle;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Turtle.class)
 public class TurtleMixin {
 
     @Inject(method = "registerGoals", at = @At("TAIL"))
-    public void registerGoals() {
+    public void registerGoals(CallbackInfo info) {
         Turtle turtle = Turtle.class.cast(this);
         turtle.targetSelector.addGoal(10, new NearestAttackableTargetGoal<Jellyfish>(turtle, Jellyfish.class, false));
     }
+    
 }
