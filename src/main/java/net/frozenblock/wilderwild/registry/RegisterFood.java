d9972889656f15b93a629607c2037275947bb691
Xfrtrex <80720298+Xfrtrex@users.noreply.github.com>
2022-09-03 09:25:01 -0700

    added back unfinished prickly pear (help why crash)

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
@@ -5,7 +5,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class RegisterFood {
-
     public static final FoodProperties BAOBAB_NUT = new FoodProperties.Builder().nutrition(1).alwaysEat().saturationMod(0.2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 120, 0), 0.5F).effect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 140, 0), 0.7F).effect(new MobEffectInstance(MobEffects.HEALTH_BOOST, 250, 0), 0.15F).build();
     public static final FoodProperties PRICKLY_PEAR = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build();
 
