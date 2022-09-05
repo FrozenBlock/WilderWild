e7e87b24054c40e66bdeb4a50e92af30dab2688e
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-09-05 12:17:12 -0700

    why is the cactus damage sound not workinggggggg

--- src/main/java/net/frozenblock/wilderwild/mixin/server/PlayerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/PlayerMixin.java
@@ -10,15 +10,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.minecraft.world.damagesource.DamageSource.CACTUS;
+import static net.minecraft.world.damagesource.DamageSource.GENERIC;
 
 @Mixin(Player.class)
 public class PlayerMixin {
 
     @Inject(method = "getHurtSound", at = @At("HEAD"), cancellable = true)
     public void getHurtSound(DamageSource damageSource, CallbackInfoReturnable<SoundEvent> info) {
+        System.out.println(damageSource);
         if (damageSource == CACTUS) {
-            info.cancel();
+            System.out.println("YOU ARE TAKING DAMAGE FROM A CACTUS;" + damageSource);
             info.setReturnValue(RegisterSounds.PLAYER_HURT_CACTUS);
+            info.cancel();
         }
     }
 
