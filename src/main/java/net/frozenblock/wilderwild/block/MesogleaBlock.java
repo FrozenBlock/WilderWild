1b61d1ff142cb4680ad34a918b2029e0faaa2d90
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-04 20:56:40 -0400

    Merp Is A Genius

--- src/main/java/net/frozenblock/wilderwild/block/MesogleaBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/MesogleaBlock.java
@@ -1,9 +1,102 @@
 package net.frozenblock.wilderwild.block;
 
-import net.minecraft.world.level.block.HalfTransparentBlock;
+import net.frozenblock.wilderwild.registry.RegisterBlockEntities;
+import net.frozenblock.wilderwild.tag.WilderEntityTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
-public class MesogleaBlock extends HalfTransparentBlock {
+public class MesogleaBlock extends /*BaseEntityBlock*/ Block implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public MesogleaBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void entityInside(net.minecraft.world.level.block.state.BlockState state, Level world, BlockPos pos, Entity entity) {
+        if (state.getValue(WATERLOGGED)) {
+            if (!entity.getType().is(WilderEntityTags.CAN_SWIM_IN_MESOGLEA)) {
+                entity.makeStuckInBlock(state, new Vec3(0.7D, 0.7D, 0.7D));
+            }
+        }
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return !blockState.getValue(WATERLOGGED) ? blockState.getShape(blockGetter, blockPos) : Shapes.empty();
+    }
+
+    @Override
+    public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
+        int i;
+        if (blockState.getValue(WATERLOGGED)) {
+            levelAccessor.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
+        }
+        return blockState;
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState blockState) {
+        if (blockState.getValue(WATERLOGGED)) {
+            return Fluids.WATER.getSource(false);
+        }
+        return super.getFluidState(blockState);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        FluidState fluidState = blockPlaceContext.getLevel().getFluidState(blockPlaceContext.getClickedPos());
+        BlockState blockState = this.defaultBlockState().setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
+        return blockState;
+    }
+
+    /*@Nullable
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level world, BlockState state, BlockEntityType<T> type) {
+        return !world.isClientSide ? createTickerHelper(type, RegisterBlockEntities.TERMITE_MOUND, (worldx, pos, statex, blockEntity) -> blockEntity.tick(worldx, pos)) : null;
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }*/
+
+    @Override
+    public RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public boolean skipRendering(BlockState blockState, BlockState blockState2, Direction direction) {
+        if (blockState2.is(this)) {
+            return true;
+        }
+        return super.skipRendering(blockState, blockState2, direction);
     }
 }
--- src/main/java/net/frozenblock/wilderwild/tag/WilderEntityTags.java
+++ src/main/java/net/frozenblock/wilderwild/tag/WilderEntityTags.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.EntityType;
 
 public final class WilderEntityTags {
     public static final TagKey<EntityType<?>> CAN_SWIM_IN_ALGAE = bind("can_swim_in_algae");
+    public static final TagKey<EntityType<?>> CAN_SWIM_IN_MESOGLEA = bind("can_swim_in_mesoglea");
     public static final TagKey<EntityType<?>> ANCIENT_HORN_IMMUNE = bind("ancient_horn_immune");
     public static final TagKey<EntityType<?>> JELLYFISH_CANT_STING = bind("jellyfish_cant_sting");
 
