469a6cf05ade2dbf0ba27e2c90c57c0fbb5789c7
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-09-05 23:03:51 -0700

    it no work u-u

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SoulFireBlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SoulFireBlockMixin.java
@@ -1,30 +1,12 @@
 package net.frozenblock.wilderwild.mixin.server;
 
-import net.frozenblock.lib.mathematics.EasyNoiseSampler;
-import net.frozenblock.wilderwild.WilderWild;
-import net.frozenblock.wilderwild.block.OsseousSculkBlock;
-import net.frozenblock.wilderwild.registry.RegisterBlocks;
 import net.frozenblock.wilderwild.registry.RegisterSounds;
-import net.frozenblock.wilderwild.tag.WilderBlockTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Iterator;
 
 @Mixin(value = SoulFireBlock.class)
 public class SoulFireBlockMixin {
