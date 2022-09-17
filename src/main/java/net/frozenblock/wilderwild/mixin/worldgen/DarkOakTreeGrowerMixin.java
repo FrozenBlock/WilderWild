ca442fe5a05e5f903334646bb55f0a902de2216e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-17 09:28:21 -0400

    Update DarkOakTreeGrowerMixin.java

--- src/main/java/net/frozenblock/wilderwild/mixin/worldgen/DarkOakTreeGrowerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/worldgen/DarkOakTreeGrowerMixin.java
@@ -5,17 +5,13 @@ import net.minecraft.core.Holder;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.DarkOakTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = DarkOakTreeGrower.class, priority = 69420)
 public class DarkOakTreeGrowerMixin {
-    @Shadow @Final
-    private float tallProbability;
 
     @Inject(method = "getConfiguredMegaFeature", at = @At("RETURN"), cancellable = true)
     public void getConfiguredMegaFeature(RandomSource randomSource, CallbackInfoReturnable<Holder<? extends ConfiguredFeature<?, ?>>> cir) {
