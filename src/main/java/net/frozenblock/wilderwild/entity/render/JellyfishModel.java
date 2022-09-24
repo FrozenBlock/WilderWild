0d1da30047d06465a5217488ed52cf4d1eb922b7
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-23 22:00:57 -0400

    Possibly RGB Jellyfish?

--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishModel.java
@@ -73,18 +73,22 @@ public class JellyfishModel<T extends Jellyfish> extends HierarchicalModel<T> {
     public float tentXRot;
     public float tentZRot;
 
+    public float red;
+    public float green;
+    public float blue;
+
     @Override
     public void renderToBuffer(PoseStack poseStack, @NotNull VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         poseStack.pushPose();
         poseStack.mulPose(Vector3f.XP.rotationDegrees(this.xRot));
         poseStack.mulPose(Vector3f.YP.rotationDegrees(this.zRot));
-        this.body.render(poseStack, buffer, packedLight, packedOverlay, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.body.render(poseStack, buffer, packedLight, packedOverlay, this.red, this.green, this.blue, alpha);
         poseStack.popPose();
 
         poseStack.pushPose();
         poseStack.mulPose(Vector3f.XP.rotationDegrees(this.tentXRot));
         poseStack.mulPose(Vector3f.YP.rotationDegrees(this.tentZRot));
-        this.tentacleBase.render(poseStack, buffer, packedLight, packedOverlay, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.tentacleBase.render(poseStack, buffer, packedLight, packedOverlay, this.red, this.green, this.blue, alpha);
         poseStack.popPose();
     }
 
--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
@@ -7,11 +7,14 @@ import net.fabricmc.api.Environment;
 import net.frozenblock.wilderwild.WilderWild;
 import net.frozenblock.wilderwild.WilderWildClient;
 import net.frozenblock.wilderwild.entity.Jellyfish;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.item.DyeColor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,6 +32,23 @@ public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jel
         poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0f - g));
         poseStack.translate(0, -1, 0);
         poseStack.scale(0.8F, 0.8F, 0.8F);
+        JellyfishModel<Jellyfish> model = this.getModel();
+        if (jelly.hasCustomName() && "I_am_Merp".equals(jelly.getName().getString())) {
+            int j = jelly.tickCount / 25 + jelly.getId();
+            int k = DyeColor.values().length;
+            int l = j % k;
+            int m = (j + 1) % k;
+            float f1 = ((float)(jelly.tickCount % 25) + h) / 25.0F;
+            float[] fs = Sheep.getColorArray(DyeColor.byId(l));
+            float[] gs = Sheep.getColorArray(DyeColor.byId(m));
+            model.red = fs[0] * (1.0F - f1) + gs[0] * f1;
+            model.green = fs[1] * (1.0F - f1) + gs[1] * f1;
+            model.blue = fs[2] * (1.0F - f1) + gs[2] * f1;
+        } else {
+            model.red = 1;
+            model.green = 1;
+            model.blue = 1;
+        }
     }
 
     @Override
@@ -39,6 +59,9 @@ public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jel
     @Override
     @Nullable
     protected RenderType getRenderType(@NotNull Jellyfish jellyfish, boolean bl, boolean bl2, boolean bl3) {
+        if (jellyfish.hasCustomName() && "I_am_Merp".equals(jellyfish.getName().getString())) {
+            return WilderWildClient.ENTITY_TRANSLUCENT_EMISSIVE_FIXED.apply(WilderWild.id(BASE_TEXTURE + "white" + ".png"), false);
+        }
         return WilderWildClient.ENTITY_TRANSLUCENT_EMISSIVE_FIXED.apply(WilderWild.id(BASE_TEXTURE + jellyfish.getVariant() + ".png"), false);
     }
 
@@ -47,4 +70,8 @@ public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jel
         return WilderWild.id(BASE_TEXTURE + jellyfish.getVariant() + ".png");
     }
 
+    @Override
+    public void render(Jellyfish entity, float entityYaw, float partialTicks, PoseStack matrixStack, MultiBufferSource buffer, int packedLight) {
+        super.render(entity, entityYaw, partialTicks, matrixStack, buffer, packedLight);
+    }
 }
