9b5cef869b65892e01332b0ebca39658e468e20f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-24 11:27:16 -0400

    Move To FrozenLib

--- src/main/java/net/frozenblock/wilderwild/misc/FrozenLibIntegration.java
+++ src/main/java/net/frozenblock/wilderwild/misc/FrozenLibIntegration.java
@@ -1,6 +1,7 @@
 package net.frozenblock.wilderwild.misc;
 
 import net.frozenblock.lib.replacements_and_lists.BlockScheduledTicks;
+import net.frozenblock.lib.replacements_and_lists.DripstoneDripWaterFrom;
 import net.frozenblock.lib.replacements_and_lists.HopperUntouchableList;
 import net.frozenblock.lib.replacements_and_lists.StructurePoolElementIdReplacements;
 import net.frozenblock.lib.sound.RegisterMovingSoundRestrictions;
@@ -11,7 +12,10 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.InstrumentItem;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 public class FrozenLibIntegration {
 
@@ -39,6 +43,13 @@ public class FrozenLibIntegration {
         HopperUntouchableList.blackListedTypes.add(RegisterBlockEntities.STONE_CHEST);
         //StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/city_center/city_center_1"), WilderWild.id("ancient_city/city_center/city_center_1"));
         //StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/city_center/city_center_2"), WilderWild.id("ancient_city/city_center/city_center_2"));
+        DripstoneDripWaterFrom.map.put(Blocks.WET_SPONGE, (level, fluidInfo, blockPos) -> {
+                BlockState blockState = Blocks.SPONGE.defaultBlockState();
+        level.setBlockAndUpdate(fluidInfo.pos(), blockState);
+        Block.pushEntitiesUp(fluidInfo.sourceState(), blockState, level, fluidInfo.pos());
+        level.gameEvent(GameEvent.BLOCK_CHANGE, fluidInfo.pos(), GameEvent.Context.of(blockState));
+        level.levelEvent(1504, blockPos, 0);
+        });
 
         StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/structures/barracks"), WilderWild.id("ancient_city/structures/barracks"));
         StructurePoolElementIdReplacements.resourceLocationReplacements.put(new ResourceLocation("ancient_city/structures/chamber_1"), WilderWild.id("ancient_city/structures/chamber_1"));
