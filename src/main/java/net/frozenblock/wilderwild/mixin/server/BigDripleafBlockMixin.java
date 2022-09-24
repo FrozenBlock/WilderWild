4e0574c0a44a26d6508a60110c44b9e3f6a405e8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-23 21:38:07 -0400

    Do The Dripleaf Things Work Now

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/BigDripleafBlockMixin.java
@@ -0,0 +1,46 @@
+package net.frozenblock.wilderwild.mixin.server;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BigDripleafBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BigDripleafBlock.class)
+public abstract class BigDripleafBlockMixin {
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random, CallbackInfo info) {
+        BlockState downState = level.getBlockState(pos.below());
+        if (downState.is(Blocks.BIG_DRIPLEAF_STEM) && downState.getValue(BlockStateProperties.POWERED)) {
+            resetTilt(state, level, pos);
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "neighborChanged", at = @At("HEAD"), cancellable = true)
+    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving, CallbackInfo info) {
+        if (fromPos == pos.below()) {
+            BlockState downState = level.getBlockState(pos.below());
+            if (downState.is(Blocks.BIG_DRIPLEAF_STEM) && downState.getValue(BlockStateProperties.POWERED)) {
+                resetTilt(state, level, pos);
+                info.cancel();
+            }
+        }
+    }
+
+    @Shadow
+    private static void resetTilt(BlockState state, Level level, BlockPos pos) {
+
+    }
+
+}
