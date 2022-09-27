4153162d9730bcdfb2f3a3dc9b03fe17cb0e7eaf
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-27 13:31:16 -0400

    Update JellyfishRenderer.java

--- src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/entity/render/JellyfishRenderer.java
@@ -8,16 +8,12 @@ import net.frozenblock.wilderwild.WilderWild;
 import net.frozenblock.wilderwild.WilderWildClient;
 import net.frozenblock.wilderwild.entity.Jellyfish;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.animal.Sheep;
-import net.minecraft.world.item.DyeColor;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
 public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jellyfish>> {
@@ -56,16 +52,10 @@ public class JellyfishRenderer extends MobRenderer<Jellyfish, JellyfishModel<Jel
     }
 
     @Override
-    @Nullable
-    protected RenderType getRenderType(@NotNull Jellyfish jellyfish, boolean bl, boolean bl2, boolean bl3) {
-        if (jellyfish.hasCustomName() && "I_am_Merp".equals(jellyfish.getName().getString())) {
-            return WilderWildClient.ENTITY_TRANSLUCENT_EMISSIVE_FIXED.apply(WilderWild.id(WHITE_TEXTURE), true);
-        }
-        return WilderWildClient.ENTITY_TRANSLUCENT_EMISSIVE_FIXED.apply(WilderWild.id(BASE_TEXTURE + jellyfish.getVariant() + ".png"), true);
-    }
-
-    @Override
     public ResourceLocation getTextureLocation(@NotNull Jellyfish jellyfish) {
+        if (jellyfish.hasCustomName() && "I_am_Merp".equals(jellyfish.getName().getString())) {
+            return WilderWild.id(WHITE_TEXTURE);
+        }
         return WilderWild.id(BASE_TEXTURE + jellyfish.getVariant() + ".png");
     }
 
