60d9a38b02efb8484f95db50e95a660945fe2c81
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-09-29 11:20:55 -0400

    client.ScottTheWozMixin

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/ScottTheWozMixin.java
@@ -0,0 +1,35 @@
+package net.frozenblock.wilderwild.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.User;
+import net.minecraft.client.resources.SplashManager;
+import net.minecraft.util.RandomSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Locale;
+
+@Environment(EnvType.CLIENT)
+@Mixin(SplashManager.class)
+public class ScottTheWozMixin {
+
+    @Shadow @Final
+    private static RandomSource RANDOM;
+    @Shadow @Final
+    private List<String> splashes;
+    @Shadow @Final
+    private User user;
+
+    @Inject(method = "getSplash", at = @At("TAIL"), cancellable = true)
+    public void getSplash(CallbackInfoReturnable<String> info) {
+        if (this.user != null && RANDOM.nextInt(this.splashes.size()) == 42) {
+            info.setReturnValue("Hey all, " + this.user.getName().toUpperCase(Locale.ROOT) + " here.");
+        }
+    }
+}
