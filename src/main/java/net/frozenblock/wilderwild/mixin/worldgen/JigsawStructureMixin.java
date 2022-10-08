f17b0a7abedc89950d7b1376b5248ad54eca524c
Treetrain1 <treetrain1@gmail.com>
2022-10-08 17:23:40 -0500

    new better jigsaw structure mixin

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/worldgen/JigsawStructureMixin.java
@@ -0,0 +1,18 @@
+package net.frozenblock.wilderwild.mixin.worldgen;
+
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(JigsawStructure.class)
+public class JigsawStructureMixin {
+
+    @ModifyConstant(method = "method_41662(Lcom/mojang/serialization/codecs/RecordCodecBuilder$Instance;)Lcom/mojang/datafixers/kinds/App;",
+            constant = @Constant(intValue = 7))
+    private static int expandSize(int constant) {
+        return 20;
+    }
+}
