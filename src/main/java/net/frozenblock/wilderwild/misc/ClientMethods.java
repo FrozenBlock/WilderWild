81dd35764478ccb444e3f7ade83938b02ae29a2b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-10-22 13:52:26 -0400

    Update ClientMethods.java

--- src/main/java/net/frozenblock/wilderwild/misc/ClientMethods.java
+++ src/main/java/net/frozenblock/wilderwild/misc/ClientMethods.java
@@ -2,8 +2,8 @@ package net.frozenblock.wilderwild.misc;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.frozenblock.lib.sound.FrozenSoundPredicates;
 import net.frozenblock.lib.sound.MovingSoundWithRestriction;
+import net.frozenblock.lib.sound.SoundPredicate.FrozenSoundPredicate;
 import net.minecraft.client.Minecraft;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -15,7 +15,7 @@ public class ClientMethods {
     public static void playClientEnderManSound(EnderMan enderMan) {
         Minecraft client = Minecraft.getInstance();
         if (client.level != null && enderMan.isAlive()) {
-            client.getSoundManager().play(new MovingSoundWithRestriction(enderMan, SoundEvents.ENDERMAN_STARE, SoundSource.HOSTILE, 2.5F, 1F, FrozenSoundPredicates.NOT_SILENT_AND_ALIVE));
+            client.getSoundManager().play(new MovingSoundWithRestriction(enderMan, SoundEvents.ENDERMAN_STARE, SoundSource.HOSTILE, 2.5F, 1F, FrozenSoundPredicate.NOT_SILENT_AND_ALIVE));
         }
     }
 
