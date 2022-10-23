227923001c2d60a419ec975a054e39e1e1dc4549
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-10-23 13:29:37 -0400

    More Item Reorganization

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterBlocks.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterBlocks.java
@@ -98,7 +98,7 @@ public final class RegisterBlocks {
      * Building Blocks
      */
     public static void registerOtherBB() {
-        registerBlock("chiseled_mud_bricks", CHISELED_MUD_BRICKS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
+        registerBlockAfter(Items.MUD_BRICKS,"chiseled_mud_bricks", CHISELED_MUD_BRICKS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
     }
 
     // WOOD
@@ -297,12 +297,12 @@ public final class RegisterBlocks {
     public static final Block ECHO_GLASS = new EchoGlassBlock(FabricBlockSettings.of(Material.GLASS, MaterialColor.COLOR_CYAN).strength(0.3F).nonOpaque().sounds(RegisterBlockSoundGroups.ECHO_GLASS));
 
     public static void registerDeepDark() {
-        registerBlock("sculk_stairs", SCULK_STAIRS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
-        registerBlock("sculk_slab", SCULK_SLAB, CreativeModeTabs.TAB_BUILDING_BLOCKS);
-        registerBlock("sculk_wall", SCULK_WALL, CreativeModeTabs.TAB_BUILDING_BLOCKS);
-        registerBlock("osseous_sculk", OSSEOUS_SCULK, CreativeModeTabs.TAB_BUILDING_BLOCKS);
-        registerBlock("hanging_tendril", HANGING_TENDRIL, CreativeModeTabs.TAB_BUILDING_BLOCKS);
-        registerBlock("echo_glass", ECHO_GLASS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
+        registerBlockAfter(Items.DEEPSLATE_TILE_WALL,"sculk_stairs", SCULK_STAIRS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
+		registerBlockAfter(Items.DEEPSLATE_TILE_WALL,"sculk_slab", SCULK_SLAB, CreativeModeTabs.TAB_BUILDING_BLOCKS);
+		registerBlockAfter(Items.DEEPSLATE_TILE_WALL,"sculk_wall", SCULK_WALL, CreativeModeTabs.TAB_BUILDING_BLOCKS);
+        registerBlockAfter(Items.SCULK,"osseous_sculk", OSSEOUS_SCULK, CreativeModeTabs.TAB_NATURE);
+        registerBlockAfter(Items.SCULK_SENSOR,"hanging_tendril", HANGING_TENDRIL, CreativeModeTabs.TAB_NATURE);
+        registerBlockAfter(Items.TINTED_GLASS, "echo_glass", ECHO_GLASS, CreativeModeTabs.TAB_BUILDING_BLOCKS);
     }
 
     private static boolean always(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
