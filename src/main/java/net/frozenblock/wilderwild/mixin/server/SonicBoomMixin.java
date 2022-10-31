76e3af9f24f1ad4ad959d8311e9409de11e04d19
Treetrain1 <treetrain1@gmail.com>
2022-10-30 20:18:04 -0500

    help how fix sonic boom mixin

--- src/main/java/net/frozenblock/wilderwild/mixin/server/SonicBoomMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/SonicBoomMixin.java
@@ -10,10 +10,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 import net.minecraft.util.Unit;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.behavior.warden.SonicBoom;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
@@ -30,65 +32,55 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(value = SonicBoom.class, priority = 1001)
 public class SonicBoomMixin {
 
-    @Final
-    @Shadow
-    private static int DURATION;
+	@Inject(method = "m_ehrxwrfs", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"), locals = LocalCapture.CAPTURE_FAILHARD)
+	private static void stopParticles(Warden warden, ServerLevel level, LivingEntity livingEntity, CallbackInfo ci, Vec3 vec3, Vec3 vec32, Vec3 vec33, int i, Vec3 vec34) {
+		BlockPos hitPos = isOccluded(level, vec3, vec34);
+		if (hitPos != null) {
+			BlockState state = level.getBlockState(hitPos);
+			if (state.is(RegisterBlocks.ECHO_GLASS)) {
+				i = Mth.floor(vec32.length()) + 10;
+			}
+		}
+	}
 
-	@Shadow
-	@Final
-	private static int TICKS_BEFORE_PLAYING_SOUND;
+	@Inject(method = "m_ehrxwrfs", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+	private static void tick(Warden warden, ServerLevel level, LivingEntity livingEntity, CallbackInfo ci, Vec3 vec3, Vec3 vec32, Vec3 vec33) {
+		boolean blocked = false;
+		for (int i = 1; i < Mth.floor(vec32.length()) + 7; ++i) {
+			Vec3 vec34 = vec3.add(vec33.scale(i));
+			BlockPos hitPos = isOccluded(level, vec3, vec34);
+			if (hitPos != null) {
+				BlockState state = level.getBlockState(hitPos);
+				if (state.is(RegisterBlocks.ECHO_GLASS)) {
+					i = Mth.floor(vec32.length()) + 10;
+					blocked = true;
+					EchoGlassBlock.damage(level, hitPos);
+				}
+			}
+		}
 
-	@Inject(at = @At("HEAD"), method = "tick(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/monster/warden/Warden;J)V", cancellable = true)
-    public void tick(ServerLevel level, Warden owner, long gameTime, CallbackInfo info) {
-        if (!FabricLoader.getInstance().isModLoaded("customsculk")) {
-			owner.getBrain().getMemory(MemoryModuleType.ATTACK_TARGET).ifPresent(livingEntity -> owner.getLookControl().setLookAt(livingEntity.position()));
-            if (!owner.getBrain().hasMemoryValue(MemoryModuleType.SONIC_BOOM_SOUND_DELAY)
-				&& !owner.getBrain().hasMemoryValue(MemoryModuleType.SONIC_BOOM_SOUND_COOLDOWN)) {
-				owner.getBrain().setMemoryWithExpiry(MemoryModuleType.SONIC_BOOM_SOUND_COOLDOWN, Unit.INSTANCE, DURATION - TICKS_BEFORE_PLAYING_SOUND);
-                owner.getBrain()
-						.getMemory(MemoryModuleType.ATTACK_TARGET)
-						.filter((target) -> owner.closerThan(target, 15.0D, 20.0D))
-						.ifPresent((target) -> {
-							Vec3 vec3 = owner.position().add(0F, 1.6F, 0F);
-							Vec3 vec32 = target.getEyePosition().subtract(vec3);
-							Vec3 vec33 = vec32.normalize();
+		if (blocked) {
+			ci.cancel();
+		}
+	}
 
-							boolean blocked = false;
-							for (int i = 1; i < Mth.floor(vec32.length()) + 7; ++i) {
-								Vec3 vec34 = vec3.add(vec33.scale(i));
-								level.sendParticles(ParticleTypes.SONIC_BOOM, vec34.x, vec34.y, vec34.z, 1, 0.0D, 0.0D, 0.0D, 0.0D);
-								BlockPos hitPos = isOccluded(level, vec3, vec34);
-								if (hitPos != null) {
-									BlockState state = level.getBlockState(hitPos);
-									if (state.is(RegisterBlocks.ECHO_GLASS)) {
-										i = Mth.floor(vec32.length()) + 10;
-										blocked = true;
-										EchoGlassBlock.damage(level, hitPos);
-									}
-								}
-							}
-
-							if (((WilderWarden) owner).isOsmiooo()) {
-								owner.playSound(RegisterSounds.ENTITY_WARDEN_BRAP, 3.0F, 1.0F);
-							} else {
-								owner.playSound(SoundEvents.WARDEN_SONIC_BOOM, 3.0F, 1.0F);
-							}
-							if (!blocked) {
-								target.hurt(DamageSource.sonicBoom(owner), 10.0F);
-								double verticalKnockback = 0.5D * (1.0D - target.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
-								double horizontalKnockback = 2.5D * (1.0D - target.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE));
-								target.push(vec33.x() * horizontalKnockback, vec33.y() * verticalKnockback, vec33.z() * horizontalKnockback);
-							}
-                });
-            }
-            info.cancel();
-        }
-    }
+	@Redirect(method = "m_ehrxwrfs", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/warden/Warden;playSound(Lnet/minecraft/sounds/SoundEvent;FF)V"))
+	private static void modifySound(Warden warden, SoundEvent soundEvent, float volume, float pitch) {
+		if (((WilderWarden) warden).isOsmiooo()) {
+			warden.playSound(RegisterSounds.ENTITY_WARDEN_BRAP, volume, pitch);
+		} else {
+			warden.playSound(soundEvent, volume, pitch);
+		}
+	}
 
 	@Unique
     private static BlockPos isOccluded(Level level, Vec3 start, Vec3 end) {
