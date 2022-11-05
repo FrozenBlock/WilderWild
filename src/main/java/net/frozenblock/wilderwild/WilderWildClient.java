6a535c0e285081c032eb10f16b7d9432bc223bd0
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-05 16:38:28 -0400

    Splashes

--- src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
+++ src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
@@ -12,6 +12,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.frozenblock.lib.impl.NewSplahes;
 import net.frozenblock.lib.math.AdvancedMath;
 import net.frozenblock.lib.sound.api.FlyBySoundHub;
 import net.frozenblock.wilderwild.entity.AncientHornProjectile;
@@ -64,6 +65,7 @@ public final class WilderWildClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+		NewSplahes.addNewSplashLocation(WilderWild.id("texts/splashes.txt"));
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.CARNATION, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.SEEDING_DANDELION, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(RegisterBlocks.POTTED_CARNATION, RenderType.cutout());
