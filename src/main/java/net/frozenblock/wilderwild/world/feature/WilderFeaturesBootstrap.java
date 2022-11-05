d825a81a965daa69747aab0695c60478ea59d1a7
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-05 11:18:11 -0400

    Mojang

--- src/main/java/net/frozenblock/wilderwild/world/feature/WilderFeaturesBootstrap.java
+++ src/main/java/net/frozenblock/wilderwild/world/feature/WilderFeaturesBootstrap.java
@@ -17,7 +17,7 @@ public final class WilderFeaturesBootstrap {
 	private static final ArrayList<FrozenPlacedFeature> REGISTERED_PLACED_FEATURES = new ArrayList<>();
 
 	public static synchronized void bootstrap(FabricWorldgenProvider.Entries entries) {
-		WilderTreeConfigured.registerTreeConfigured();
+		WilderTreeConfigured.registerTreeConfigured(entries);
 		addEntries(entries);
 		WilderMiscConfigured.Pre.init();
 		addEntries(entries);
