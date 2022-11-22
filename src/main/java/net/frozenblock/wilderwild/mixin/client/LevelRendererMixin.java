5dd55d3710b1298cf8c3d6f992261cded6fded89
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-21 22:06:43 -0500

    meh ww dev can have it for now

--- src/main/java/net/frozenblock/wilderwild/mixin/client/LevelRendererMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/LevelRendererMixin.java
@@ -1,18 +1,37 @@
 package net.frozenblock.wilderwild.mixin.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexBuffer;
+import com.mojang.math.Matrix4f;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.frozenblock.lib.wind.ClientWindManager;
 import net.frozenblock.wilderwild.misc.config.ClothConfigInteractionHandler;
 import net.frozenblock.wilderwild.registry.RegisterSounds;
+import net.minecraft.client.CloudStatus;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.FogRenderer;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.ShaderInstance;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SculkShriekerBlock;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,11 +39,35 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(LevelRenderer.class)
+@Mixin(value = LevelRenderer.class, priority = 1001)
 public class LevelRendererMixin {
 
-    @Shadow
-    private @Nullable ClientLevel level;
+    @Shadow @Nullable
+    private ClientLevel level;
+	@Shadow @Final
+	private Minecraft minecraft;
+	@Shadow @Final
+	private RenderBuffers renderBuffers;
+	@Shadow @Final
+	private EntityRenderDispatcher entityRenderDispatcher;
+	@Shadow
+	private int ticks;
+	@Shadow
+	private boolean generateClouds;
+	@Shadow @Nullable
+	private VertexBuffer cloudBuffer;
+	@Shadow
+	private int prevCloudX;
+	@Shadow
+	private int prevCloudY;
+	@Shadow
+	private int prevCloudZ;
+	@Shadow
+	private Vec3 prevCloudColor;
+	@Shadow @Nullable
+	private CloudStatus prevCloudsType;
+	@Shadow @Final
+	private static ResourceLocation CLOUDS_LOCATION;
 
     @Inject(method = "levelEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playLocalSound(DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZ)V", ordinal = 0), cancellable = true)
     private void levelEvent(int eventId, BlockPos pos, int data, CallbackInfo ci) {
@@ -46,6 +89,89 @@ public class LevelRendererMixin {
             }
         }
     }
+
+	@Inject(method = "renderClouds", at = @At("HEAD"), cancellable = true)
+	public void renderClouds(PoseStack poseStack, Matrix4f projectionMatrix, float partialTick, double camX, double camY, double camZ, CallbackInfo info) {
+		info.cancel();
+		float f = this.level.effects().getCloudHeight();
+		if (!Float.isNaN(f)) {
+			RenderSystem.disableCull();
+			RenderSystem.enableBlend();
+			RenderSystem.enableDepthTest();
+			RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+			RenderSystem.depthMask(true);
+
+			double cloudX = (camX / 12.0D) - ClientWindManager.getCloudX(partialTick);
+			double cloudY = (double)(f - (float)camY + 0.33F) + ClientWindManager.getCloudY(partialTick);
+			double cloudZ = (camZ / 12.0D + 0.33000001311302185D) - ClientWindManager.getCloudZ(partialTick);
+
+			cloudX -= Mth.floor(cloudX / 2048.0D) * 2048;
+			cloudZ -= Mth.floor(cloudZ / 2048.0D) * 2048;
+			float l = (float)(cloudX - (double)Mth.floor(cloudX));
+			float m = (float)(cloudY / 4.0D - (double)Mth.floor(cloudY / 4.0D)) * 4.0F;
+			float n = (float)(cloudZ - (double)Mth.floor(cloudZ));
+			Vec3 vec3 = this.level.getCloudColor(partialTick);
+			int o = (int)Math.floor(cloudX);
+			int p = (int)Math.floor(cloudY / 4.0D);
+			int q = (int)Math.floor(cloudZ);
+			if (o != this.prevCloudX || p != this.prevCloudY || q != this.prevCloudZ || this.minecraft.options.getCloudsType() != this.prevCloudsType || this.prevCloudColor.distanceToSqr(vec3) > 2.0E-4D) {
+				this.prevCloudX = o;
+				this.prevCloudY = p;
+				this.prevCloudZ = q;
+				this.prevCloudColor = vec3;
+				this.prevCloudsType = this.minecraft.options.getCloudsType();
+				this.generateClouds = true;
+			}
+
+			if (this.generateClouds) {
+				this.generateClouds = false;
+				BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
+				if (this.cloudBuffer != null) {
+					this.cloudBuffer.close();
+				}
+
+				this.cloudBuffer = new VertexBuffer();
+				BufferBuilder.RenderedBuffer renderedBuffer = this.buildClouds(bufferBuilder, cloudX, cloudY, cloudZ, vec3);
+				this.cloudBuffer.bind();
+				this.cloudBuffer.upload(renderedBuffer);
+				VertexBuffer.unbind();
+			}
+
+			RenderSystem.setShader(GameRenderer::getPositionTexColorNormalShader);
+			RenderSystem.setShaderTexture(0, CLOUDS_LOCATION);
+			FogRenderer.levelFogColor();
+			poseStack.pushPose();
+			poseStack.scale(12.0F, 1.0F, 12.0F);
+			poseStack.translate(-l, m, -n);
+			if (this.cloudBuffer != null) {
+				this.cloudBuffer.bind();
+				int r = this.prevCloudsType == CloudStatus.FANCY ? 0 : 1;
+
+				for(int s = r; s < 2; ++s) {
+					if (s == 0) {
+						RenderSystem.colorMask(false, false, false, false);
+					} else {
+						RenderSystem.colorMask(true, true, true, true);
+					}
+
+					ShaderInstance shaderInstance = RenderSystem.getShader();
+					this.cloudBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, shaderInstance);
+				}
+
+				VertexBuffer.unbind();
+			}
+
+			poseStack.popPose();
+			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+			RenderSystem.enableCull();
+			RenderSystem.disableBlend();
+		}
+	}
+
+	@Shadow
+	private BufferBuilder.RenderedBuffer buildClouds(BufferBuilder builder, double x, double y, double z, Vec3 cloudColor) {
+		throw new AssertionError("Mixin injection failed - WIlderWild LevelRendererMixin");
+	}
 
     /*@Shadow
     private void renderChunkLayer(RenderType renderType, PoseStack poseStack, double d, double e, double f, Matrix4f matrix4f) {
