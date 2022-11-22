75148e8e23984682a0c8f6bb88b5261517695b62
Soro335 <gabeonelove335@gmail.com>
2022-11-22 19:24:46 +0300

    Palm fixes(need new foliage placer)

--- src/main/java/net/frozenblock/wilderwild/world/feature/WilderConfiguredFeatures.java
+++ src/main/java/net/frozenblock/wilderwild/world/feature/WilderConfiguredFeatures.java
@@ -219,7 +219,9 @@ public final class WilderConfiguredFeatures {
 			register("big_shrubs", Feature.RANDOM_SELECTOR,
 					new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(WilderTreePlaced.BIG_SHRUB_CHECKED, 1.0F)), WilderTreePlaced.BIG_SHRUB_CHECKED));
 
-
+	public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> PALM =
+			register("palm", Feature.RANDOM_SELECTOR,
+					new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(WilderTreePlaced.PALM_CHECKED, 1.0F)), WilderTreePlaced.PALM_CHECKED));
 	//FLOWERS
     public static final Holder<ConfiguredFeature<RandomPatchConfiguration, ?>> SEEDING_DANDELION =
             register("seeding_dandelion", Feature.FLOWER,
