83e951395e13db9b0f583efecf3b90d8fbcd3e91
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-23 16:01:17 -0500

    Update PalmFoliagePlacer.java

--- src/main/java/net/frozenblock/wilderwild/world/generation/foliage/PalmFoliagePlacer.java
+++ src/main/java/net/frozenblock/wilderwild/world/generation/foliage/PalmFoliagePlacer.java
@@ -46,7 +46,7 @@ public class PalmFoliagePlacer extends FoliagePlacer {
 			double dirX = offsetPos.x - origin.x;
 			double dirZ = offsetPos.z - origin.z;
 			for (int r = 0; r < radius; r++) {
-				double yOffset = (2 * (Math.sin(((Math.PI * (fronds - r)) / divRad - minus)))) - 1.3;
+				double yOffset = (2 * (Math.sin(((Math.PI * (fronds - r)) / divRad) - minus))) - 1.3;
 				placeLeavesAtPos(level, blockSetter, random, config, blockPos, (dirX * r), yOffset, (dirZ * r));
 			}
 			angle += rotAngle;
