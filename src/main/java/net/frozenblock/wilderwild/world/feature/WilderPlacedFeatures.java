7e687e02e236b83f645a3f866b6de02cd0f5a233
Treetrain1 <treetrain1@gmail.com>
2022-11-23 21:27:18 -0600

    fix worldgen + remove uvlock

--- src/main/java/net/frozenblock/wilderwild/world/feature/WilderPlacedFeatures.java
+++ src/main/java/net/frozenblock/wilderwild/world/feature/WilderPlacedFeatures.java
@@ -25,6 +25,7 @@ import net.minecraft.world.level.levelgen.placement.BlockPredicateFilter;
 import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.EnvironmentScanPlacement;
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.NoiseBasedCountPlacement;
 import net.minecraft.world.level.levelgen.placement.NoiseThresholdCountPlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
@@ -299,7 +300,7 @@ public final class WilderPlacedFeatures {
 	public static final Holder<PlacedFeature> NEMATOCYST = PlacementUtils.register(
 			"nematocyst",
 			WilderConfiguredFeatures.NEMATOCYST,
-			CountPlacement.of(ConstantInt.of(16 )),
+			CountPlacement.of(ConstantInt.of(64)),
 			InSquarePlacement.spread(),
 			PlacementUtils.RANGE_BOTTOM_TO_MAX_TERRAIN_HEIGHT,
 			BiomeFilter.biome()
@@ -308,7 +309,7 @@ public final class WilderPlacedFeatures {
 	public static final Holder<PlacedFeature> NEMATOCYST_PURPLE = PlacementUtils.register(
 			"nematocyst_purple",
 			WilderConfiguredFeatures.NEMATOCYST_PURPLE,
-			CountPlacement.of(ConstantInt.of(16)),
+			CountPlacement.of(ConstantInt.of(64)),
 			InSquarePlacement.spread(),
 			PlacementUtils.RANGE_BOTTOM_TO_MAX_TERRAIN_HEIGHT,
 			BiomeFilter.biome()
