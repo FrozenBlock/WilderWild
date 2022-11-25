f1cc52284de32eb9c94e30433a24115e4ec5a331
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-25 10:07:39 -0500

    Update TumbleweedBlock.java

--- src/main/java/net/frozenblock/wilderwild/block/TumbleweedBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/TumbleweedBlock.java
@@ -6,11 +6,8 @@ import net.frozenblock.wilderwild.registry.RegisterBlocks;
 import net.frozenblock.wilderwild.registry.RegisterEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -18,6 +15,7 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -46,8 +44,8 @@ public class TumbleweedBlock extends Block implements SimpleWaterloggedBlock {
 			if (!level.isClientSide) {
 				Tumbleweed weed = new Tumbleweed(RegisterEntities.TUMBLEWEED, level);
 				level.addFreshEntity(weed);
-				weed.setPos(new Vec3(pos.getX(), pos.getY(), pos.getZ()));
-				level.destroyBlock(pos, false);
+				weed.setPos(new Vec3(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5));
+				level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
 				itemStack.hurtAndBreak(1, player, (playerx) -> playerx.broadcastBreakEvent(hand));
 				level.gameEvent(player, GameEvent.SHEAR, pos);
 			}
