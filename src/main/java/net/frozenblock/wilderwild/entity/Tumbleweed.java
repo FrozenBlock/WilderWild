b26da666849bd34cecd8988a47f0d8315dffd2d2
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-24 21:01:43 -0500

    Update Tumbleweed.java

--- src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
@@ -60,7 +60,7 @@ public class Tumbleweed extends Mob {
 	@Override
 	protected void doPush(@NotNull Entity entity) {
 		super.doPush(entity);
-		if (this.getDeltaMovement().length() > 0.3) {
+		if (this.getDeltaMovement().length() > 0.2) {
 			//TODO: Tumbleweed Damagesource
 			entity.hurt(DamageSource.mobAttack(this), 2F);
 			this.destroy();
@@ -68,22 +68,9 @@ public class Tumbleweed extends Mob {
 	}
 
 	@Override
-	public boolean canBeCollidedWith() {
-		return true;
-	}
-
-	@Override
 	public void tick() {
 		super.tick();
 		this.setYRot(0F);
-		double multiplier = (this.level.getBrightness(LightLayer.SKY, this.blockPosition()) * 0.0667) * (this.wasTouchingWater ? this.getWaterSlowDown() : 1F);
-		double windX = Mth.clamp(WindManager.windX * windMultiplier, -windClamp, windClamp);
-		double windZ = Mth.clamp(WindManager.windZ * windMultiplier, -windClamp, windClamp);
-		Vec3 deltaMovement = this.getDeltaMovement();
-		deltaMovement = deltaMovement.add((windX * 0.2) * multiplier, 0, (windZ * 0.2) * multiplier);
-		deltaMovement = new Vec3(deltaMovement.x, deltaMovement.y < 0 ? deltaMovement.y * 0.88 : deltaMovement.y, deltaMovement.z);
-		this.setDeltaMovement(deltaMovement);
-
 		Vec3 deltaPos = this.getPosition(1).subtract(this.getPosition(0));
 		this.prevPitch = this.pitch;
 		this.prevYaw = this.yaw;
@@ -92,11 +79,21 @@ public class Tumbleweed extends Mob {
 		this.roll += deltaPos.x * 35;
 		this.yaw += deltaPos.y * 35;
 		this.pitch += deltaPos.z * 35;
+
+		double multiplier = (this.level.getBrightness(LightLayer.SKY, this.blockPosition()) * 0.0667) * (this.wasTouchingWater ? this.getWaterSlowDown() : 1F);
+		double windX = Mth.clamp(WindManager.windX * windMultiplier, -windClamp, windClamp);
+		double windZ = Mth.clamp(WindManager.windZ * windMultiplier, -windClamp, windClamp);
+		Vec3 deltaMovement = this.getDeltaMovement();
+		deltaMovement = deltaMovement.add((windX * 0.2) * multiplier, 0, (windZ * 0.2) * multiplier);
+		deltaMovement = new Vec3(deltaMovement.x, deltaMovement.y < 0 ? deltaMovement.y * 0.88 : deltaMovement.y, deltaMovement.z);
 		if (deltaPos.y <= 0 && this.isOnGround()) {
 			deltaMovement = deltaMovement.add(0, Math.min(0.5, ((deltaPos.horizontalDistance() * 1.1))) * multiplier, 0);
 		}
-		if (deltaMovement.horizontalDistance() > 0.05 && deltaPos.horizontalDistance() < 0.01) {
+		if (deltaMovement.horizontalDistance() > 0.01 && deltaPos.horizontalDistance() < 0.01) {
 			deltaMovement = deltaMovement.add(0, Math.min(0.5, ((deltaMovement.horizontalDistance() * 0.05))) * multiplier, 0);
+		}
+		if (this.wasEyeInWater) {
+			deltaMovement =deltaMovement.add(0, 0.05, 0);
 		}
 		this.setDeltaMovement(deltaMovement);
 	}
