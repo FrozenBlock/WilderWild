3508a37c80821610de2187a7b07a1779fe71aeff
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-24 20:53:24 -0500

    hh

--- src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
@@ -13,6 +13,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.HumanoidArm;
@@ -57,16 +58,31 @@ public class Tumbleweed extends Mob {
 	}
 
 	@Override
+	protected void doPush(@NotNull Entity entity) {
+		super.doPush(entity);
+		if (this.getDeltaMovement().length() > 0.3) {
+			//TODO: Tumbleweed Damagesource
+			entity.hurt(DamageSource.mobAttack(this), 2F);
+		}
+	}
+
+	@Override
+	public boolean canBeCollidedWith() {
+		return true;
+	}
+
+	@Override
 	public void tick() {
 		super.tick();
 		this.setYRot(0F);
-		double multiplier = this.level.getBrightness(LightLayer.SKY, this.blockPosition()) * 0.0667;
+		double multiplier = (this.level.getBrightness(LightLayer.SKY, this.blockPosition()) * 0.0667) * (this.wasTouchingWater ? this.getWaterSlowDown() : 1F);
 		double windX = Mth.clamp(WindManager.windX * windMultiplier, -windClamp, windClamp);
 		double windZ = Mth.clamp(WindManager.windZ * windMultiplier, -windClamp, windClamp);
 		Vec3 deltaMovement = this.getDeltaMovement();
 		deltaMovement = deltaMovement.add((windX * 0.2) * multiplier, 0, (windZ * 0.2) * multiplier);
 		deltaMovement = new Vec3(deltaMovement.x, deltaMovement.y < 0 ? deltaMovement.y * 0.88 : deltaMovement.y, deltaMovement.z);
 		this.setDeltaMovement(deltaMovement);
+
 		Vec3 deltaPos = this.getPosition(1).subtract(this.getPosition(0));
 		this.prevPitch = this.pitch;
 		this.prevYaw = this.yaw;
@@ -76,11 +92,12 @@ public class Tumbleweed extends Mob {
 		this.yaw += deltaPos.y * 35;
 		this.pitch += deltaPos.z * 35;
 		if (deltaPos.y <= 0 && this.isOnGround()) {
-			this.setDeltaMovement(deltaMovement.add(0, Math.min(0.5, ((deltaPos.horizontalDistance() * 1.1))) * multiplier, 0));
+			deltaMovement = deltaMovement.add(0, Math.min(0.5, ((deltaPos.horizontalDistance() * 1.1))) * multiplier, 0);
 		}
-		if (this.wasTouchingWater || this.wasOnFire) {
-			this.destroy();
+		if (deltaMovement.horizontalDistance() > 0.05 && deltaPos.horizontalDistance() < 0.01) {
+			deltaMovement = deltaMovement.add(0, Math.min(0.5, ((deltaMovement.horizontalDistance() * 0.05))) * multiplier, 0);
 		}
+		this.setDeltaMovement(deltaMovement);
 	}
 
 	public void destroy() {
