93fee22dc1fe67d7fa6648e86253e9611b39e777
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-24 23:15:43 -0500

    shears

--- src/main/java/net/frozenblock/wilderwild/block/TumbleweedBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/TumbleweedBlock.java
@@ -1,10 +1,21 @@
 package net.frozenblock.wilderwild.block;
 
 import java.util.Objects;
+import net.frozenblock.wilderwild.entity.Tumbleweed;
 import net.frozenblock.wilderwild.registry.RegisterBlocks;
+import net.frozenblock.wilderwild.registry.RegisterEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -12,8 +23,11 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,6 +37,22 @@ public class TumbleweedBlock extends Block implements SimpleWaterloggedBlock {
 	public TumbleweedBlock(Properties properties) {
 		super(properties);
 		this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, @NotNull Level level, @NotNull BlockPos pos, Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+		ItemStack itemStack = player.getItemInHand(hand);
+		if (itemStack.is(Items.SHEARS)) {
+			Tumbleweed weed = new Tumbleweed(RegisterEntities.TUMBLEWEED, level);
+			level.addFreshEntity(weed);
+			weed.setPos(new Vec3(pos.getX(), 0, pos.getZ()));
+			level.destroyBlock(pos, false);
+			itemStack.hurtAndBreak(1, player, (playerx) -> playerx.broadcastBreakEvent(hand));
+			level.gameEvent(player, GameEvent.SHEAR, pos);
+			return InteractionResult.sidedSuccess(level.isClientSide);
+		} else {
+			return super.use(state, level, pos, player, hand, hit);
+		}
 	}
 
 	@Override
