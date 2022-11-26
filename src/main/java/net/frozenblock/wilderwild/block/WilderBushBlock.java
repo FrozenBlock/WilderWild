7201ac9399a71815454d2809abee8a32e2ab1efa
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-11-26 16:20:46 -0500

    1 Block Tall

--- src/main/java/net/frozenblock/wilderwild/block/WilderBushBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/WilderBushBlock.java
@@ -3,12 +3,12 @@ package net.frozenblock.wilderwild.block;
 import net.frozenblock.wilderwild.tag.WilderBlockTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.DoublePlantBlock;
+import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-public class WilderBushBlock extends DoublePlantBlock {
+public class WilderBushBlock extends BushBlock {
 	public WilderBushBlock(BlockBehaviour.Properties properties) {
 		super(properties);
 	}
