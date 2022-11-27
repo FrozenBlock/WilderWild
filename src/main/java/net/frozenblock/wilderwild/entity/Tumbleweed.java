0013dab363387edc6964e207178bffd55172b37a
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-11-26 21:44:38 -0700

    no swim sound

--- src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Tumbleweed.java
@@ -293,6 +293,11 @@ public class Tumbleweed extends Mob {
 	}
 
 	@Override
+	protected SoundEvent getSwimSound() {
+		return null;
+	}
+
+	@Override
 	protected void playStepSound(@NotNull BlockPos pos, @NotNull BlockState state) {
 		this.playSound(RegisterSounds.ENTITY_TUMBLEWEED_BOUNCE, 0.15f, 1.0f);
 	}
