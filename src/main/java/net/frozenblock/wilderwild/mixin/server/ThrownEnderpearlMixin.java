7f70a4ea8b4236fa3454d55b19d6b8915e8c3323
Merp is Me <27739930+MerpisMe@users.noreply.github.com>
2022-12-03 20:24:15 -0500

    ffs

--- src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
@@ -1,10 +1,8 @@
 package net.frozenblock.wilderwild.mixin.server;
 
 import net.frozenblock.wilderwild.registry.RegisterSounds;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.projectile.ThrownEgg;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,12 +16,7 @@ public class ThrownEnderpearlMixin {
     @Inject(method = "onHit", at = @At("HEAD"))
 	public void onHit(HitResult result, CallbackInfo info) {
 		ThrownEnderpearl pearl = ThrownEnderpearl.class.cast(this);
-		if (!pearl.level.isClientSide && !pearl.isRemoved()) {
-			pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), RegisterSounds.ITEM_ENDERPEARL_LAND, SoundSource.BLOCKS, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
-			pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.BLOCKS, 0.4F, 1.0F);
-			for(int i = 0; i < 32; ++i) {
-				pearl.level.addParticle(ParticleTypes.PORTAL, pearl.getX(), pearl.getY() + pearl.level.random.nextDouble() * 2.0, pearl.getZ(), pearl.level.random.nextGaussian(), 0.0, pearl.level.random.nextGaussian());
-			}
-		}
+		pearl.playSound(RegisterSounds.ITEM_ENDERPEARL_LAND, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+		//pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.BLOCKS, 0.4F, 1.0F);
 	}
 }
