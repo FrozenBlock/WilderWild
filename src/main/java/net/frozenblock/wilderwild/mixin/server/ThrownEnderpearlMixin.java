0f08887c40140501cea5ed44c5da37796b3088bd
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-04 17:54:57 -0500

    There we go!!! :D

--- src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
@@ -3,6 +3,8 @@ package net.frozenblock.wilderwild.mixin.server;
 import net.frozenblock.wilderwild.misc.config.ClothConfigInteractionHandler;
 import net.frozenblock.wilderwild.registry.RegisterSounds;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,9 +18,21 @@ public class ThrownEnderpearlMixin {
     @Inject(method = "onHit", at = @At("HEAD"))
 	public void onHit(HitResult result, CallbackInfo info) {
 		ThrownEnderpearl pearl = ThrownEnderpearl.class.cast(this);
+		Player owner =  pearl.getOwner() instanceof Player ? ((Player)pearl.getOwner()) : null;
+
 		if (ClothConfigInteractionHandler.enderpearlLandingSounds()) {
-			pearl.playSound(RegisterSounds.ITEM_ENDERPEARL_LAND, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
-			pearl.playSound(SoundEvents.CHORUS_FRUIT_TELEPORT, 0.4F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+			boolean silent = pearl.isSilent();
+			if (!pearl.level.isClientSide) {
+				if (!silent) {
+					pearl.level.playSound(owner, pearl.getX(), pearl.getY(), pearl.getZ(), RegisterSounds.ITEM_ENDERPEARL_LAND, SoundSource.NEUTRAL, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+				}
+				if (owner == null || !owner.isSilent()) {
+					pearl.level.playSound(owner, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.NEUTRAL, 0.4F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+				}
+			} else if (owner != null) {
+				owner.playSound(RegisterSounds.ITEM_ENDERPEARL_LAND, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+				owner.playSound(SoundEvents.CHORUS_FRUIT_TELEPORT, 0.4F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+			}
 		}
 	}
 }
