2556037da7bad699ea8a3f64ee4ff1da8f70551d
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-04 18:28:19 -0500

    spaghetti

--- src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/ThrownEnderpearlMixin.java
@@ -6,6 +6,7 @@ import net.frozenblock.wilderwild.registry.RegisterSounds;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.phys.HitResult;
@@ -20,23 +21,28 @@ public class ThrownEnderpearlMixin {
     @Inject(method = "onHit", at = @At("HEAD"))
 	public void onHit(HitResult result, CallbackInfo info) {
 		ThrownEnderpearl pearl = ThrownEnderpearl.class.cast(this);
-		Player owner =  pearl.getOwner() instanceof Player ? ((Player)pearl.getOwner()) : null;
 
 		if (ClothConfigInteractionHandler.enderpearlLandingSounds()) {
-			boolean silent = pearl.isSilent();
 			if (!pearl.level.isClientSide) {
-				if (!silent) {
-					float pitch = 0.85F + (pearl.level.random.nextFloat() * 0.2F);
-					pearl.level.playSound(owner, pearl.getX(), pearl.getY(), pearl.getZ(), RegisterSounds.ITEM_ENDERPEARL_LAND, SoundSource.NEUTRAL, 0.6F, pitch);
-					if (owner instanceof ServerPlayer player) {
+				Entity owner = pearl.getOwner();
+				
+				if (owner instanceof ServerPlayer player) {
+					if (!pearl.isSilent()) {
+						float pitch = 0.85F + (pearl.level.random.nextFloat() * 0.2F);
+						pearl.level.playSound(player, pearl.getX(), pearl.getY(), pearl.getZ(), RegisterSounds.ITEM_ENDERPEARL_LAND, SoundSource.NEUTRAL, 0.6F, pitch);
 						FrozenSoundPackets.createLocalPlayerSound(player, RegisterSounds.ITEM_ENDERPEARL_LAND, 0.6F, pitch);
 					}
-				}
-				if (owner != null && !owner.isSilent()) {
-					float pitch = 0.85F + (pearl.level.random.nextFloat() * 0.2F);
-					pearl.level.playSound(owner, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.NEUTRAL, 0.4F, pitch);
-					if (owner instanceof ServerPlayer player) {
+					if (!player.isSilent()) {
+						float pitch = 0.85F + (pearl.level.random.nextFloat() * 0.2F);
+						pearl.level.playSound(player, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.NEUTRAL, 0.4F, pitch);
 						FrozenSoundPackets.createLocalPlayerSound(player, SoundEvents.CHORUS_FRUIT_TELEPORT, 0.4F, pitch);
+					}
+				} else {
+					if (!pearl.isSilent()) {
+						pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), RegisterSounds.ITEM_ENDERPEARL_LAND, SoundSource.NEUTRAL, 0.6F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
+					}
+					if (owner != null && !owner.isSilent()) {
+						pearl.level.playSound(null, pearl.getX(), pearl.getY(), pearl.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.NEUTRAL, 0.4F, 0.85F + (pearl.level.random.nextFloat() * 0.2F));
 					}
 				}
 			}
