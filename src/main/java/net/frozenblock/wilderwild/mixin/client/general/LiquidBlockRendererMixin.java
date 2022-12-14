8982f05f1dff38a7ba3baeb5885561fdeb338224
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-14 17:15:42 -0500

    faster

--- src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -26,6 +27,15 @@ public class LiquidBlockRendererMixin {
 	@Shadow
 	private TextureAtlasSprite waterOverlay;
 
+	@Unique
+	float u0;
+	@Unique
+	float u1;
+	@Unique
+	float v0;
+	@Unique
+	float v1;
+
     @Inject(method = "shouldRenderFace", at = @At(value = "HEAD"), cancellable = true)
     private static void shouldRenderFace(BlockAndTintGetter blockAndTintGetter, BlockPos blockPos, FluidState fluidState, BlockState blockState, Direction side, FluidState fluidState2, CallbackInfoReturnable<Boolean> info) {
         if (blockState.getBlock() instanceof MesogleaBlock && side != Direction.UP && !FrozenBools.HAS_SODIUM) {
@@ -35,25 +45,29 @@ public class LiquidBlockRendererMixin {
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 8))
 	private void sideTextureBottom1(Args args) {
-		args.set(7, this.waterOverlay.getU0());
-		args.set(8, this.waterOverlay.getV1());
+		this.u0 = this.waterOverlay.getU0();
+		this.u1 = this.waterOverlay.getU1();
+		this.v0 = this.waterOverlay.getV0();
+		this.v1 = this.waterOverlay.getV1();
+		args.set(7, this.u0);
+		args.set(8, this.v1);
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 9))
 	private void sideTextureBottom2(Args args) {
-		args.set(7, this.waterOverlay.getU0());
-		args.set(8, this.waterOverlay.getV0());
+		args.set(7, this.u0);
+		args.set(8, this.v0);
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 10))
 	private void sideTextureBottom3(Args args) {
-		args.set(7, this.waterOverlay.getU1());
-		args.set(8, this.waterOverlay.getV0());
+		args.set(7, this.u1);
+		args.set(8, this.v0);
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 11))
 	private void sideTextureBottom4(Args args) {
-		args.set(7, this.waterOverlay.getU1());
-		args.set(8, this.waterOverlay.getV1());
+		args.set(7, this.u1);
+		args.set(8, this.v1);
 	}
 }
