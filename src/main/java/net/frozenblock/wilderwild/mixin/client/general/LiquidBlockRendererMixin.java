f2b58c315f0bdd7e63afd0a0b530d80176bd6adb
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-14 15:27:18 -0500

    xfrtrex does this work

--- src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
@@ -8,8 +8,12 @@ import net.minecraft.client.renderer.block.LiquidBlockRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,10 +24,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class LiquidBlockRendererMixin {
 
     @Inject(method = "shouldRenderFace", at = @At(value = "HEAD"), cancellable = true)
-    private static void shouldRenderFace(BlockAndTintGetter blockAndTintGetter, BlockPos blockPos, FluidState fluidState, BlockState blockState, Direction direction, FluidState fluidState2, CallbackInfoReturnable<Boolean> info) {
-        if (blockState.getBlock() instanceof MesogleaBlock && direction != Direction.UP && !FrozenBools.HAS_SODIUM) {
+    private static void shouldRenderFace(BlockAndTintGetter blockAndTintGetter, BlockPos blockPos, FluidState fluidState, BlockState blockState, Direction side, FluidState fluidState2, CallbackInfoReturnable<Boolean> info) {
+        if (blockState.getBlock() instanceof MesogleaBlock && side != Direction.UP && !FrozenBools.HAS_SODIUM) {
             info.setReturnValue(false);
         }
     }
+
+	@Inject(method = "isFaceOccludedByState", at = @At(value = "HEAD"), cancellable = true)
+	private static void isFaceOccludedByState(BlockGetter level, Direction face, float height, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> info) {
+		if (state.getBlock() instanceof GlassBlock && face == Direction.UP) {
+			info.setReturnValue(true);
+		}
+	}
 
 }
