5d1ec09f5df0aee5cb689d0ea17bb4d70bf15528
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-15 11:57:08 -0500

    Revert "hhhhh"
    This reverts commit 195455af27a7adfbf6a9d27f66a7638c6b67e19c.

--- src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
+++ src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
@@ -163,10 +163,8 @@ public final class WilderSharedWorldgen {
 		);
 
 		list.add(
-				SurfaceRules.sequence(
-						SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND)),
-						SurfaceRules.ifTrue(SurfaceRules.waterStartCheck(-6, -1), SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, SANDSTONE)))
-				)
+				SurfaceRules.sequence(SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND))),
+						SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, SANDSTONE)))
 		);
 
 		if (ClothConfigInteractionHandler.betaBeaches()) {
@@ -251,8 +249,8 @@ public final class WilderSharedWorldgen {
 
 	public static SurfaceRules.RuleSource oasisRules() {
 		return SurfaceRules.sequence(
-				SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND)),
-				SurfaceRules.ifTrue(SurfaceRules.waterStartCheck(-6, -1), SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, SANDSTONE)))
+				SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SANDSTONE), SAND))),
+				SurfaceRules.ifTrue(SurfaceRules.isBiome(RegisterWorldgen.OASIS), SurfaceRules.ifTrue(SurfaceRules.VERY_DEEP_UNDER_FLOOR, SANDSTONE))
 		);
 	}
 
