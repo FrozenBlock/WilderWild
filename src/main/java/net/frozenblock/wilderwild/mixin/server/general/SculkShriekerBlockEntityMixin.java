06cbd02f93e91f3a8737fab1fa7042cca920f71f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-15 13:40:04 -0500

    fixed shrieker bubbling

--- src/main/java/net/frozenblock/wilderwild/mixin/server/general/SculkShriekerBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/general/SculkShriekerBlockEntityMixin.java
@@ -1,20 +1,25 @@
 package net.frozenblock.wilderwild.mixin.server.general;
 
 import net.frozenblock.wilderwild.misc.WilderSharedConstants;
+import net.frozenblock.wilderwild.misc.interfaces.SculkShriekerTickInterface;
 import net.frozenblock.wilderwild.misc.server.EasyPacket;
 import net.frozenblock.wilderwild.registry.RegisterProperties;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.SculkShriekerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.gameevent.GameEventListener;
+import net.minecraft.world.level.gameevent.vibrations.VibrationListener;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +27,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SculkShriekerBlockEntity.class)
-public class SculkShriekerBlockEntityMixin {
+public class SculkShriekerBlockEntityMixin implements SculkShriekerTickInterface {
+
+	@Shadow
+	private VibrationListener listener;
 
 	@Unique
     public int wilderWild$bubbles;
@@ -53,13 +61,28 @@ public class SculkShriekerBlockEntityMixin {
         if (shrieker.getBlockState().getValue(RegisterProperties.SOULS_TAKEN) == 2) {
             info.cancel();
         } else {
-            if (shrieker.getBlockState().getValue(BlockStateProperties.WATERLOGGED)) {//TODO: fix this. i want it to emit a constant flow of bubbles but it just doesnt
-                if (this.wilderWild$bubbles > 0 && level != null) {
-                    --this.wilderWild$bubbles;
-                    EasyPacket.EasyFloatingSculkBubblePacket.createParticle(level, Vec3.atCenterOf(shrieker.getBlockPos()), Math.random() > 0.7 ? 1 : 0, 20 + WilderSharedConstants.random().nextInt(80), 0.075, level.random.nextIntBetweenInclusive(8, 14));
-                }
+            if (shrieker.getBlockState().getValue(BlockStateProperties.WATERLOGGED)) {
+                this.wilderWild$bubbles = 50;
             }
         }
     }
 
+	@Inject(at = @At("TAIL"), method = "load")
+	public void load(CompoundTag tag, CallbackInfo info) {
+		this.wilderWild$bubbles = tag.getInt("wilderwildBubbles");
+	}
+
+	@Inject(at = @At("TAIL"), method = "saveAdditional")
+	public void saveAdditional(CompoundTag tag, CallbackInfo info) {
+		tag.putInt("wilderwildBubbles", this.wilderWild$bubbles);
+	}
+
+	@Override
+	public void tickServer(Level level, BlockPos pos) {
+		this.listener.tick(level);
+		if (this.wilderWild$bubbles > 0) {
+			--this.wilderWild$bubbles;
+			EasyPacket.EasyFloatingSculkBubblePacket.createParticle(level, Vec3.atCenterOf(pos), Math.random() > 0.7 ? 1 : 0, 20 + WilderSharedConstants.random().nextInt(80), 0.075, level.random.nextIntBetweenInclusive(8, 14));
+		}
+	}
 }
