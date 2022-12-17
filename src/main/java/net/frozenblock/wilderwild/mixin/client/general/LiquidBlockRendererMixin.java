939bf6f51be029f3f65db698b0af00161a0c7bd8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2022-12-17 09:21:33 -0500

    fix lava rendering

--- src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/general/LiquidBlockRendererMixin.java
@@ -1,5 +1,6 @@
 package net.frozenblock.wilderwild.mixin.client.general;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.frozenblock.lib.FrozenBools;
@@ -8,8 +9,11 @@ import net.minecraft.client.renderer.block.LiquidBlockRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,6 +21,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
@@ -43,31 +48,48 @@ public class LiquidBlockRendererMixin {
         }
     }
 
+	@Unique
+	private boolean isWater;
+
+	@Redirect(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;is(Lnet/minecraft/tags/TagKey;)Z"))
+	private boolean getIsWater(FluidState par1, TagKey<Fluid> lava, BlockAndTintGetter level, BlockPos pos, VertexConsumer vertexConsumer, BlockState blockState, FluidState fluidState) {
+		this.isWater = par1.is(FluidTags.WATER);
+		return par1.is(FluidTags.LAVA);
+	}
+
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 8))
 	private void sideTextureBottom1(Args args) {
-		this.wilderWild$u0 = this.waterOverlay.getU0();
-		this.wilderWild$u1 = this.waterOverlay.getU1();
-		this.wilderWild$v0 = this.waterOverlay.getV0();
-		this.wilderWild$v1 = this.waterOverlay.getV1();
-		args.set(7, this.wilderWild$u0);
-		args.set(8, this.wilderWild$v1);
+		if (this.isWater) {
+			this.wilderWild$u0 = this.waterOverlay.getU0();
+			this.wilderWild$u1 = this.waterOverlay.getU1();
+			this.wilderWild$v0 = this.waterOverlay.getV0();
+			this.wilderWild$v1 = this.waterOverlay.getV1();
+			args.set(7, this.wilderWild$u0);
+			args.set(8, this.wilderWild$v1);
+		}
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 9))
 	private void sideTextureBottom2(Args args) {
-		args.set(7, this.wilderWild$u0);
-		args.set(8, this.wilderWild$v0);
+		if (this.isWater) {
+			args.set(7, this.wilderWild$u0);
+			args.set(8, this.wilderWild$v0);
+		}
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 10))
 	private void sideTextureBottom3(Args args) {
-		args.set(7, this.wilderWild$u1);
-		args.set(8, this.wilderWild$v0);
+		if (this.isWater) {
+			args.set(7, this.wilderWild$u1);
+			args.set(8, this.wilderWild$v0);
+		}
 	}
 
 	@ModifyArgs(method = "tesselate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/LiquidBlockRenderer;vertex(Lcom/mojang/blaze3d/vertex/VertexConsumer;DDDFFFFFI)V", ordinal = 11))
 	private void sideTextureBottom4(Args args) {
-		args.set(7, this.wilderWild$u1);
-		args.set(8, this.wilderWild$v1);
+		if (this.isWater) {
+			args.set(7, this.wilderWild$u1);
+			args.set(8, this.wilderWild$v1);
+		}
 	}
 }
