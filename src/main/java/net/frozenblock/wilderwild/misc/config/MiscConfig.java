e476ae26ec6955419a082a2a8c5d0a3984da9d53
Treetrain1 <treetrain1@gmail.com>
2022-12-17 12:19:51 -0600

    add misc tab

--- src/main/java/net/frozenblock/wilderwild/misc/config/MiscConfig.java
+++ src/main/java/net/frozenblock/wilderwild/misc/config/MiscConfig.java
@@ -1,18 +1,20 @@
 package net.frozenblock.wilderwild.misc.config;
-/*
+
+import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.frozenblock.lib.config.FrozenConfig;
+import net.frozenblock.lib.config.api.FrozenConfig;
 import net.frozenblock.wilderwild.WilderWild;
+import net.frozenblock.wilderwild.misc.WilderSharedConstants;
 import static net.frozenblock.wilderwild.misc.config.WilderWildConfig.text;
 import static net.frozenblock.wilderwild.misc.config.WilderWildConfig.tooltip;
 
 @Config(name = "misc")
-public final class MiscConfig {
+public final class MiscConfig implements ConfigData {
 
 	@ConfigEntry.Gui.CollapsibleObject
 	public BiomeAmbienceConfig biomeAmbience = new BiomeAmbienceConfig();
@@ -59,4 +61,4 @@ public final class MiscConfig {
 				deepDarkAmbience, dripstoneCavesAmbience
 		);
 	}
-}*/
+}
