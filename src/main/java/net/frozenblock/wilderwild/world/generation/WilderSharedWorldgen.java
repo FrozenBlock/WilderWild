813e1edfe43b4b76f09d61b0d57f684b4bd0d71a
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-01 17:47:19 -0500

    Revert "live enabling and disabling of beta beaches"
    This reverts commit 2b79378ff63ccea581075a6df92c616b0b81f4b3.

--- src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
+++ src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
@@ -129,11 +129,6 @@ public final class WilderSharedWorldgen {
 		list.add(cypressSurfaceRules());
 		list.add(warmRiverRules());
 		list.add(oasisRules());
-		return FrozenSurfaceRules.sequence(list);
-	}
-
-	public static SurfaceRules.SequenceRuleSource liveSurfaceRules() {
-		List<SurfaceRules.RuleSource> list = new ArrayList<>();
 		if (ClothConfigInteractionHandler.betaBeaches()) {
 			list.add(gravelBetaBeaches());
 			list.add(sandBetaBeaches());
