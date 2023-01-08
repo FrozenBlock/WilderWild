1991de4c71d22f12f823b77aad168435b7088074
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-07 19:44:22 -0500

    Fixed!!

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterSurfaceRules.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterSurfaceRules.java
@@ -1,7 +1,7 @@
 package net.frozenblock.wilderwild.registry;
 
 import java.util.ArrayList;
-import net.frozenblock.lib.worldgen.surface.api.FrozenPresetBoundRuleSource;
+import net.frozenblock.lib.worldgen.surface.api.FrozenDimensionBoundRuleSource;
 import net.frozenblock.lib.worldgen.surface.api.FrozenSurfaceRuleEntrypoint;
 import net.frozenblock.wilderwild.misc.WilderSharedConstants;
 import net.frozenblock.wilderwild.world.generation.WilderSharedWorldgen;
@@ -26,7 +26,7 @@ public final class RegisterSurfaceRules implements FrozenSurfaceRuleEntrypoint {
 	}
 
 	@Override
-	public void addSurfaceRules(ArrayList<FrozenPresetBoundRuleSource> context) {
+	public void addSurfaceRules(ArrayList<FrozenDimensionBoundRuleSource> context) {
 
 	}
 }
