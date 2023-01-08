2b746b35bd9513630cde97a7e97cfbf777295ef4
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-08 17:05:58 -0500

    live-changing beta beaches pogger

--- src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
+++ src/main/java/net/frozenblock/wilderwild/world/generation/WilderSharedWorldgen.java
@@ -15,6 +15,7 @@ import net.frozenblock.wilderwild.misc.config.ClothConfigInteractionHandler;
 import net.frozenblock.wilderwild.registry.RegisterWorldgen;
 import net.frozenblock.wilderwild.registry.WilderRegistry;
 import net.frozenblock.wilderwild.tag.WilderBiomeTags;
+import net.frozenblock.wilderwild.world.generation.conditionsource.BetaBeachConditionSource;
 import net.frozenblock.wilderwild.world.generation.noise.WilderNoise;
 import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.block.Block;
@@ -206,8 +207,8 @@ public final class WilderSharedWorldgen {
 		);
 	}
 
-	public static SurfaceRules.SequenceRuleSource betaBeaches() {
-		return (SurfaceRules.SequenceRuleSource) SurfaceRules.sequence(gravelBetaBeaches(), sandBetaBeaches(), multilayerSandBetaBeaches());
+	public static SurfaceRules.RuleSource betaBeaches() {
+		return SurfaceRules.ifTrue(BetaBeachConditionSource.betaBeachConditionSource(1), SurfaceRules.sequence(gravelBetaBeaches(), sandBetaBeaches(), multilayerSandBetaBeaches()));
 	}
 
 	public static SurfaceRules.RuleSource gravelBetaBeaches() {
