69f9d4ecc13dc45580bdc651c43f569ff5a0d8be
Treetrain1 <treetrain1@gmail.com>
2023-01-13 20:55:51 -0600

    merge

--- src/main/java/net/frozenblock/wilderwild/world/generation/conditionsource/BetaBeachConditionSource.java
+++ src/main/java/net/frozenblock/wilderwild/world/generation/conditionsource/BetaBeachConditionSource.java
@@ -1,4 +1,4 @@
-package net.frozenblock.wilderwild.world.gen.conditionsource;
+package net.frozenblock.wilderwild.world.generation.conditionsource;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
