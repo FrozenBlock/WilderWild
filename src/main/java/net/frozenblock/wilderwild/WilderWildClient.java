9aee64ed93de801491bee5372bd22daae62eb845
Soro335 <gabeonelove335@gmail.com>
2023-01-22 00:58:23 +0300

    badlands color

--- src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
+++ src/main/java/net/frozenblock/wilderwild/WilderWildClient.java
@@ -263,6 +263,9 @@ public final class WilderWildClient implements ClientModInitializer {
 		ColorProviderRegistry.BLOCK.register(((state, level, pos, tintIndex) ->
 			BiomeColors.getAverageFoliageColor(Objects.requireNonNull(level), Objects.requireNonNull(pos))
 		), RegisterBlocks.POTTED_GRASS);
+		ColorProviderRegistry.BLOCK.register(((state, level, pos, tintIndex) ->
+			BiomeColors.getAverageFoliageColor(Objects.requireNonNull(level), Objects.requireNonNull(pos))
+		), RegisterBlocks.BUSH);
 	}
 
 	private static void receiveAncientHornProjectilePacket() {
--- src/main/java/net/frozenblock/wilderwild/world/additions/gen/WilderBiomeSettings.java
+++ src/main/java/net/frozenblock/wilderwild/world/additions/gen/WilderBiomeSettings.java
@@ -3,6 +3,7 @@ package net.frozenblock.wilderwild.world.additions.gen;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.frozenblock.wilderwild.misc.WilderSharedConstants;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
 
 public final class WilderBiomeSettings {
@@ -11,6 +12,10 @@ public final class WilderBiomeSettings {
         BiomeModifications.create(WilderSharedConstants.id("replace_deep_dark_fog")).add(ModificationPhase.REPLACEMENTS,
                 (context) -> context.getBiomeKey().equals(Biomes.DEEP_DARK),
                 (modificationContext) -> modificationContext.getEffects().setFogColor(0));
+
+		BiomeModifications.create(WilderSharedConstants.id("replace_badlands_foliage_color")).add(ModificationPhase.REPLACEMENTS,
+				(context) -> context.hasTag(BiomeTags.IS_BADLANDS),
+				(modificationContext) -> modificationContext.getEffects().setFoliageColor(11445290));
 
         WilderMusic.playMusic();
         WilderSpawns.addFireflies();
