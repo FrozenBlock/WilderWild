1285bc9eac5f8db8a73e144ab7729409e1b7c011
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-22 19:18:23 -0500

    ok i think supply chests lag bruhhh

--- src/main/java/net/frozenblock/wilderwild/mixin/server/general/RandomizableContainerBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/general/RandomizableContainerBlockEntityMixin.java
@@ -36,7 +36,7 @@ public class RandomizableContainerBlockEntityMixin {
 		Level level = blockEntity.getLevel();
 		if (this.lootTable != null && level != null && level.getServer() != null && blockEntity instanceof ChestBlockEntity chest) {
 			BlockState state = level.getBlockState(blockEntity.getBlockPos());
-			WilderSharedConstants.log("CHEST 1", WilderSharedConstants.UNSTABLE_LOGGING);
+			WilderSharedConstants.log("LOOT TABLE: " + lootTable.toString(), WilderSharedConstants.UNSTABLE_LOGGING);
 			if (state.hasProperty(BlockStateProperties.WATERLOGGED) && state.getValue(BlockStateProperties.WATERLOGGED)) {
 				WilderSharedConstants.log("BEFORE BUBBLES", WilderSharedConstants.UNSTABLE_LOGGING);
 				((ChestBlockEntityInterface) chest).setCanBubble(true);
@@ -57,6 +57,8 @@ public class RandomizableContainerBlockEntityMixin {
 					jellyfish.setDeltaMovement(0, 0.1 + level.random.nextDouble() * 0.07, 0);
 					level.addFreshEntity(jellyfish);
 					WilderSharedConstants.log("SPAWNED JELLY", WilderSharedConstants.UNSTABLE_LOGGING);
+				} else {
+					WilderSharedConstants.log("NO JELLY", WilderSharedConstants.UNSTABLE_LOGGING);
 				}
 			}
 		}
