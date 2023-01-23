586a001210cac4f83e5eaddfc0bba7de9e90c8d4
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-22 19:14:43 -0500

    WHY DOES IT LAG

--- src/main/java/net/frozenblock/wilderwild/mixin/server/general/RandomizableContainerBlockEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/general/RandomizableContainerBlockEntityMixin.java
@@ -2,6 +2,7 @@ package net.frozenblock.wilderwild.mixin.server.general;
 
 import net.frozenblock.lib.math.api.EasyNoiseSampler;
 import net.frozenblock.wilderwild.entity.Jellyfish;
+import net.frozenblock.wilderwild.misc.WilderSharedConstants;
 import net.frozenblock.wilderwild.misc.interfaces.ChestBlockEntityInterface;
 import net.frozenblock.wilderwild.registry.RegisterEntities;
 import net.minecraft.core.BlockPos;
@@ -35,9 +36,13 @@ public class RandomizableContainerBlockEntityMixin {
 		Level level = blockEntity.getLevel();
 		if (this.lootTable != null && level != null && level.getServer() != null && blockEntity instanceof ChestBlockEntity chest) {
 			BlockState state = level.getBlockState(blockEntity.getBlockPos());
+			WilderSharedConstants.log("CHEST 1", WilderSharedConstants.UNSTABLE_LOGGING);
 			if (state.hasProperty(BlockStateProperties.WATERLOGGED) && state.getValue(BlockStateProperties.WATERLOGGED)) {
+				WilderSharedConstants.log("BEFORE BUBBLES", WilderSharedConstants.UNSTABLE_LOGGING);
 				((ChestBlockEntityInterface) chest).setCanBubble(true);
+				WilderSharedConstants.log("SET BUBBLES TRUE", WilderSharedConstants.UNSTABLE_LOGGING);
 				if (this.lootTable.getPath().toLowerCase().contains("shipwreck") && EasyNoiseSampler.threadSafeRandom.nextBoolean()) {
+					WilderSharedConstants.log("MAKING JELLY", WilderSharedConstants.UNSTABLE_LOGGING);
 					Jellyfish jellyfish = new Jellyfish(RegisterEntities.JELLYFISH, level);
 					BlockPos chestPos = chest.getBlockPos();
 					jellyfish.setVariantFromPos(level, chestPos);
@@ -51,6 +56,7 @@ public class RandomizableContainerBlockEntityMixin {
 					jellyfish.setPos(chestPos.getX() + 0.5 + additionalX, chestPos.getY() + 0.75, chestPos.getZ() + 0.5 + additionalZ);
 					jellyfish.setDeltaMovement(0, 0.1 + level.random.nextDouble() * 0.07, 0);
 					level.addFreshEntity(jellyfish);
+					WilderSharedConstants.log("SPAWNED JELLY", WilderSharedConstants.UNSTABLE_LOGGING);
 				}
 			}
 		}
