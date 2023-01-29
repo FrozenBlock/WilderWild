65a068a0ed22a837cd61fae8fbe75e62a71c22b8
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-01-29 18:30:37 -0500

    jellies dont drop items from falls

--- src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
@@ -378,10 +378,12 @@ public class Jellyfish extends NoFlopAbstractFish {
 
     @Override
     protected void dropFromLootTable(@NotNull DamageSource damageSource, boolean bl) {
-        ResourceLocation resourceLocation = this.getJellyLootTable();
-        LootTable lootTable = Objects.requireNonNull(this.level.getServer()).getLootTables().get(resourceLocation);
-        LootContext.Builder builder = this.createLootContext(bl, damageSource);
-        lootTable.getRandomItems(builder.create(LootContextParamSets.ENTITY), this::spawnAtLocation);
+		if (damageSource != DamageSource.FALL) {
+			ResourceLocation resourceLocation = this.getJellyLootTable();
+			LootTable lootTable = Objects.requireNonNull(this.level.getServer()).getLootTables().get(resourceLocation);
+			LootContext.Builder builder = this.createLootContext(bl, damageSource);
+			lootTable.getRandomItems(builder.create(LootContextParamSets.ENTITY), this::spawnAtLocation);
+		}
     }
 
     @Override
