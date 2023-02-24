cecee343c972ba05ac9e8adb555f17b70caf6048
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-23 19:35:15 -0500

    untested cactus placement thing

--- src/main/java/net/frozenblock/wilderwild/mixin/server/general/CactusBlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/general/CactusBlockMixin.java
@@ -18,28 +18,18 @@
 
 package net.frozenblock.wilderwild.mixin.server.general;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.BigDripleafBlock;
-import net.minecraft.world.level.block.BigDripleafStemBlock;
 import net.minecraft.world.level.block.CactusBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Material;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CactusBlock.class)
 public final class CactusBlockMixin {
 
-	@Inject(at = @At("RETURN"), method = "getStateForPlacement", cancellable = true)
-	public void wilderWild$getStateForPlacement(BlockPlaceContext context, CallbackInfoReturnable<BlockState> info) {
-		if (BlockBehaviour.class.cast(this) instanceof BigDripleafStemBlock bigDripleafStemBlock) {
-			info.setReturnValue(bigDripleafStemBlock.defaultBlockState().setValue(BlockStateProperties.POWERED, context.getLevel().hasNeighborSignal(context.getClickedPos())));
-		} else if (BlockBehaviour.class.cast(this) instanceof BigDripleafBlock bigDripleafBlock) {
-			info.setReturnValue(bigDripleafBlock.defaultBlockState().setValue(BlockStateProperties.POWERED, context.getLevel().hasNeighborSignal(context.getClickedPos())));
-		}
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/Material;isSolid()Z"), method = "canSurvive")
+	public boolean wilderWild$canSurviveIsSolid(Material par1) {
+		return true;
 	}
 
 }
