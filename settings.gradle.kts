75b56b91bae622a604be7a188d1b67ffb9f72644
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-02-26 13:53:46 -0500

    Revert "better local repository"
    This reverts commit 0f8f1c93a01534625ccad8a080f6d63a46050f68.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,16 +29,15 @@ rootProject.name = "Wilder Wild"
 
 localRepository("FrozenLib", "WilderWild")
 
-val allowLocalModUse = true
-val allowLocalModInConsoleMode = true
-
 val androidInjectedInvokedFromIde by extra("android.injected.invoked.from.ide")
 val xpcServiceName by extra("XPC_SERVICE_NAME")
 val ideaInitialDirectory by extra("IDEA_INITIAL_DIRECTORY")
-
-val isIDE = androidInjectedInvokedFromIde != "" || (System.getenv(xpcServiceName) ?: "").contains("intellij") || (System.getenv(xpcServiceName) ?: "").contains(".idea") || System.getenv(ideaInitialDirectory) != null
+val isIDE = !androidInjectedInvokedFromIde.equals(null) || (System.getenv(xpcServiceName) ?: "").contains("intellij") || (System.getenv(xpcServiceName) ?: "").contains(".idea") || System.getenv(ideaInitialDirectory) != null
 
 fun localRepository(mod: String, projectFileName: String) {
+	val allowLocalModUse = true
+	val allowLocalModInConsoleMode = true
+
 	val path = "../$mod"
     val file = File(path)
 
@@ -47,15 +46,13 @@ fun localRepository(mod: String, projectFileName: String) {
 
     val prefixedModName = ":$mod"
 
-    if (allowLocalModUse && (isIDE || allowLocalModInConsoleMode)) {
-        if (file.exists()) {
-            include(prefixedModName)
-            project(prefixedModName).projectDir = file
-            project(prefixedModName).buildFileName = "./build.gradle"
-        } else if (fileGitHub.exists()) {
-            include(prefixedModName)
-            project(prefixedModName).projectDir = fileGitHub
-            project(prefixedModName).buildFileName = "./build.gradle"
-        }
+	if (allowLocalModUse && (isIDE || allowLocalModInConsoleMode) && file.exists()) {
+		include(prefixedModName)
+        project(prefixedModName).projectDir = file
+        project(prefixedModName).buildFileName = "./build.gradle"
+	} else if (allowLocalModUse && (isIDE || allowLocalModInConsoleMode) && fileGitHub.exists()) {
+		include(prefixedModName)
+        project(prefixedModName).projectDir = fileGitHub
+        project(prefixedModName).buildFileName = "./build.gradle"
 	}
 }
