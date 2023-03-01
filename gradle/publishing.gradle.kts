61d6fb580acf2aa4ad5c10a8160997c5fafa3e33
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-03-01 13:13:32 -0600

    no way

--- gradle/publishing.gradle.kts
+++ gradle/publishing.gradle.kts
@@ -17,18 +17,13 @@ extra {
 	properties.forEach { (a, b) ->
 		project.extra[a] = b
 	}
-
-	modrinth_version = makeModrinthVersion(mod_version)
-	display_name = makeName(mod_version)
-
-	changelog_text = getChangelog(file(changelog_file))
 }
 
 public val modrinth_id by extra("modrinth_id")
 public val curseforge_id by extra("curseforge_id")
 public val release_type by extra("release_type")
 public val curseforge_minecraft_version by extra("curseforge_minecraft_version")
-public val changelog_file by extra("changelog_file)
+public val changelog_file by extra("changelog_file")
 
 public val modrinth_version = makeModrinthVersion(mod_version)
 public val display_name = makeName(mod_version)
