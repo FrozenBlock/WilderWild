57dd9092117dec8ca8621b9fd15cb3b88d1cc5da
Treetrain1 <treetrain1@gmail.com>
2023-03-01 20:26:48 -0600

    update ext mods

--- build.gradle.kts
+++ build.gradle.kts
@@ -236,7 +236,7 @@ dependencies {
     }
 
     // NBT Crafting
-    modImplementation("maven.modrinth:nbt-crafting:${nbtcrafting_version}")?.let { include(it) }
+    modImplementation("com.github.Treetrain1:nbt-crafting:jitpack-SNAPSHOT")?.let { include(it) }
 
     // CaffeineConfig
     modImplementation("net.caffeinemc:mixin-config:1.0.0+1.17")?.let { include(it) }
