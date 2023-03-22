2cf8be325887dad2e91f310c4e66da63f10eb8a7
Treetrain1 <treetrain1@gmail.com>
2023-03-22 03:33:49 -0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -246,7 +246,7 @@ dependencies {
     }
 
     // Brush Extender
-    modImplementation("com.github.AViewFromTheTop:BrushExtender:main-SNAPSHOT")?.let { include(it) }
+    modImplementation("com.github.AViewFromTheTop:BrushExtender:7d6f333e40")?.let { include(it) }
 
     // NBT Crafting
     modImplementation("com.github.Treetrain1:nbt-crafting:jitpack-1.19.4-SNAPSHOT")?.let { include(it) }
