542e72ce122d3457b3c4f049709fc560a0529700
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-07-06 23:48:52 -0400

    temp jelly food

--- src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Jellyfish.java
@@ -26,6 +26,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import net.frozenblock.lib.entity.api.NoFlopAbstractFish;
 import net.frozenblock.wilderwild.entity.ai.jellyfish.JellyfishAi;
+import net.frozenblock.wilderwild.entity.ai.jellyfish.JellyfishTemptGoal;
 import net.frozenblock.wilderwild.entity.variant.JellyfishVariant;
 import net.frozenblock.wilderwild.misc.server.EasyPacket;
 import net.frozenblock.wilderwild.registry.RegisterEntities;
@@ -49,6 +50,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.Difficulty;
@@ -72,13 +74,23 @@ import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.FollowParentGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.PanicGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
@@ -128,6 +140,11 @@ public class Jellyfish extends NoFlopAbstractFish {
 	public Jellyfish(@NotNull EntityType<? extends Jellyfish> entityType, @NotNull Level level) {
 		super(entityType, level);
 		this.getNavigation().setCanFloat(false);
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal(3, new JellyfishTemptGoal(this, 1.25));
 	}
 
 	public static int getJellyfish(@NotNull ServerLevel level, boolean pearlescent) {
@@ -499,7 +516,7 @@ public class Jellyfish extends NoFlopAbstractFish {
 		if (itemStack.is(Items.WATER_BUCKET)) {
 			return super.mobInteract(player, hand);
 		}
-		if (!itemStack.is(WilderItemTags.NEMATOCYSTS) || (this.getVariant().pearlescent() && !itemStack.is(WilderItemTags.PEARLESCENT_NEMATOCYSTS)) || (this.getVariant().isNormal() && itemStack.is(WilderItemTags.PEARLESCENT_NEMATOCYSTS))) {
+		if (!itemStack.is(this.getVariant().reproductionFood())) {
 			return InteractionResult.PASS;
 		}
 		if (this.isBaby()) {
---
+++ src/main/java/net/frozenblock/wilderwild/entity/ai/jellyfish/JellyfishTemptGoal.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2022-2023 FrozenBlock
+ * This file is part of Wilder Wild.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.frozenblock.wilderwild.entity.ai.jellyfish;
+
+import java.util.EnumSet;
+import net.frozenblock.wilderwild.entity.Jellyfish;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
+
+public class JellyfishTemptGoal extends Goal {
+	private static final TargetingConditions TEMP_TARGETING = TargetingConditions.forNonCombat().range(10.0).ignoreLineOfSight();
+	private final TargetingConditions targetingConditions;
+	protected final Jellyfish mob;
+	private final double speedModifier;
+	@Nullable
+	protected Player player;
+	private int calmDown;
+	private boolean isRunning;
+
+	public JellyfishTemptGoal(Jellyfish mob, double speedModifier) {
+		this.mob = mob;
+		this.speedModifier = speedModifier;
+		this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+		this.targetingConditions = TEMP_TARGETING.copy().selector(this::shouldFollow);
+	}
+
+	@Override
+	public boolean canUse() {
+		if (this.mob.getTarget() != null) {
+			return false;
+		}
+		if (this.calmDown > 0) {
+			--this.calmDown;
+			return false;
+		}
+		this.player = this.mob.level().getNearestPlayer(this.targetingConditions, this.mob);
+		return this.player != null;
+	}
+
+	private boolean shouldFollow(LivingEntity entity) {
+		TagKey<Item> tag = this.mob.getVariant().reproductionFood();
+		return entity.getMainHandItem().is(tag) || entity.getOffhandItem().is(tag);
+	}
+
+	@Override
+	public void start() {
+		this.isRunning = true;
+	}
+
+	@Override
+	public void stop() {
+		this.player = null;
+		this.mob.getNavigation().stop();
+		this.calmDown = JellyfishTemptGoal.reducedTickDelay(100);
+		this.isRunning = false;
+	}
+
+	@Override
+	public void tick() {
+		this.mob.getLookControl().setLookAt(this.player, this.mob.getMaxHeadYRot() + 20, this.mob.getMaxHeadXRot());
+		if (this.mob.distanceToSqr(this.player) < 6.25) {
+			this.mob.getNavigation().stop();
+		} else {
+			this.mob.moveToAccurate(this.player, this.speedModifier);
+		}
+	}
+	public boolean isRunning() {
+		return this.isRunning;
+	}
+}
+
