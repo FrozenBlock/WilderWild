865c6e9416c0a3df209dde2c21cc6a54bd7b8fea
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-08-14 00:11:29 -0400

    Update WilderPreMixinInjectConstants.java

--- src/main/java/net/frozenblock/wilderwild/misc/WilderPreMixinInjectConstants.java
+++ src/main/java/net/frozenblock/wilderwild/misc/WilderPreMixinInjectConstants.java
@@ -6,7 +6,7 @@ import net.frozenblock.wilderwild.mixin.WilderWildMixinPlugin;
 /**
  * This class was created to fix issue #289: https://github.com/FrozenBlock/WilderWild/issues/289.
  * <p>
- * If a variable is going to be loaded prior to Mixin Injection (e.g. {@link WilderWildMixinPlugin}, please put said variable here instead of {@link WilderSharedConstants}.
+ * If a variable is going to be loaded prior to Mixin Injection (e.g. {@link WilderWildMixinPlugin},) please put said variable here instead of {@link WilderSharedConstants}.
  */
 public class WilderPreMixinInjectConstants {
 	public static final boolean HAS_FALLINGLEAVES = FabricLoader.getInstance().isModLoaded("fallingleaves");
