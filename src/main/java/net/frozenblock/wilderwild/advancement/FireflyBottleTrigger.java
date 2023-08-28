73781eb40c51eced54c14228055817cf70deb98f
Treetrain1 <treetrain1@gmail.com>
2023-08-27 21:33:54 -0500

    23w33a

--- src/main/java/net/frozenblock/wilderwild/advancement/FireflyBottleTrigger.java
+++ src/main/java/net/frozenblock/wilderwild/advancement/FireflyBottleTrigger.java
@@ -24,12 +24,12 @@ import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.DeserializationContext;
 import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.advancements.critereon.SerializationContext;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import java.util.Optional;
 
 public class FireflyBottleTrigger extends SimpleCriterionTrigger<FireflyBottleTrigger.TriggerInstance> {
 
@@ -43,38 +43,38 @@ public class FireflyBottleTrigger extends SimpleCriterionTrigger<FireflyBottleTr
 
 	@Override
 	@NotNull
-	public TriggerInstance createInstance(@NotNull JsonObject jsonObject, @NotNull ContextAwarePredicate contextAwarePredicate, @NotNull DeserializationContext deserializationContext) {
-		ItemPredicate itemPredicate = ItemPredicate.fromJson(jsonObject.get("item"));
+	public TriggerInstance createInstance(@NotNull JsonObject jsonObject, @NotNull Optional<ContextAwarePredicate> contextAwarePredicate, @NotNull DeserializationContext deserializationContext) {
+		Optional<ItemPredicate> itemPredicate = ItemPredicate.fromJson(jsonObject.get("item"));
 		return new TriggerInstance(itemPredicate, contextAwarePredicate);
 	}
 
 	public void trigger(@NotNull ServerPlayer player, @NotNull ItemStack stack) {
-		this.trigger(player, (conditions) -> conditions.matches(stack));
+		this.trigger(player, conditions -> conditions.matches(stack));
 	}
 
 	public static class TriggerInstance
 		extends AbstractCriterionTriggerInstance {
-		private final ItemPredicate item;
+		private final Optional<ItemPredicate> item;
 
-		public TriggerInstance(@NotNull ItemPredicate item, @NotNull ContextAwarePredicate contextAwarePredicate) {
+		public TriggerInstance(@NotNull Optional<ItemPredicate> item, @NotNull Optional<ContextAwarePredicate> contextAwarePredicate) {
 			super(ID, contextAwarePredicate);
 			this.item = item;
 		}
 
 		@NotNull
 		public static TriggerInstance fireflyBottle(@NotNull ItemPredicate item) {
-			return new TriggerInstance(item, ContextAwarePredicate.ANY);
+			return new TriggerInstance(Optional.of(item), Optional.empty());
 		}
 
 		public boolean matches(@NotNull ItemStack stack) {
-			return this.item.matches(stack);
-		}
+            return this.item.map(itemPredicate -> itemPredicate.matches(stack)).orElse(false);
+        }
 
 		@Override
 		@NotNull
-		public JsonObject serializeToJson(@NotNull SerializationContext context) {
-			JsonObject jsonObject = super.serializeToJson(context);
-			jsonObject.add("item", this.item.serializeToJson());
+		public JsonObject serializeToJson() {
+			JsonObject jsonObject = super.serializeToJson();
+			this.item.ifPresent(itemPredicate -> jsonObject.add("item", itemPredicate.serializeToJson()));
 			return jsonObject;
 		}
 	}
