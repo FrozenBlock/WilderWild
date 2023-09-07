68535585ea1df29ccc232ec3c7f6fcf00b183c55
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2023-09-08 00:04:29 +0200

    preventing another null pointer exception

--- src/main/java/net/frozenblock/wilderwild/mixin/server/sculk/SculkBlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/sculk/SculkBlockMixin.java
@@ -219,6 +219,9 @@ public abstract class SculkBlockMixin {
 
 	@Inject(method = "attemptUseCharge", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V", shift = At.Shift.AFTER))
 	private void wilderWild$handlePlacement(SculkSpreader.ChargeCursor chargeCursor, LevelAccessor levelAccessor, BlockPos blockPos, RandomSource randomSource, SculkSpreader sculkSpreader, boolean bl, CallbackInfoReturnable<Integer> cir) {
+		if(this.wilderWild$placedState == null) {
+			return;
+		}
 		if (this.wilderWild$isWorldgen && this.wilderWild$placedState.getBlock() instanceof OsseousSculkBlock osseousSculkBlock) {
 			int growthAmount = Math.max(0, this.wilderWild$placedState.getValue(OsseousSculkBlock.HEIGHT_LEFT) - this.wilderWild$randomSource.nextInt(2));
 			for (int a = 0; a < growthAmount; a++) {
