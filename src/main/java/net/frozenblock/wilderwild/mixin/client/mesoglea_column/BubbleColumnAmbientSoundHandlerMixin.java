47af0cf27933401c4db04244321e08d62572bbea
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-09-07 00:01:34 -0400

    fix

--- src/main/java/net/frozenblock/wilderwild/mixin/client/mesoglea_column/BubbleColumnAmbientSoundHandlerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/mesoglea_column/BubbleColumnAmbientSoundHandlerMixin.java
@@ -23,8 +23,10 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.frozenblock.wilderwild.block.MesogleaBlock;
 import net.minecraft.client.resources.sounds.BubbleColumnAmbientSoundHandler;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BubbleColumnBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -32,18 +34,18 @@ import org.spongepowered.asm.mixin.injection.At;
 public class BubbleColumnAmbientSoundHandlerMixin {
 
 	@WrapOperation(method = "method_29714(Lnet/minecraft/world/level/block/state/BlockState;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 0))
-	private static boolean wilderWild$filterWithMesoglea(BlockState state, Operation<Boolean> operation) {
-		return operation.call(state) || MesogleaBlock.hasBubbleColumn(state);
+	private static boolean wilderWild$filterWithMesoglea(BlockState state, Block block, Operation<Boolean> operation) {
+		return operation.call(state, block) || MesogleaBlock.hasBubbleColumn(state);
 	}
 
 	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 0))
-	private static boolean wilderWild$mesogleaAndBubbleCheck(BlockState state, Operation<Boolean> operation) {
-		return operation.call(state) || MesogleaBlock.hasBubbleColumn(state);
+	private static boolean wilderWild$mesogleaAndBubbleCheck(BlockState state, Block block, Operation<Boolean> operation) {
+		return operation.call(state, block) || MesogleaBlock.hasBubbleColumn(state);
 	}
 
 	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getValue(Lnet/minecraft/world/level/block/state/properties/Property;)Ljava/lang/Comparable;", ordinal = 0))
-	private static boolean wilderWild$blockStateValueCheckWithMesoglea(BlockState state, Operation<Boolean> operation) {
-		return state.hasProperty(BubbleColumnBlock.DRAG_DOWN) ? operation.call(state) : MesogleaBlock.isDraggingDown(state);
+	private static boolean wilderWild$blockStateValueCheckWithMesoglea(BlockState state, Property<?> property, Operation<Boolean> operation) {
+		return state.hasProperty(BubbleColumnBlock.DRAG_DOWN) ? operation.call(state, property) : MesogleaBlock.isDraggingDown(state);
 	}
 
 }
