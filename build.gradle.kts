37c4314b314421cb4ac6502d470a9f1947923b1a
Treetrain1 <treetrain1@gmail.com>
2023-09-27 17:40:30 -0500

    remove vineflower plugin bc fabric loom 1.4 adds it

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ buildscript {
 
 plugins {
     id("fabric-loom") version("+")
-    id("io.github.juuxel.loom-vineflower") version("+")
     id("org.quiltmc.gradle.licenser") version("+")
     id("org.ajoberstar.grgit") version("+")
     id("com.modrinth.minotaur") version("+")
