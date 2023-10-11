cc7e2ce696b1d3b36e0b03b7410244d9dd4bdcce
Treetrain1 <treetrain1@gmail.com>
2023-10-10 19:56:48 -0500

    Revert "typo"
    This reverts commit 2761f5bc1f155f310f30f1d72e6aa11236f3dbb4.

--- src/main/kotlin/net/frozenblock/wilderwild/WilderWildConfigUtils.kt
+++ src/main/kotlin/net/frozenblock/wilderwild/WilderWildConfigUtils.kt
@@ -2,7 +2,7 @@ package net.frozenblock.wilderwild
 
 import java.nio.file.Path
 
-// this file is to make sure mc classes aren't loaded early
+// this file is to make sure mc classes arent loaded early
 
 fun configPath(name: String, json5: Boolean = true): Path
         = Path.of("./config/$MOD_ID/$name.${if (json5) "json5" else "json"}")
