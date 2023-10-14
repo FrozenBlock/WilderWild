405edad379227ec0220e42ea0dd2073bf950e690
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-14 00:28:06 -0400

    fix mixin

--- src/main/java/net/frozenblock/wilderwild/mixin/server/crab/VibrationSystemTickerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/server/crab/VibrationSystemTickerMixin.java
@@ -3,7 +3,7 @@ package net.frozenblock.wilderwild.mixin.server.crab;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.frozenblock.wilderwild.entity.Crab;
-import net.minecraft.core.particles.VibrationParticleOption;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.gameevent.vibrations.VibrationInfo;
 import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public interface VibrationSystemTickerMixin {
 
 	@WrapOperation(method = "method_51408", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"))
-	private static int wilderWild$removeParticleIfCrab(ServerLevel usedLevel, VibrationParticleOption vibrationParticleOption, double x, double y, double z, int count, double xOffset, double yOffset, double zOffset, double speed, Operation<Integer> operation, ServerLevel level, VibrationSystem.Data data, VibrationSystem.User user, VibrationInfo vibrationInfo) {
+	private static int wilderWild$removeParticleIfCrab(ServerLevel usedLevel, ParticleOptions vibrationParticleOption, double x, double y, double z, int count, double xOffset, double yOffset, double zOffset, double speed, Operation<Integer> operation, VibrationSystem.Data data, VibrationSystem.User user, ServerLevel level, VibrationInfo vibrationInfo) {
 		if (!(user instanceof Crab.VibrationUser)) {
 			return operation.call(usedLevel, vibrationParticleOption, x, y, z, count, xOffset, yOffset, zOffset, speed);
 		}
@@ -22,7 +22,7 @@ public interface VibrationSystemTickerMixin {
 	}
 
 	@WrapOperation(method = "tryReloadVibrationParticle", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"))
-	private static int wilderWild$removeParticleFromReloadIfCrab(ServerLevel usedLevel, VibrationParticleOption vibrationParticleOption, double x, double y, double z, int count, double xOffset, double yOffset, double zOffset, double speed, Operation<Integer> operation, ServerLevel level, VibrationSystem.Data data, VibrationSystem.User user) {
+	private static int wilderWild$removeParticleFromReloadIfCrab(ServerLevel usedLevel, ParticleOptions vibrationParticleOption, double x, double y, double z, int count, double xOffset, double yOffset, double zOffset, double speed, Operation<Integer> operation, ServerLevel level, VibrationSystem.Data data, VibrationSystem.User user) {
 		if (!(user instanceof Crab.VibrationUser)) {
 			return operation.call(usedLevel, vibrationParticleOption, x, y, z, count, xOffset, yOffset, zOffset, speed);
 		}
