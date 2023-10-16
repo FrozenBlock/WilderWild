07d9f08c4c4206d5bf255fa61bee4e15a5136489
Treetrain1 <treetrain1@gmail.com>
2023-10-15 22:33:19 -0500

    fix processed minecraft version

--- build.gradle.kts
+++ build.gradle.kts
@@ -309,7 +309,7 @@ tasks {
         val properties = HashMap<String, Any>()
         properties["mod_id"] = mod_id
         properties["version"] = version
-        properties["minecraft_version"] = "~1.20.2-"
+        properties["minecraft_version"] = "~$minecraft_version-"
 
         properties["fabric_loader_version"] = ">=$loader_version"
         properties["fabric_api_version"] = ">=$fabric_api_version"
