f6c47bf3026a0b62a087e1e62bbbbfca91b1ee69
Treetrain1 <treetrain1@gmail.com>
2023-10-23 00:24:15 -0500

    a little more nutrition

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterFood.java
@@ -23,7 +23,7 @@ import net.minecraft.world.food.FoodProperties;
 public final class RegisterFood {
 	public static final FoodProperties BAOBAB_NUT = new FoodProperties.Builder().nutrition(1).saturationMod(0.2F).build();
 	public static final FoodProperties CRAB_CLAW = new FoodProperties.Builder().nutrition(1).saturationMod(0.1F).build();
-	public static final FoodProperties COOKED_CRAB_CLAW = new FoodProperties.Builder().nutrition(3).saturationMod(0.5F).build();
+	public static final FoodProperties COOKED_CRAB_CLAW = new FoodProperties.Builder().nutrition(4).saturationMod(0.5F).build();
 	public static final FoodProperties PRICKLY_PEAR = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build();
 
 	private RegisterFood() {
