a3105bc8afcca3b1239ca7d6cd6f28c29b6120c9
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-24 00:23:13 -0400

    why tf did it mixin itself

--- src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
@@ -3,12 +3,13 @@ package net.frozenblock.wilderwild.mixin.entity.crab;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.frozenblock.wilderwild.registry.RegisterMobEffects;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ServerGamePacketListenerImpl.class)
-public class ServerGamePacketListenerImpl {
+public class ServerGamePacketListenerImplMixin {
 
 	@Shadow
 	public ServerPlayer player;
