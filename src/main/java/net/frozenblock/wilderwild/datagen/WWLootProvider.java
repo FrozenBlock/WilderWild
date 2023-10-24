6217f35135d12d66f3732ffbd711661a4c37e13d
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-23 23:03:16 -0400

    add liscences

--- src/main/java/net/frozenblock/wilderwild/datagen/WWLootProvider.java
+++ src/main/java/net/frozenblock/wilderwild/datagen/WWLootProvider.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2023 FrozenBlock
+ * This file is part of Wilder Wild.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.frozenblock.wilderwild.datagen;
 
 import java.util.function.BiConsumer;
@@ -15,6 +33,7 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.LootItemKilledByPlayerCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import org.jetbrains.annotations.NotNull;
 
 public class WWLootProvider extends SimpleFabricLootTableProvider {
 
@@ -23,7 +42,7 @@ public class WWLootProvider extends SimpleFabricLootTableProvider {
 	}
 
 	@Override
-	public void generate(BiConsumer<ResourceLocation, LootTable.Builder> output) {
+	public void generate(@NotNull BiConsumer<ResourceLocation, LootTable.Builder> output) {
 		output.accept(
 			RegisterEntities.CRAB.getDefaultLootTable(),
 			LootTable.lootTable().withPool(
