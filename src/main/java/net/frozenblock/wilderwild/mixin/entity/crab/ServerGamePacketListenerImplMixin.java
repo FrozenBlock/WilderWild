96342acaecc3800db662630b86ce5f6bad3659ab
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-24 00:24:53 -0400

    bruh moment

--- src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
@@ -1,6 +1,6 @@
 package net.frozenblock.wilderwild.mixin.entity.crab;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.frozenblock.wilderwild.registry.RegisterMobEffects;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
@@ -14,7 +14,7 @@ public class ServerGamePacketListenerImplMixin {
 	@Shadow
 	public ServerPlayer player;
 
-	@ModifyReturnValue(method = "handleUseItemOn", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;MAX_INTERACTION_DISTANCE:D"))
+	@ModifyExpressionValue(method = "handleUseItemOn", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;MAX_INTERACTION_DISTANCE:D"))
 	public double wilderWild$handleUseItemOn(double original) {
 		if (this.player != null && this.player.hasEffect(RegisterMobEffects.REACH)) {
 			int amplifier = this.player.getEffect(RegisterMobEffects.REACH).getAmplifier() + 1;
@@ -23,7 +23,7 @@ public class ServerGamePacketListenerImplMixin {
 		return original;
 	}
 
-	@ModifyReturnValue(method = "handleInteract", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;MAX_INTERACTION_DISTANCE:D"))
+	@ModifyExpressionValue(method = "handleInteract", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;MAX_INTERACTION_DISTANCE:D"))
 	public double wilderWild$handleInteract(double original) {
 		if (this.player != null && this.player.hasEffect(RegisterMobEffects.REACH)) {
 			int amplifier = this.player.getEffect(RegisterMobEffects.REACH).getAmplifier() + 1;
