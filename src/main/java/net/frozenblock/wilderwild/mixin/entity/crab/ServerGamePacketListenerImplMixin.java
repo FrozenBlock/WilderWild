c4286c367c9b10af1c5108f0ee56302640c7e28e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-24 00:34:00 -0400

    what about now

--- src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
@@ -23,6 +23,15 @@ public class ServerGamePacketListenerImplMixin {
 		return original;
 	}
 
+	@ModifyExpressionValue(method = "handleUseItemOn", at = @At(value = "CONSTANT", args = "doubleValue=64", ordinal = 0))
+	public double wilderWild$handleUseItemOn2(double original) {
+		if (this.player != null && this.player.hasEffect(RegisterMobEffects.REACH)) {
+			int amplifier = this.player.getEffect(RegisterMobEffects.REACH).getAmplifier() + 1;
+			return original + (amplifier * 0.75D);
+		}
+		return original;
+	}
+
 	@ModifyExpressionValue(method = "handleInteract", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;MAX_INTERACTION_DISTANCE:D"))
 	public double wilderWild$handleInteract(double original) {
 		if (this.player != null && this.player.hasEffect(RegisterMobEffects.REACH)) {
