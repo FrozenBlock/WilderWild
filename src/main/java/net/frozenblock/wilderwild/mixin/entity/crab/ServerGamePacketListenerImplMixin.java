1e88ea7abf10a8d18c0fad3f21a22693ad0597ea
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-10-24 00:37:18 -0400

    what about now

--- src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/ServerGamePacketListenerImplMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.frozenblock.wilderwild.registry.RegisterMobEffects;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,7 +28,7 @@ public class ServerGamePacketListenerImplMixin {
 	public double wilderWild$handleUseItemOn2(double original) {
 		if (this.player != null && this.player.hasEffect(RegisterMobEffects.REACH)) {
 			int amplifier = this.player.getEffect(RegisterMobEffects.REACH).getAmplifier() + 1;
-			return original + (amplifier * 0.75D);
+			return Mth.square(Math.sqrt(original) + amplifier * 0.75D);
 		}
 		return original;
 	}
