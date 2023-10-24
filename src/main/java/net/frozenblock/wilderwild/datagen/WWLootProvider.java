ccd49713e9fe0d67791140aa4da8300000996c8c
Treetrain1 <treetrain1@gmail.com>
2023-10-24 04:27:17 -0500

    possible fix to #315

--- src/main/java/net/frozenblock/wilderwild/datagen/WWLootProvider.java
+++ src/main/java/net/frozenblock/wilderwild/datagen/WWLootProvider.java
@@ -21,16 +21,23 @@ package net.frozenblock.wilderwild.datagen;
 import java.util.function.BiConsumer;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.frozenblock.wilderwild.block.DisplayLanternBlock;
+import net.frozenblock.wilderwild.misc.WilderSharedConstants;
+import net.frozenblock.wilderwild.registry.RegisterBlocks;
 import net.frozenblock.wilderwild.registry.RegisterEntities;
 import net.frozenblock.wilderwild.registry.RegisterItems;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.AlternativesEntry;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.CopyBlockState;
+import net.minecraft.world.level.storage.loot.functions.CopyNbtFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.LootItemKilledByPlayerCondition;
+import net.minecraft.world.level.storage.loot.providers.nbt.ContextNbtProvider;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import org.jetbrains.annotations.NotNull;
@@ -52,6 +59,18 @@ public class WWLootProvider extends SimpleFabricLootTableProvider {
 						.apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 1.0F)))
 					)
 					.when(LootItemKilledByPlayerCondition.killedByPlayer())
+			)
+		);
+
+		output.accept(
+			WilderSharedConstants.id("blocks/display_lantern_fireflies"),
+			LootTable.lootTable().withPool(
+				LootPool.lootPool()
+					.setRolls(ConstantValue.exactly(1.0F))
+					.add(
+						LootItem.lootTableItem(RegisterBlocks.DISPLAY_LANTERN)
+							.apply(CopyNbtFunction.copyData(ContextNbtProvider.BLOCK_ENTITY).copy("Fireflies", "BlockEntityTag.Fireflies"))
+					)
 			)
 		);
 	}
