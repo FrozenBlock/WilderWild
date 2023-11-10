1c28890a24b478d131c8577e86d7736870d9fc34
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-11-10 04:20:16 -0500

    milkweed shearing with dispensers

--- src/main/java/net/frozenblock/wilderwild/block/MilkweedBlock.java
+++ src/main/java/net/frozenblock/wilderwild/block/MilkweedBlock.java
@@ -25,6 +25,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -42,6 +43,7 @@ import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class MilkweedBlock extends TallFlowerBlock {
 	private static final int MAX_AGE = 3;
@@ -67,7 +69,7 @@ public class MilkweedBlock extends TallFlowerBlock {
 	@Override
 	public void randomTick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
 		if (random.nextFloat() > 0.83F && isLower(state) && !isFullyGrown(state)) {
-			this.setAgeOnBothHalves(state, level, pos, state.getValue(BlockStateProperties.AGE_3) + 1);
+			setAgeOnBothHalves(this, state, level, pos, state.getValue(BlockStateProperties.AGE_3) + 1);
 		}
 	}
 
@@ -78,40 +80,47 @@ public class MilkweedBlock extends TallFlowerBlock {
 			ItemStack itemStack = player.getItemInHand(hand);
 			if (!level.isClientSide) {
 				if (itemStack.is(Items.SHEARS)) {
-					ItemStack stack = new ItemStack(RegisterItems.MILKWEED_POD);
-					stack.setCount(level.random.nextIntBetweenInclusive(2, 5));
-					popResource(level, pos, stack);
-					level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.GROWING_PLANT_CROP, SoundSource.BLOCKS, 1.0F, 1.0F);
 					itemStack.hurtAndBreak(1, player, (playerx) -> playerx.broadcastBreakEvent(hand));
-					level.gameEvent(player, GameEvent.SHEAR, pos);
+					player.awardStat(Stats.ITEM_USED.get(Items.SHEARS));
+					shear(level, pos, player);
 				} else {
 					level.playSound(null, player.getX(), player.getY(), player.getZ(), RegisterSounds.BLOCK_MILKWEED_RUSTLE, SoundSource.BLOCKS, 0.8F, 0.9F + (level.random.nextFloat() * 0.15F));
 					EasyPacket.EasySeedPacket.createParticle(level, Vec3.atCenterOf(pos).add(0, 0.3, 0), level.random.nextIntBetweenInclusive(14, 28), true);
+					setAgeOnBothHalves(this, state, level, pos, 0);
 				}
-				this.setAgeOnBothHalves(state, level, pos, 0);
 			}
 			return InteractionResult.SUCCESS;
 		}
 		return super.use(state, level, pos, player, hand, hit);
 	}
 
+	public static void shear(@NotNull Level level, BlockPos pos, @Nullable Player player) {
+		ItemStack stack = new ItemStack(RegisterItems.MILKWEED_POD);
+		stack.setCount(level.random.nextIntBetweenInclusive(2, 5));
+		popResource(level, pos, stack);
+		level.playSound(null, pos, SoundEvents.GROWING_PLANT_CROP, SoundSource.BLOCKS, 1.0F, 1.0F);
+		level.gameEvent(player, GameEvent.SHEAR, pos);
+		BlockState state = level.getBlockState(pos);
+		setAgeOnBothHalves(state.getBlock(), state, level, pos, 0);
+	}
+
 	@Override
 	public void performBonemeal(@NotNull ServerLevel level, @NotNull RandomSource random, @NotNull BlockPos pos, @NotNull BlockState state) {
 		if (isLower(state) && !isFullyGrown(state)) {
-			this.setAgeOnBothHalves(state, level, pos, state.getValue(BlockStateProperties.AGE_3) + 1);
+			setAgeOnBothHalves(this, state, level, pos, state.getValue(BlockStateProperties.AGE_3) + 1);
 			return;
 		}
 		Block.popResource(level, pos, new ItemStack(this));
 	}
 
-	public void setAgeOnBothHalves(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, int age) {
+	public static void setAgeOnBothHalves(Block thisBlock, @NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, int age) {
 		if (age > MAX_AGE) {
 			return;
 		}
 		level.setBlockAndUpdate(pos, state.setValue(BlockStateProperties.AGE_3, age));
 		BlockPos movedPos = state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.UPPER ? pos.below() : pos.above();
 		BlockState secondState = level.getBlockState(movedPos);
-		if (secondState.is(this)) {
+		if (secondState.is(thisBlock)) {
 			level.setBlockAndUpdate(movedPos, secondState.setValue(BlockStateProperties.AGE_3, age));
 		}
 	}
---
+++ src/main/java/net/frozenblock/wilderwild/mixin/block/milkweed/ShearsDispenseItemBehaviorMixin.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 FrozenBlock
+ * This file is part of Wilder Wild.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.frozenblock.wilderwild.mixin.block.milkweed;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.frozenblock.wilderwild.block.MilkweedBlock;
+import net.frozenblock.wilderwild.registry.RegisterBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.dispenser.ShearsDispenseItemBehavior;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ShearsDispenseItemBehavior.class)
+public class ShearsDispenseItemBehaviorMixin {
+
+	@WrapOperation(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/dispenser/ShearsDispenseItemBehavior;tryShearBeehive(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)Z"))
+	private static boolean wilderWild$execute(ServerLevel usedLevel, BlockPos pos, @NotNull Operation<Boolean> operation) {
+		return operation.call(usedLevel, pos) || wilderWild$tryShearMilkweed(usedLevel, pos);
+	}
+
+	@Unique
+	private static boolean wilderWild$tryShearMilkweed(@NotNull ServerLevel level, BlockPos pos) {
+		BlockState blockState = level.getBlockState(pos);
+		if (blockState.getBlock() == RegisterBlocks.MILKWEED && MilkweedBlock.isFullyGrown(blockState)) {
+			MilkweedBlock.shear(level, pos, null);
+			return true;
+		}
+		return false;
+	}
+
+}
