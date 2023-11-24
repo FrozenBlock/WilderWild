f864f8437377727b6017212d8716b3ec7fc765c4
Treetrain1 <39657565+Treetrain1@users.noreply.github.com>
2023-11-23 23:14:05 -0600

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -313,7 +313,7 @@ tasks {
 
         properties["fabric_loader_version"] = ">=0.14.22"
         properties["fabric_api_version"] = ">=$fabric_api_version"
-        properties["frozenlib_version"] = ">=${frozenlib_version.split('-').getFirst()}-"
+        properties["frozenlib_version"] = ">=${frozenlib_version.split('-')[0]}-"
 
         properties.forEach { (a, b) -> inputs.property(a, b) }
 
