866dec7b85fd5ca3a21eb5ac66f387da5c3b5a17
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-02-05 02:09:02 -0500

    Prevent snowlogging waterlogged blocks

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/snowlogging/SimpleWaterloggedBlockMixin.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 FrozenBlock
+ * This file is part of Wilder Wild.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.frozenblock.wilderwild.mixin.snowlogging;
+
+import net.frozenblock.wilderwild.block.impl.SnowloggingUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SimpleWaterloggedBlock.class)
+public interface SimpleWaterloggedBlockMixin {
+
+	@Inject(method = "canPlaceLiquid", at = @At("HEAD"), cancellable = true)
+	default void wilderWild$canPlaceLiquid(Player player, BlockGetter level, BlockPos pos, BlockState state, Fluid fluid, CallbackInfoReturnable<Boolean> info) {
+		if (SnowloggingUtils.isSnowlogged(state)) {
+			info.setReturnValue(false);
+		}
+	}
+
+}
