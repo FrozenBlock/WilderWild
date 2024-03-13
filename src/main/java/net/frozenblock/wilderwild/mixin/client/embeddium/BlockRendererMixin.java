85bc505df7ec2e4a92609edbc08df733fe38398c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-03-13 01:20:37 -0400

    fix snowlogging with embeddium/sodium

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/embeddium/BlockRendererMixin.java
@@ -0,0 +1,41 @@
+package net.frozenblock.wilderwild.mixin.client.embeddium;
+
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import net.frozenblock.wilderwild.block.impl.SnowloggingUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.joml.Vector3fc;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockRenderer.class)
+public abstract class BlockRendererMixin {
+
+	@Shadow
+	public abstract void renderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers);
+
+	@Inject(method = "renderModel", at = @At("HEAD"), remap = false)
+	public void wilderWild$renderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers, CallbackInfo info) {
+		if (SnowloggingUtils.isSnowlogged(ctx.state())) {
+			BlockState snowState = SnowloggingUtils.getSnowEquivalent(ctx.state());
+			BlockRenderContext snowRenderContext = new BlockRenderContext(ctx.world());
+			Vector3fc origin = ctx.origin();
+			snowRenderContext.update(
+				ctx.pos(),
+				new BlockPos((int) origin.x(), (int) origin.y(), (int) origin.z()),
+				snowState,
+				Minecraft.getInstance().getBlockRenderer().getBlockModel(snowState),
+				ctx.seed(),
+				ctx.renderLayer()
+			);
+			this.renderModel(snowRenderContext, buffers);
+		}
+	}
+
+}
