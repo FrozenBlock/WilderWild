d5d81a212bba39b45346d750024c34e240c07f02
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-04-08 18:22:48 -0400

    bruh

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,9 @@ buildscript {
     }
     dependencies {
         classpath("org.kohsuke:github-api:+")
-
         // remove these 2 to get normal fabric loom versions
-        classpath(files("libs/fabric-loom-1.5.local.jar"))
-        classpath("net.fabricmc:mapping-io:+")
+        //classpath(files("libs/fabric-loom-1.5.local.jar"))
+        //classpath("net.fabricmc:mapping-io:+")
     }
 }
 
