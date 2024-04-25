a8fcb68be4d3c8969d468f4b0406573baf64e266
Treetrain1 <treetrain1@gmail.com>
2024-04-25 17:38:42 -0500

    1.20.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -243,7 +243,7 @@ tasks {
             "mod_id" to mod_id,
             "version" to version,
             "protocol_version" to protocol_version,
-            "minecraft_version" to "~1.20.5-",
+            "minecraft_version" to minecraft_version,
 
             "fabric_api_version" to ">=$fabric_api_version",
             "frozenlib_version" to ">=${frozenlib_version.split('-').firstOrNull()}-"
