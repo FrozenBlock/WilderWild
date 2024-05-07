f9168fbdd7a4c740140c0047eaffa112ec094cc9
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-05-07 13:18:48 -0400

    bruh

--- src/main/java/net/frozenblock/wilderwild/config/gui/BlockConfigGui.java
+++ src/main/java/net/frozenblock/wilderwild/config/gui/BlockConfigGui.java
@@ -54,20 +54,6 @@ public final class BlockConfigGui {
 		var modifiedFire = modifiedConfig.fire;
 		category.setBackground(WilderSharedConstants.id("textures/config/block.png"));
 
-		var blockStateCompat = category.addEntry(
-			FrozenClothConfig.syncedEntry(
-				entryBuilder.startBooleanToggle(text("blockstate_compat"), modifiedConfig.blockStateCompat)
-					.setDefaultValue(defaultConfig.blockStateCompat)
-					.setSaveConsumer(newValue -> config.blockStateCompat = newValue)
-					.requireRestart()
-					.setTooltip(tooltip("blockstate_compat"))
-					.build(),
-				clazz,
-				"blockStateCompat",
-				configInstance
-			)
-		);
-
 		var shriekerGargling = category.addEntry(
 			FrozenClothConfig.syncedEntry(
 				entryBuilder.startBooleanToggle(text("shrieker_gargling"), modifiedConfig.shriekerGargling)
@@ -444,6 +430,20 @@ public final class BlockConfigGui {
 			flowerSounds, frostedIceSounds, gravelSounds, iceSounds, leafSounds, lilyPadSounds,
 			magmaSounds, mushroomBlockSounds, podzolSounds, reinforcedDeepslateSounds,
 			sandstoneSounds, saplingSounds, sugarCaneSounds, witherRoseSounds
+		);
+
+		var blockStateCompat = category.addEntry(
+			FrozenClothConfig.syncedEntry(
+				entryBuilder.startBooleanToggle(text("blockstate_compat"), modifiedConfig.blockStateCompat)
+					.setDefaultValue(defaultConfig.blockStateCompat)
+					.setSaveConsumer(newValue -> config.blockStateCompat = newValue)
+					.requireRestart()
+					.setTooltip(tooltip("blockstate_compat"))
+					.build(),
+				clazz,
+				"blockStateCompat",
+				configInstance
+			)
 		);
 	}
 
