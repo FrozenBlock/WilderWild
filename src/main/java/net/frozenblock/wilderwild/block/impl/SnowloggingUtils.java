edec1a1a07f62a9637d7aaab0f14b6a611943891
Treetrain1 <treetrain1@gmail.com>
2024-06-10 20:25:23 -0500

    fix datagen crash

--- src/main/java/net/frozenblock/wilderwild/block/impl/SnowloggingUtils.java
+++ src/main/java/net/frozenblock/wilderwild/block/impl/SnowloggingUtils.java
@@ -18,6 +18,7 @@
 
 package net.frozenblock.wilderwild.block.impl;
 
+import net.frozenblock.wilderwild.config.BlockConfig;
 import net.frozenblock.wilderwild.registry.RegisterProperties;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -41,10 +42,8 @@ public class SnowloggingUtils {
 	public static final IntegerProperty SNOW_LAYERS = RegisterProperties.SNOW_LAYERS;
 	public static final int MAX_LAYERS = 8;
 
-	public static volatile boolean SNOWLOGGING = false;
-
 	public static boolean supportsSnowlogging(@Nullable BlockState state) {
-		if (!SNOWLOGGING) return false;
+		if (!BlockConfig.SNOWLOGGING) return false;
 		//noinspection ConstantValue
 		return state != null && state.getValues() != null && state.hasProperty(SNOW_LAYERS);
 	}
