890e39c3cf01a1910795d5a110790548476d9bf8
Wet Noodle <120989937+Kluski42@users.noreply.github.com>
2024-07-05 00:06:41 -0400

    Amethyst update

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/snowlogging/worldgen/BuddingAmethystBlockMixin.java
@@ -0,0 +1,37 @@
+package net.frozenblock.wilderwild.mixin.snowlogging.worldgen;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.frozenblock.wilderwild.block.impl.SnowloggingUtils;
+import net.frozenblock.wilderwild.config.BlockConfig;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BuddingAmethystBlock;
+import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BuddingAmethystBlock.class)
+public abstract class BuddingAmethystBlockMixin {
+	@ModifyReturnValue(method = "canClusterGrowAtState", at = @At("RETURN"))
+	private static boolean wilderWild$canClusterGrowAtState(boolean original, BlockState state) {
+		return original || state.is(Blocks.SNOW);
+	}
+
+	@ModifyExpressionValue(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;defaultBlockState()Lnet/minecraft/world/level/block/state/BlockState;"))
+	public BlockState wilderWild$randomTick(BlockState newBlockState, @Local(ordinal = 1) BlockState oldBlockState) {
+		if (!BlockConfig.SNOWLOGGING) return newBlockState;
+		int layers = getSnowLayers(oldBlockState);
+		return newBlockState.setValue(SnowloggingUtils.SNOW_LAYERS, layers);
+	}
+
+	@Unique
+	private static int getSnowLayers(@NotNull BlockState state) {
+		if (SnowloggingUtils.supportsSnowlogging(state)) return SnowloggingUtils.getSnowLayers(state);
+		if (state.is(Blocks.SNOW)) return state.getValue(SnowLayerBlock.LAYERS);
+		return 0;
+	}
+}
