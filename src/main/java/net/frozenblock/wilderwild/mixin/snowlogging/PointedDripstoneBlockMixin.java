221c0e5e08550c3e022593dae9131c4999a0d4d0
Wet Noodle <120989937+Kluski42@users.noreply.github.com>
2024-07-30 20:37:34 +0200

    Landing on pointed dripstone no longer damages entities that land on the snow.

--- src/main/java/net/frozenblock/wilderwild/mixin/snowlogging/PointedDripstoneBlockMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/snowlogging/PointedDripstoneBlockMixin.java
@@ -1,13 +1,21 @@
 package net.frozenblock.wilderwild.mixin.snowlogging;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.frozenblock.wilderwild.block.impl.SnowloggingUtils;
 import net.frozenblock.wilderwild.config.BlockConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.PointedDripstoneBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +27,25 @@ public abstract class PointedDripstoneBlockMixin extends Block {
 
 	public PointedDripstoneBlockMixin(Properties properties) {
 		super(properties);
+	}
+
+	/**
+	 * Checks if the player landed on a snow layer, and cancels the dripstone damage if so.
+	 */
+	@WrapOperation(method = "fallOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;causeFallDamage(FFLnet/minecraft/world/damagesource/DamageSource;)Z"))
+	public boolean wilderWild$fallOn(
+		Entity entity, float modifiedFallDistance, float damageMultiplier, DamageSource damageSource, Operation<Boolean> original,
+		Level world, BlockState state, BlockPos pos, Entity entityOriginal, float fallDistanceOriginal
+	) {
+		if (SnowloggingUtils.isSnowlogged(state)) {
+			double entityY = entityOriginal.position().y();
+			VoxelShape snowLayerShape = SnowloggingUtils.getSnowEquivalent(state).getShape(world, pos);
+			if (entityY <= (snowLayerShape.max(Direction.Axis.Y)) + pos.getY()) {
+				super.fallOn(world, state, pos, entity, fallDistanceOriginal);
+				return false;
+			}
+		}
+		return original.call(entity, modifiedFallDistance, damageMultiplier, damageSource);
 	}
 
 	@Unique
