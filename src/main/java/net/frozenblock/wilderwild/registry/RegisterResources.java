08cd3fad4c9244d3949ab8f27842975b8031e15e
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-08-20 20:50:53 -0400

    Revert "resource pack lol"
    This reverts commit 547f0281b490d54823f0b4985894550ee4b796fa.

--- src/main/java/net/frozenblock/wilderwild/registry/RegisterResources.java
+++ src/main/java/net/frozenblock/wilderwild/registry/RegisterResources.java
@@ -39,8 +39,8 @@ public final class RegisterResources {
 		);
 
 		ResourceManagerHelper.registerBuiltinResourcePack(
-			WilderConstants.id("foliage2"),
-			container, Component.literal("New Vegetation Colors"),
+			WilderConstants.id("old_wilder_wild_panoramas"),
+			container, Component.literal("Old Wilder Wild Panoramas"),
 			ResourcePackActivationType.NORMAL
 		);
 
