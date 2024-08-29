e56b51b24cf776b6581496c3d85db9726ac0364c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-08-29 00:56:59 -0400

    Update MixinsConfig.java

--- src/main/java/net/frozenblock/wilderwild/config/MixinsConfig.java
+++ src/main/java/net/frozenblock/wilderwild/config/MixinsConfig.java
@@ -18,12 +18,12 @@
 
 package net.frozenblock.wilderwild.config;
 
-import blue.endless.jankson.Comment;
 import net.frozenblock.lib.config.api.instance.Config;
 import net.frozenblock.lib.config.api.instance.json.JsonConfig;
 import net.frozenblock.lib.config.api.instance.json.JsonType;
 import net.frozenblock.lib.config.api.registry.ConfigRegistry;
 import static net.frozenblock.wilderwild.WilderConstants.MOD_ID;
+import net.frozenblock.lib.shadow.blue.endless.jankson.Comment;
 import net.frozenblock.wilderwild.WilderPreLoadConstants;
 
 public final class MixinsConfig {
