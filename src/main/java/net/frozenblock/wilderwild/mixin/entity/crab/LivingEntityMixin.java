e1ae370846d1babad3d26f3974234f6e1a922871
Treetrain1 <treetrain1@gmail.com>
2024-09-07 13:51:27 -0500

    Fix `LivingEntityMixin`

--- src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/LivingEntityMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/entity/crab/LivingEntityMixin.java
@@ -28,7 +28,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class LivingEntityMixin {
 
 	@ModifyExpressionValue(
-		method = "travel",
+		method = "travelInFluid",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/world/entity/LivingEntity;onClimbable()Z"
