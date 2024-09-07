f84b6dba5a39d7d85b800354234e0a838eb6ee07
Treetrain1 <treetrain1@gmail.com>
2024-09-07 14:11:25 -0500

    Fix brush mixin

--- src/main/java/net/frozenblock/wilderwild/mixin/client/brush/LevelEventHandlerMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/client/brush/LevelEventHandlerMixin.java
@@ -23,20 +23,22 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.frozenblock.wilderwild.block.ScorchedBlock;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LevelEventHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Environment(EnvType.CLIENT)
-@Mixin(LevelRenderer.class)
-public class LevelRendererMixin {
+@Mixin(LevelEventHandler.class)
+public class LevelEventHandlerMixin {
 
 	@Shadow
+	@Final
 	@Nullable
 	private ClientLevel level;
 
