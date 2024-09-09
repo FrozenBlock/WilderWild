984d7795963ace932bf66675c3af39e984f838d2
Wet Noodle <120989937+Kluski42@users.noreply.github.com>
2024-09-09 12:19:50 -0400

    Snowloggable ladders

---
+++ src/main/java/net/frozenblock/wilderwild/mixin/snowlogging/blocks/LadderBlockMixin.java
@@ -0,0 +1,38 @@
+package net.frozenblock.wilderwild.mixin.snowlogging.blocks;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.frozenblock.wilderwild.block.impl.SnowloggingUtils;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LadderBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LadderBlock.class)
+public abstract class LadderBlockMixin extends Block {
+
+	public LadderBlockMixin(Properties settings) {
+		super(settings);
+	}
+
+	@Unique
+	@Override
+	protected boolean isRandomlyTicking(BlockState state) {
+		return super.isRandomlyTicking(state) || SnowloggingUtils.isSnowlogged(state);
+	}
+
+	@Inject(method = "createBlockStateDefinition", at = @At(value = "TAIL"))
+	public void wilderWild$createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder, CallbackInfo info) {
+		SnowloggingUtils.addSnowLayersToDefinition(builder);
+	}
+
+	@ModifyReturnValue(method = "getStateForPlacement", at = @At("RETURN"))
+	public BlockState wilderWild$getStateForPlacement(BlockState original, BlockPlaceContext context) {
+		return SnowloggingUtils.getSnowPlacementState(original, context);
+	}
+}
