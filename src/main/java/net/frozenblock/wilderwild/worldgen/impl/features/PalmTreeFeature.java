410c1167b80dfa6fc8c684903c4525c9f053a81a
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-10 18:21:43 -0400

    much better palm trees

--- src/main/java/net/frozenblock/wilderwild/worldgen/impl/features/PalmTreeFeature.java
+++ src/main/java/net/frozenblock/wilderwild/worldgen/impl/features/PalmTreeFeature.java
@@ -23,7 +23,7 @@ import net.minecraft.world.level.levelgen.feature.TreeFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 
 public class PalmTreeFeature extends TreeFeature {
-	public static final float COCONUT_CHANCE = 0.4F;
+	public static final float COCONUT_CHANCE = 0.25F;
 
 	public PalmTreeFeature(Codec<TreeConfiguration> codec) {
 		super(codec);
