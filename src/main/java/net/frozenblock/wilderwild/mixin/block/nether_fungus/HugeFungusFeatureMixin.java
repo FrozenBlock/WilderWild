c9b8570aa5b3cb0e30c15cb3910d38c4d7ac7fda
Wet Noodle <120989937+Kluski42@users.noreply.github.com>
2024-09-11 17:49:39 -0400

    Requires plus shape to grow a thick big fungus.

--- src/main/java/net/frozenblock/wilderwild/mixin/block/nether_fungus/HugeFungusFeatureMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/block/nether_fungus/HugeFungusFeatureMixin.java
@@ -7,22 +7,72 @@ import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.frozenblock.wilderwild.config.WWBlockConfig;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.HugeFungusConfiguration;
 import net.minecraft.world.level.levelgen.feature.HugeFungusFeature;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(HugeFungusFeature.class)
 public class HugeFungusFeatureMixin {
 	@ModifyVariable(method = "place", at = @At(value = "STORE"))
-	public boolean wilderWild$placeThickener(boolean original, FeaturePlaceContext<HugeFungusConfiguration> context) {
-		return original || (context.config().planted && WWBlockConfig.get().bigFungusGrowth);
+	public boolean wilderWild$placeThickener(boolean original, FeaturePlaceContext<HugeFungusConfiguration> context,
+											 @Share("wilderWild$newPos") LocalRef<BlockPos> newPos) {
+		newPos.set(context.origin());
+		if (original) return true;
+		if (context.config().planted && WWBlockConfig.get().bigFungusGrowth) {
+			Level level = (Level) context.level();
+			BlockPos pos = context.origin();
+			Block fungus = level.getBlockState(pos).getBlock();
+			if (canGrowThickFungus(level, pos, fungus)) {
+				newPos.set(pos);
+				clearFungi(level, pos);
+				return true;
+			}
+			for (Direction direction : Direction.Plane.HORIZONTAL) {
+				BlockPos adjacentPos = pos.relative(direction);
+				if (level.getBlockState(adjacentPos).is(fungus)) {
+					if (canGrowThickFungus(level, adjacentPos, fungus)) {
+						newPos.set(adjacentPos);
+						clearFungi(level, adjacentPos);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
+
+	@ModifyVariable(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/WorldGenLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"), ordinal = 1)
+	public BlockPos wilderWild$placeUpdateOrigin(BlockPos value, @Share("wilderWild$newPos") LocalRef<BlockPos> newPos) {
+		return newPos.get();
+	}
+
+	@Unique
+	private boolean canGrowThickFungus(Level level, BlockPos pos, Block fungus) {
+		for (Direction direction : Direction.Plane.HORIZONTAL) {
+			if (!level.getBlockState(pos.relative(direction)).is(fungus)) return false;
+		}
+		return true;
+	}
+
+	@Unique
+	private void clearFungi(Level level, BlockPos pos) {
+		level.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_INVISIBLE);
+		for (Direction direction : Direction.Plane.HORIZONTAL) {
+			level.setBlock(pos.relative(direction), Blocks.AIR.defaultBlockState(), Block.UPDATE_INVISIBLE);
+		}
+	}
+
 
 	@ModifyVariable(method = "placeStem", at = @At(value = "STORE"), ordinal = 1)
 	public boolean wilderWild$placeStemShouldPlace(
