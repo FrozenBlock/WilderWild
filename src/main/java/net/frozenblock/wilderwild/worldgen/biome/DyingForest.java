a1e997209899aa7357f678185a16eec07db5d95d
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-09-12 14:14:54 -0400

    Update DyingForest.java

--- src/main/java/net/frozenblock/wilderwild/worldgen/biome/DyingForest.java
+++ src/main/java/net/frozenblock/wilderwild/worldgen/biome/DyingForest.java
@@ -21,7 +21,10 @@ package net.frozenblock.wilderwild.worldgen.biome;
 import com.mojang.datafixers.util.Pair;
 import java.util.function.Consumer;
 import net.frozenblock.lib.worldgen.biome.api.FrozenBiome;
+import net.frozenblock.lib.worldgen.biome.api.parameters.Continentalness;
+import net.frozenblock.lib.worldgen.biome.api.parameters.Erosion;
 import net.frozenblock.lib.worldgen.biome.api.parameters.OverworldBiomeBuilderParameters;
+import net.frozenblock.lib.worldgen.biome.api.parameters.Temperature;
 import net.frozenblock.wilderwild.WWConstants;
 import net.frozenblock.wilderwild.config.WWWorldgenConfig;
 import net.frozenblock.wilderwild.worldgen.WWSharedWorldgen;
@@ -50,8 +53,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class DyingForest extends FrozenBiome {
-	public static final Climate.Parameter TEMPERATURE = Climate.Parameter.span(-0.465F, -0.300F);
-	public static final Climate.Parameter HUMIDITY = Climate.Parameter.span(-0.105F, 0.050F);
+	public static final Climate.Parameter TEMPERATURE_A = Climate.Parameter.span(-0.465F, -0.300F);
+	public static final Climate.Parameter HUMIDITY_A = Climate.Parameter.span(-0.105F, 0.050F);
+	public static final Climate.Parameter TEMPERATURE_MAPLE = Temperature.COOL;
+	public static final Climate.Parameter HUMIDITY_MAPLE = Climate.Parameter.span(-1F, 0.1F);
+	public static final Climate.Parameter WEIRDNESS_MAPLE = Climate.Parameter.span(0.5F, 0.8F);
+	public static final Climate.Parameter EROSION_MAPLE = Climate.Parameter.span(Erosion.EROSION_3, Erosion.EROSION_6);
+	public static final Climate.Parameter CONTINENTALNESS_MAPLE = Climate.Parameter.span(Continentalness.COAST, Continentalness.FAR_INLAND);
 	public static final float TEMP = 0.35F;
 	public static final float DOWNFALL = 0.65F;
 	public static final int WATER_COLOR = WWSharedWorldgen.STOCK_WATER_COLOR;
@@ -167,12 +175,23 @@ public class DyingForest extends FrozenBiome {
 			for (Climate.ParameterPoint point : OverworldBiomeBuilderParameters.points(Biomes.FOREST)) {
 				this.addSurfaceBiome(
 					parameters,
-					TEMPERATURE,
-					HUMIDITY,
+					TEMPERATURE_A,
+					HUMIDITY_A,
 					point.continentalness(),
 					point.erosion(),
 					point.weirdness(),
 					point.offset()
+				);
+			}
+			if (WWWorldgenConfig.get().biomeGeneration.generateMapleGrove) {
+				this.addSurfaceBiome(
+					parameters,
+					TEMPERATURE_MAPLE,
+					HUMIDITY_MAPLE,
+					CONTINENTALNESS_MAPLE,
+					EROSION_MAPLE,
+					WEIRDNESS_MAPLE,
+					0F
 				);
 			}
 		}
