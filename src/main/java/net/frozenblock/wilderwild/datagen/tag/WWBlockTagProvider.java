593e9732836235bc98bbd7e69ca4b2e51a62fe26
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-10-10 21:21:20 -0400

    fix missing cherry in tag

--- src/main/java/net/frozenblock/wilderwild/datagen/tag/WWBlockTagProvider.java
+++ src/main/java/net/frozenblock/wilderwild/datagen/tag/WWBlockTagProvider.java
@@ -817,7 +817,7 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 			.add(Blocks.STRIPPED_MANGROVE_WOOD)
 			.add(Blocks.STRIPPED_OAK_WOOD)
 			.add(Blocks.STRIPPED_SPRUCE_WOOD)
-			.add(Blocks.STRIPPED_ACACIA_WOOD)
+			.add(Blocks.STRIPPED_CHERRY_WOOD)
 			.add(WWBlocks.STRIPPED_BAOBAB_WOOD)
 			.add(WWBlocks.STRIPPED_CYPRESS_WOOD)
 			.add(WWBlocks.STRIPPED_PALM_WOOD)
