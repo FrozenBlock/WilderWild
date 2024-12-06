3ffbe9d248b49c9d0c48e6abd2db036475caa877
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-05 19:42:23 -0500

    Fix Ostriches

--- src/main/java/net/frozenblock/wilderwild/client/renderer/entity/OstrichRenderer.java
+++ src/main/java/net/frozenblock/wilderwild/client/renderer/entity/OstrichRenderer.java
@@ -27,33 +27,49 @@ import net.frozenblock.wilderwild.client.renderer.entity.state.OstrichRenderStat
 import net.frozenblock.wilderwild.entity.Ostrich;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.AgeableMobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.SaddleLayer;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Math;
 
-public class OstrichRenderer extends MobRenderer<Ostrich, OstrichRenderState, EntityModel<OstrichRenderState>> {
+public class OstrichRenderer extends AgeableMobRenderer<Ostrich, OstrichRenderState, EntityModel<OstrichRenderState>> {
 	private static final ResourceLocation OSTRICH_LOCATION = WWConstants.id("textures/entity/ostrich/ostrich.png");
 	private static final ResourceLocation OSTRICH_SADDLE_LOCATION = WWConstants.id("textures/entity/ostrich/ostrich_saddle.png");
 
-	private boolean isInbred = false;
-	private final EntityModel<OstrichRenderState> normalModel = this.getModel();
+	private final EntityModel<OstrichRenderState> normalModel;
+	private final EntityModel<OstrichRenderState> normalBabyModel;
 	private final EntityModel<OstrichRenderState> inbredModel;
+	private final EntityModel<OstrichRenderState> inbredBabyModel;
 
 	public OstrichRenderer(EntityRendererProvider.Context context) {
-		super(context, new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH)), 0.75F);
+		super(context, new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH)), new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH_BABY)), 0.75F);
+
+		this.normalModel = this.adultModel;
+		this.normalBabyModel = this.babyModel;
+
 		this.inbredModel = new OstrichInbredModel(context.bakeLayer(WWModelLayers.OSTRICH_INBRED));
-		this.addLayer(new SaddleLayer<>(this, new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH_SADDLE)), OSTRICH_SADDLE_LOCATION));
+		this.inbredBabyModel = new OstrichInbredModel(context.bakeLayer(WWModelLayers.OSTRICH_BABY_INBRED));
+
+		this.addLayer(
+			new SaddleLayer<>(
+				this,
+				new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH_SADDLE)),
+				new OstrichModel(context.bakeLayer(WWModelLayers.OSTRICH_BABY_SADDLE)),
+				OSTRICH_SADDLE_LOCATION
+			)
+		);
 	}
 
 	@Override
 	public void render(@NotNull OstrichRenderState renderState, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight) {
-		boolean isOstrichInbred = renderState.isInbred;
-		if (this.isInbred != isOstrichInbred) {
-			this.model = !isOstrichInbred ? this.normalModel : this.inbredModel;
-			this.isInbred = isOstrichInbred;
+		if (renderState.isInbred) {
+			this.adultModel = this.inbredModel;
+			this.babyModel = this.inbredBabyModel;
+		} else {
+			this.adultModel = this.normalModel;
+			this.babyModel = this.normalBabyModel;
 		}
 		super.render(renderState, poseStack, multiBufferSource, packedLight);
 	}
