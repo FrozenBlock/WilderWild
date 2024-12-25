151e2574d803a42d5ba09450eccef62b55336aec
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-24 22:57:02 -0500

    bottle colors

--- src/main/java/net/frozenblock/wilderwild/entity/variant/FireflyColor.java
+++ src/main/java/net/frozenblock/wilderwild/entity/variant/FireflyColor.java
@@ -29,36 +29,37 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
-public record FireflyColor(ResourceLocation key, ResourceLocation texture) implements StringRepresentable {
+public record FireflyColor(ResourceLocation key, ResourceLocation texture, float modelPredicate) implements StringRepresentable {
 	public static final Codec<FireflyColor> CODEC = WilderWildRegistries.FIREFLY_COLOR.byNameCodec();
 	public static final StreamCodec<RegistryFriendlyByteBuf, FireflyColor> STREAM_CODEC = ByteBufCodecs.fromCodecWithRegistries(CODEC);
 
-	public static final FireflyColor BLACK = register(WWConstants.id("black"), WWConstants.id("textures/entity/firefly/firefly_black.png"));
-	public static final FireflyColor BLUE = register(WWConstants.id("blue"), WWConstants.id("textures/entity/firefly/firefly_blue.png"));
-	public static final FireflyColor BROWN = register(WWConstants.id("brown"), WWConstants.id("textures/entity/firefly/firefly_brown.png"));
-	public static final FireflyColor CYAN = register(WWConstants.id("cyan"), WWConstants.id("textures/entity/firefly/firefly_cyan.png"));
-	public static final FireflyColor GRAY = register(WWConstants.id("gray"), WWConstants.id("textures/entity/firefly/firefly_gray.png"));
-	public static final FireflyColor GREEN = register(WWConstants.id("green"), WWConstants.id("textures/entity/firefly/firefly_green.png"));
-	public static final FireflyColor LIGHT_BLUE = register(WWConstants.id("light_blue"), WWConstants.id("textures/entity/firefly/firefly_light_blue.png"));
-	public static final FireflyColor LIGHT_GRAY = register(WWConstants.id("light_gray"), WWConstants.id("textures/entity/firefly/firefly_light_gray.png"));
-	public static final FireflyColor LIME = register(WWConstants.id("lime"), WWConstants.id("textures/entity/firefly/firefly_lime.png"));
-	public static final FireflyColor MAGENTA = register(WWConstants.id("magenta"), WWConstants.id("textures/entity/firefly/firefly_magenta.png"));
-	public static final FireflyColor ON = register(WWConstants.id("on"), WWConstants.id("textures/entity/firefly/firefly_on.png"));
-	public static final FireflyColor ORANGE = register(WWConstants.id("orange"), WWConstants.id("textures/entity/firefly/firefly_orange.png"));
-	public static final FireflyColor PINK = register(WWConstants.id("pink"), WWConstants.id("textures/entity/firefly/firefly_pink.png"));
-	public static final FireflyColor PURPLE = register(WWConstants.id("purple"), WWConstants.id("textures/entity/firefly/firefly_purple.png"));
-	public static final FireflyColor RED = register(WWConstants.id("red"), WWConstants.id("textures/entity/firefly/firefly_red.png"));
-	public static final FireflyColor WHITE = register(WWConstants.id("white"), WWConstants.id("textures/entity/firefly/firefly_white.png"));
-	public static final FireflyColor YELLOW = register(WWConstants.id("yellow"), WWConstants.id("textures/entity/firefly/firefly_yellow.png"));
+	public static final FireflyColor ON = register(WWConstants.id("on"), WWConstants.id("textures/entity/firefly/firefly_on.png"), 0F);
+	public static final FireflyColor BLACK = register(WWConstants.id("black"), WWConstants.id("textures/entity/firefly/firefly_black.png"), 0.01F);
+	public static final FireflyColor BLUE = register(WWConstants.id("blue"), WWConstants.id("textures/entity/firefly/firefly_blue.png"), 0.02F);
+	public static final FireflyColor BROWN = register(WWConstants.id("brown"), WWConstants.id("textures/entity/firefly/firefly_brown.png"), 0.03F);
+	public static final FireflyColor CYAN = register(WWConstants.id("cyan"), WWConstants.id("textures/entity/firefly/firefly_cyan.png"), 0.04F);
+	public static final FireflyColor GRAY = register(WWConstants.id("gray"), WWConstants.id("textures/entity/firefly/firefly_gray.png"), 0.05F);
+	public static final FireflyColor GREEN = register(WWConstants.id("green"), WWConstants.id("textures/entity/firefly/firefly_green.png"), 0.06F);
+	public static final FireflyColor LIGHT_BLUE = register(WWConstants.id("light_blue"), WWConstants.id("textures/entity/firefly/firefly_light_blue.png"), 0.07F);
+	public static final FireflyColor LIGHT_GRAY = register(WWConstants.id("light_gray"), WWConstants.id("textures/entity/firefly/firefly_light_gray.png"), 0.08F);
+	public static final FireflyColor LIME = register(WWConstants.id("lime"), WWConstants.id("textures/entity/firefly/firefly_lime.png"), 0.09F);
+	public static final FireflyColor MAGENTA = register(WWConstants.id("magenta"), WWConstants.id("textures/entity/firefly/firefly_magenta.png"), 0.10F);
+	public static final FireflyColor ORANGE = register(WWConstants.id("orange"), WWConstants.id("textures/entity/firefly/firefly_orange.png"), 0.11F);
+	public static final FireflyColor PINK = register(WWConstants.id("pink"), WWConstants.id("textures/entity/firefly/firefly_pink.png"), 0.12F);
+	public static final FireflyColor PURPLE = register(WWConstants.id("purple"), WWConstants.id("textures/entity/firefly/firefly_purple.png"), 0.13F);
+	public static final FireflyColor RED = register(WWConstants.id("red"), WWConstants.id("textures/entity/firefly/firefly_red.png"), 0.14F);
+	public static final FireflyColor WHITE = register(WWConstants.id("white"), WWConstants.id("textures/entity/firefly/firefly_white.png"), 0.15F);
+	public static final FireflyColor YELLOW = register(WWConstants.id("yellow"), WWConstants.id("textures/entity/firefly/firefly_yellow.png"), 0.16F);
 
-	public FireflyColor(@NotNull ResourceLocation key, @NotNull ResourceLocation texture) {
+	public FireflyColor(@NotNull ResourceLocation key, @NotNull ResourceLocation texture, float modelPredicate) {
 		this.key = key;
 		this.texture = texture;
+		this.modelPredicate = modelPredicate;
 	}
 
 	@NotNull
-	public static FireflyColor register(@NotNull ResourceLocation key, @NotNull ResourceLocation texture) {
-		return Registry.register(WilderWildRegistries.FIREFLY_COLOR, key, new FireflyColor(key, texture));
+	public static FireflyColor register(@NotNull ResourceLocation key, @NotNull ResourceLocation texture, float modelPredicate) {
+		return Registry.register(WilderWildRegistries.FIREFLY_COLOR, key, new FireflyColor(key, texture, modelPredicate));
 	}
 
 	public static void init() {
