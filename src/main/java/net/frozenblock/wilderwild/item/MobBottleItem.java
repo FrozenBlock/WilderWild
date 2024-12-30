2d6061bdc023f44bd20810a843fad850fcbc9425
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-12-29 21:19:11 -0500

    Update MobBottleItem.java

--- src/main/java/net/frozenblock/wilderwild/item/MobBottleItem.java
+++ src/main/java/net/frozenblock/wilderwild/item/MobBottleItem.java
@@ -85,7 +85,7 @@ public class MobBottleItem extends Item {
 
 					if (!player.getAbilities().instabuild) player.setItemInHand(interactionHand, ItemUtils.createFilledResult(stack, player, new ItemStack(Items.GLASS_BOTTLE)));
 					player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-					entity.playSound(this.releaseSound, 1F, level.getRandom().nextFloat() * 0.2F + 0.9F);
+					player.playSound(this.releaseSound, 0.75F, level.getRandom().nextFloat() * 0.2F + 0.9F);
 					if (stack.has(DataComponents.CUSTOM_NAME)) {
 						entity.setCustomName(stack.getHoverName());
 					}
