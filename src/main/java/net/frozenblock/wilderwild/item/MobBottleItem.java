ca770e66b47ef3882c81d7156f159c714de9f8a1
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-01-31 02:45:41 -0500

    Update MobBottleItem.java

--- src/main/java/net/frozenblock/wilderwild/item/MobBottleItem.java
+++ src/main/java/net/frozenblock/wilderwild/item/MobBottleItem.java
@@ -24,6 +24,7 @@ import net.frozenblock.wilderwild.registry.WWDataComponents;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
@@ -79,7 +80,7 @@ public class MobBottleItem extends Item {
 
 					if (!player.getAbilities().instabuild) player.setItemInHand(interactionHand, ItemUtils.createFilledResult(stack, player, new ItemStack(Items.GLASS_BOTTLE)));
 					player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-					player.playSound(this.releaseSound, 0.75F, level.getRandom().nextFloat() * 0.2F + 0.9F);
+					level.playSound(player, player.getX(), player.getEyeY(), player.getZ(), this.releaseSound, SoundSource.NEUTRAL, 0.75F, level.getRandom().nextFloat() * 0.2F + 0.9F);
 					if (stack.has(DataComponents.CUSTOM_NAME)) {
 						entity.setCustomName(stack.getHoverName());
 					}
