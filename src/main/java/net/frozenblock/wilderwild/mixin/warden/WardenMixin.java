55a474dcb2884a2da3415342f63b3beb718dd3c9
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-02-20 13:51:44 -0500

    Update WardenMixin.java

--- src/main/java/net/frozenblock/wilderwild/mixin/warden/WardenMixin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/warden/WardenMixin.java
@@ -60,10 +60,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(value = Warden.class, priority = 69420)
 public final class WardenMixin extends Monster implements WilderWarden {
