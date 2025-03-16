526745eda51b4055a51615f1ac55c3b9482c05e3
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-03-15 20:55:48 -0400

    Update WWCreativeInventorySorting.java

--- src/main/java/net/frozenblock/wilderwild/registry/WWCreativeInventorySorting.java
+++ src/main/java/net/frozenblock/wilderwild/registry/WWCreativeInventorySorting.java
@@ -233,7 +233,7 @@ public class WWCreativeInventorySorting {
 		addAfterInNaturalBlocks(Items.CORNFLOWER, WWBlocks.CARNATION);
 		addAfterInNaturalBlocks(WWBlocks.CARNATION, WWBlocks.MARIGOLD);
 		addAfterInNaturalBlocks(WWBlocks.MARIGOLD, WWBlocks.PASQUEFLOWER);
-		addAfterInNaturalBlocks(Items.WITHER_ROSE, WWBlocks.RED_HIBISCUS);
+		addBeforeInNaturalBlocks(Items.TORCH, WWBlocks.RED_HIBISCUS);
 		addAfterInNaturalBlocks(WWBlocks.RED_HIBISCUS, WWBlocks.YELLOW_HIBISCUS);
 		addAfterInNaturalBlocks(WWBlocks.YELLOW_HIBISCUS, WWBlocks.WHITE_HIBISCUS);
 		addAfterInNaturalBlocks(WWBlocks.WHITE_HIBISCUS, WWBlocks.PINK_HIBISCUS);
