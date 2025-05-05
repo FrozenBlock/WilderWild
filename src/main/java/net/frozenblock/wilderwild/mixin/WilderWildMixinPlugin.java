98029252065d7225896c9f71693ca48bcf03c789
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-05-04 23:42:06 -0400

    What we have been is what we are

--- src/main/java/net/frozenblock/wilderwild/mixin/WilderWildMixinPlugin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/WilderWildMixinPlugin.java
@@ -33,7 +33,6 @@ public final class WilderWildMixinPlugin implements IMixinConfigPlugin {
 	private boolean hasEmbeddiumMod;
 	private boolean disableNonSodium;
 	private boolean hasFallingLeavesMod;
-	private boolean hasParticleRainMod;
 
 	@Override
 	public void onLoad(String mixinPackage) {
@@ -41,7 +40,6 @@ public final class WilderWildMixinPlugin implements IMixinConfigPlugin {
 		this.hasEmbeddiumMod = FabricLoader.getInstance().isModLoaded("embeddium");
 		this.disableNonSodium = this.hasEmbeddiumMod || FrozenBools.HAS_SODIUM;
 		this.hasFallingLeavesMod = FabricLoader.getInstance().isModLoaded("fallingleaves");
-		this.hasParticleRainMod = FabricLoader.getInstance().isModLoaded("particlerain");
 	}
 
 	@Override
@@ -71,7 +69,6 @@ public final class WilderWildMixinPlugin implements IMixinConfigPlugin {
 		if (mixinClassName.contains("client.warden.")) return this.mixinsConfig.client_warden;
 		if (mixinClassName.contains("client.wind.")) {
 			if (mixinClassName.contains("fallingleaves") && !this.hasFallingLeavesMod) return false;
-			if (mixinClassName.contains("particlerain") && !this.hasParticleRainMod) return false;
 			return this.mixinsConfig.client_wind;
 		}
 
