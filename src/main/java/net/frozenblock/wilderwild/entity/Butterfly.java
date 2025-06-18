bfe58c35c7ad7203cd1b592635ae9e670185e504
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-06-17 20:07:09 -0400

    Update Butterfly.java

--- src/main/java/net/frozenblock/wilderwild/entity/Butterfly.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Butterfly.java
@@ -74,7 +74,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.ServerLevelAccessor;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathType;
 import net.minecraft.world.phys.Vec3;
