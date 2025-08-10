19468c4f410e97dfccbbe2c60d33bded999b140f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-08-10 01:24:13 -0400

    bruh

--- src/main/java/net/frozenblock/wilderwild/client/renderer/entity/layers/StellaWardenLayer.java
+++ src/main/java/net/frozenblock/wilderwild/client/renderer/entity/layers/StellaWardenLayer.java
@@ -18,18 +18,12 @@
 package net.frozenblock.wilderwild.client.renderer.entity.layers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import java.util.List;
 import net.frozenblock.wilderwild.client.animation.definitions.impl.WilderWarden;
 import net.minecraft.client.model.WardenModel;
-import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.WardenEmissiveLayer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FastColor;
 import net.minecraft.world.entity.monster.warden.Warden;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,29 +34,7 @@ public class StellaWardenLayer<T extends Warden, M extends WardenModel<T>> exten
 
 	@Override
 	public void render(@NotNull PoseStack poseStack, @NotNull MultiBufferSource vertexConsumerProvider, int i, @NotNull T wardenEntity, float f, float g, float partialTick, float j, float k, float l) {
-		if (!wardenEntity.isInvisible() && ((WilderWarden) wardenEntity).wilderWild$isStella()) {
-			this.onlyDrawSelectedParts();
-			VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderType.entityTranslucentEmissive(this.texture));
-			float alpha = this.alphaFunction.apply(wardenEntity, partialTick, j);
-			this.getParentModel()
-				.renderToBuffer(
-					poseStack,
-					vertexConsumer,
-					i,
-					LivingEntityRenderer.getOverlayCoords(wardenEntity, 0F),
-					FastColor.ARGB32.colorFromFloat(alpha, 1F, 1F, 1F)
-				);
-			this.resetDrawForAllParts();
-		}
-	}
-
-	private void onlyDrawSelectedParts() {
-		List<ModelPart> list = this.drawSelector.getPartsToDraw(this.getParentModel());
-		this.getParentModel().root().getAllParts().forEach(part -> part.skipDraw = true);
-		list.forEach(part -> part.skipDraw = false);
-	}
-
-	private void resetDrawForAllParts() {
-		this.getParentModel().root().getAllParts().forEach(part -> part.skipDraw = false);
+		if (!((WilderWarden) wardenEntity).wilderWild$isStella()) return;
+		super.render(poseStack, vertexConsumerProvider, i, wardenEntity, f, g, partialTick, j, k, l);
 	}
 }
