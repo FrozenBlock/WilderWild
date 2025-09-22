efc787954d4118d137c0ab93d4876090fac7677f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-09-22 19:06:38 -0400

    Update WWClientWindManager.java

--- src/main/java/net/frozenblock/wilderwild/wind/WWClientWindManager.java
+++ src/main/java/net/frozenblock/wilderwild/wind/WWClientWindManager.java
@@ -109,7 +109,7 @@ public final class WWClientWindManager implements ClientWindManagerExtension {
 
 		level.addParticle(
 			new WindParticleOptions(
-				(int) (10D + (horizontalWind * 30D)),
+				(int) (15D + (horizontalWind * 40D)),
 				wind.x * 0.01D, wind.y * 0.0015D, wind.z * 0.01D,
 				wind.length() <= 0.25D ? WindParticleOptions.ParticleLength.SMALL : WindParticleOptions.ParticleLength.MEDIUM
 			),
