208e16a49e46a79104d0deb123ee64f4756c069c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-10-13 23:15:30 -0400

    underwater crab movement fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 Please clear changelog after each release.
 Put the changelog BELOW the dashes. ANYTHING ABOVE IS IGNORED.
 -----------------
+- Crabs no longer freeze in place and jitter when attempting to climb blocks such as Mud.
+- Crabs no longer freeze in place and jitter when attempting to climb blocks underwater.
+- Crabs no longer attempt to "cling" to blocks while descending underwater.
 - Fixed Hollowed Crimson Stems and Hollowed Warped Stems incorrectly being in the `wilderwild:hollow_logs_that_burn` item tag. ([#513](https://github.com/FrozenBlock/WilderWild/issues/513))
 - Fixed a crash on 1.21.5+ when using Bone Meal in a Jungle biome. ([#510](https://github.com/FrozenBlock/WilderWild/issues/510))
--- src/main/java/net/frozenblock/wilderwild/entity/Crab.java
+++ src/main/java/net/frozenblock/wilderwild/entity/Crab.java
@@ -305,8 +305,11 @@ public class Crab extends Animal implements VibrationSystem, Bucketable, Variant
 		super.tick();
 		if (!isClient) {
 			this.cancelMovementToDescend = false;
-			if (this.horizontalCollision) {
-				this.setMoveState(this.getDeltaPos().y() >= 0 ? MoveState.CLIMBING : MoveState.DESCENDING);
+
+			final boolean inWater = this.isInWater();
+			final boolean isMovingUp = this.getDeltaPos().y() >= 0;
+			if (this.horizontalCollision && !(inWater && !isMovingUp)) {
+				this.setMoveState(isMovingUp ? MoveState.CLIMBING : MoveState.DESCENDING);
 				if (this.isCrabDescending() && this.level().noBlockCollision(this, this.makeBoundingBox().expandTowards(0D, -this.getEmptyAreaSearchDistance(), 0D))) {
 					this.cancelMovementToDescend = this.latchOntoWall(LATCH_TO_WALL_FORCE, false);
 				}
@@ -336,7 +339,7 @@ public class Crab extends Animal implements VibrationSystem, Bucketable, Variant
 			} else {
 				this.setMoveState(MoveState.WALKING);
 				this.setTargetClimbAnimX(0F);
-				if (!this.onGround() && !this.isInWater()) {
+				if (!this.onGround() && !inWater) {
 					if (this.level().noBlockCollision(this, this.makeBoundingBox().expandTowards(0D, -this.getEmptyAreaSearchDistance(), 0D))) {
 						this.cancelMovementToDescend = this.latchOntoWall(LATCH_TO_WALL_FORCE, false);
 					}
@@ -399,15 +402,17 @@ public class Crab extends Animal implements VibrationSystem, Bucketable, Variant
 	public Vec3 findNearestWall() {
 		BlockPos crabPos = this.blockPosition();
 		if (this.mainSupportingBlockPos.isPresent() && this.getOnPos().equals(crabPos)) crabPos = crabPos.above();
+
+		final boolean inWater = this.isInWater();
 		ArrayList<Vec3> vecs = new ArrayList<>();
 		for (BlockPos pos : BlockPos.betweenClosed(crabPos.offset(-1, 0, -1), crabPos.offset(1, 0, 1))) {
-			BlockState state = this.level().getBlockState(pos);
-			VoxelShape collisionShape = state.getCollisionShape(this.level(), pos, CollisionContext.of(this));
-			if (isWallPosSlowable(pos, state, collisionShape)) {
-				Optional<Vec3> optionalVec3 = FrozenShapes.closestPointTo(pos, collisionShape, this.position());
+			final BlockState state = this.level().getBlockState(pos);
+			final VoxelShape collisionShape = state.getCollisionShape(this.level(), pos, CollisionContext.of(this));
+			if (this.isWallPosSlowable(pos, state, collisionShape)) {
+				final Optional<Vec3> optionalVec3 = FrozenShapes.closestPointTo(pos, collisionShape, this.position());
 				if (optionalVec3.isPresent()) {
 					vecs.add(optionalVec3.get());
-				} else if (state.getFluidState().is(FluidTags.WATER)) {
+				} else if (!inWater && state.getFluidState().is(FluidTags.WATER)) {
 					vecs.add(pos.getCenter());
 				}
 			}
@@ -431,9 +436,7 @@ public class Crab extends Animal implements VibrationSystem, Bucketable, Variant
 	}
 
 	public boolean isWallPosSlowable(@NotNull BlockPos pos, @NotNull BlockState state, @NotNull VoxelShape collisionShape) {
-		if (state.isAir() || state.getFluidState().is(FluidTags.LAVA)) {
-			return false;
-		}
+		if (state.isAir() || state.getFluidState().is(FluidTags.LAVA)) return false;
 		return (!collisionShape.isEmpty() && pos.getY() + collisionShape.min(Direction.Axis.Y) <= this.getEyeY()) || (state.getFluidState().is(FluidTags.WATER));
 	}
 
