e6005ebde70ad1013339dcc715bae32e962ea224
Ethan Stokes <erstokes10@gmail.com>
2026-03-24 01:04:36 -0500

    bestestER rc3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ pluginManagement {
 rootProject.name = "Wilder Wild"
 
 localRepository("cloth-config", "me.shedaniel.cloth:cloth-config-fabric", true, true)
-localRepository("FrozenLib", "maven.modrinth:frozenlib", true, true)
+localRepository("FrozenLib", "maven.modrinth:frozenlib", true, false)
 localRepository("SimpleCopperPipesMC", "maven.modrinth:simple-copper-pipes", true, false)
 
 fun localRepository(repo: String, dependencySub: String, kotlin: Boolean, enabled: Boolean) {
