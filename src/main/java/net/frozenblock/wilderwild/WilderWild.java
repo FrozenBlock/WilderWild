6b72a9c87d84540effc3855087c3add77f44293b
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-04-07 19:21:32 -0400

    Update WilderWild.java

--- src/main/java/net/frozenblock/wilderwild/WilderWild.java
+++ src/main/java/net/frozenblock/wilderwild/WilderWild.java
@@ -118,10 +118,10 @@ public final class WilderWild extends FrozenModInitializer implements FrozenMobC
 
 	@Override
 	public void newCategories(ArrayList<FrozenMobCategory> context) {
-		context.add(FrozenMobCategoryEntrypoint.createCategory(id("firefly"), WWEntityConfig.FIREFLY_SPAWN_CAP.get(), true, false, 40));
-		context.add(FrozenMobCategoryEntrypoint.createCategory(id("butterfly"), WWEntityConfig.BUTTERFLY_SPAWN_CAP.get(), true, false, 80));
-		context.add(FrozenMobCategoryEntrypoint.createCategory(id("jellyfish"), WWEntityConfig.JELLYFISH_SPAWN_CAP.get(), true, false, 64));
-		context.add(FrozenMobCategoryEntrypoint.createCategory(id("crab"), WWEntityConfig.CRAB_SPAWN_CAP.get(), true, false, 64));
-		context.add(FrozenMobCategoryEntrypoint.createCategory(id("tumbleweed"), WWEntityConfig.TUMBLEWEED_SPAWN_CAP.get(), true, false, 64));
+		context.add(FrozenMobCategoryEntrypoint.createCategory(id("firefly"), "FF", WWEntityConfig.FIREFLY_SPAWN_CAP.get(), true, false, 40));
+		context.add(FrozenMobCategoryEntrypoint.createCategory(id("butterfly"), "BF", WWEntityConfig.BUTTERFLY_SPAWN_CAP.get(), true, false, 80));
+		context.add(FrozenMobCategoryEntrypoint.createCategory(id("jellyfish"), "JF", WWEntityConfig.JELLYFISH_SPAWN_CAP.get(), true, false, 64));
+		context.add(FrozenMobCategoryEntrypoint.createCategory(id("crab"), "CR", WWEntityConfig.CRAB_SPAWN_CAP.get(), true, false, 64));
+		context.add(FrozenMobCategoryEntrypoint.createCategory(id("tumbleweed"), "TW", WWEntityConfig.TUMBLEWEED_SPAWN_CAP.get(), true, false, 64));
 	}
 }
