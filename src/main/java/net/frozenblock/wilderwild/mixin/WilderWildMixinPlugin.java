8e9baac0713b2e3e11e58abf1ffce68bc22c6d96
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-05-22 13:38:09 -0400

    Update WilderWildMixinPlugin.java

--- src/main/java/net/frozenblock/wilderwild/mixin/WilderWildMixinPlugin.java
+++ src/main/java/net/frozenblock/wilderwild/mixin/WilderWildMixinPlugin.java
@@ -58,7 +58,6 @@ public final class WilderWildMixinPlugin implements IMixinConfigPlugin {
 		if (mixinClassName.contains("client.allay.")) return this.mixinsConfig.client_allay;
 		if (mixinClassName.contains("client.brush.")) return this.mixinsConfig.client_brush;
 		if (mixinClassName.contains("client.easter.")) return this.mixinsConfig.client_easter;
-		if (mixinClassName.contains("client.enderman.")) return this.mixinsConfig.client_enderman;
 		if (mixinClassName.contains("client.mesoglea.")) {
 			if ((mixinClassName.contains("LiquidBlockRenderer") || mixinClassName.contains("EntityRenderDispatcher")) && this.disableNonSodium) return false;
 			return this.mixinsConfig.client_mesoglea;
