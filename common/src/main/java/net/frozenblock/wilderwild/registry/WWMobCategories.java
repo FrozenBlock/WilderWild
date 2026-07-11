a5fe3db1e310b5e47b6872edde7b8c2e0de8e253
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-07-11 02:20:25 -0400

    Update WWMobCategories.java

--- common/src/main/java/net/frozenblock/wilderwild/registry/WWMobCategories.java
+++ common/src/main/java/net/frozenblock/wilderwild/registry/WWMobCategories.java
@@ -1,7 +1,6 @@
 package net.frozenblock.wilderwild.registry;
 
 import net.frozenblock.lib.entity.api.category.MobCategoryApiEntrypoint;
-import net.frozenblock.lib.entity.api.category.MutableMobCategory;
 import net.frozenblock.wilderwild.WWConstants;
 import net.frozenblock.wilderwild.config.WWEntityConfig;
 import net.minecraft.world.entity.MobCategory;
@@ -20,68 +19,58 @@ public final class WWMobCategories implements MobCategoryApiEntrypoint {
 	@Override
 	public void add(Context context) {
 		context.add(
-			MutableMobCategory.create(
-				WWConstants.MOD_ID,
-				"firefly",
-				"FF",
-				WWEntityConfig.FIREFLY_SPAWN_CAP.get(),
-				true,
-				false,
-				40,
-				category -> FIREFLY = category
-			)
+			WWConstants.MOD_ID,
+			"firefly",
+			"FF",
+			WWEntityConfig.FIREFLY_SPAWN_CAP.get(),
+			true,
+			false,
+			40,
+			category -> FIREFLY = category
 		);
 
 		context.add(
-			MutableMobCategory.create(
-				WWConstants.MOD_ID,
-				"butterfly",
-				"BF",
-				WWEntityConfig.BUTTERFLY_SPAWN_CAP.get(),
-				true,
-				false,
-				80,
-				category -> BUTTERFLY = category
-			)
+			WWConstants.MOD_ID,
+			"butterfly",
+			"BF",
+			WWEntityConfig.BUTTERFLY_SPAWN_CAP.get(),
+			true,
+			false,
+			80,
+			category -> BUTTERFLY = category
 		);
 
 		context.add(
-			MutableMobCategory.create(
-				WWConstants.MOD_ID,
-				"jellyfish",
-				"JF",
-				WWEntityConfig.JELLYFISH_SPAWN_CAP.get(),
-				true,
-				false,
-				64,
-				category -> JELLYFISH = category
-			)
+			WWConstants.MOD_ID,
+			"jellyfish",
+			"JF",
+			WWEntityConfig.JELLYFISH_SPAWN_CAP.get(),
+			true,
+			false,
+			64,
+			category -> JELLYFISH = category
 		);
 
 		context.add(
-			MutableMobCategory.create(
-				WWConstants.MOD_ID,
-				"crab",
-				"CR",
-				WWEntityConfig.CRAB_SPAWN_CAP.get(),
-				true,
-				false,
-				64,
-				category -> CRAB = category
-			)
+			WWConstants.MOD_ID,
+			"crab",
+			"CR",
+			WWEntityConfig.CRAB_SPAWN_CAP.get(),
+			true,
+			false,
+			64,
+			category -> CRAB = category
 		);
 
 		context.add(
-			MutableMobCategory.create(
-				WWConstants.MOD_ID,
-				"tumbleweed",
-				"TW",
-				WWEntityConfig.TUMBLEWEED_SPAWN_CAP.get(),
-				true,
-				false,
-				64,
-				category -> TUMBLEWEED = category
-			)
+			WWConstants.MOD_ID,
+			"tumbleweed",
+			"TW",
+			WWEntityConfig.TUMBLEWEED_SPAWN_CAP.get(),
+			true,
+			false,
+			64,
+			category -> TUMBLEWEED = category
 		);
 	}
 }
