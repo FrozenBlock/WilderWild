be074a4bf0859a66831cbc11ae1ab30bae45dd1f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2026-08-16 11:40:29 -0400

    DeferredSimpleParticleType

--- common/src/main/java/net/frozenblock/wilderwild/registry/WWParticleTypes.java
+++ common/src/main/java/net/frozenblock/wilderwild/registry/WWParticleTypes.java
@@ -24,6 +24,7 @@ import net.frozenblock.lib.particle.api.VibrationParticleVisibilityApi;
 import net.frozenblock.lib.platform.api.registry.DeferredHolder;
 import net.frozenblock.lib.platform.api.registry.DeferredParticleType;
 import net.frozenblock.lib.platform.api.registry.DeferredRegister;
+import net.frozenblock.lib.platform.api.registry.DeferredSimpleParticleType;
 import net.frozenblock.lib.platform.api.registry.ParticleTypeHelper;
 import net.frozenblock.wilderwild.WWConstants;
 import net.frozenblock.wilderwild.block.entity.IcicleBlockEntity;
@@ -45,96 +46,96 @@ import net.minecraft.resources.Identifier;
 public final class WWParticleTypes {
 	private static final DeferredRegister.ParticleTypes REGISTER = DeferredRegister.createParticleTypes(WWConstants.MOD_ID);
 
-	public static final DeferredParticleType<SimpleParticleType> POLLEN = register("pollen");
+	public static final DeferredSimpleParticleType POLLEN = register("pollen");
 	public static final DeferredParticleType<SeedParticleOptions> SEED = register("seed",
 		false,
 		SeedParticleOptions.CODEC,
 		SeedParticleOptions.STREAM_CODEC
 	);
-	public static final DeferredParticleType<SimpleParticleType> PALE_FOG = register("pale_fog");
-	public static final DeferredParticleType<SimpleParticleType> PALE_FOG_SMALL = register("pale_fog_small");
-	public static final DeferredParticleType<SimpleParticleType> PALE_SPORE = register("pale_spore");
+	public static final DeferredSimpleParticleType PALE_FOG = register("pale_fog");
+	public static final DeferredSimpleParticleType PALE_FOG_SMALL = register("pale_fog_small");
+	public static final DeferredSimpleParticleType PALE_SPORE = register("pale_spore");
 	public static final DeferredParticleType<FloatingSculkBubbleParticleOptions> FLOATING_SCULK_BUBBLE = register("floating_sculk_bubble",
 		false,
 		FloatingSculkBubbleParticleOptions.CODEC,
 		FloatingSculkBubbleParticleOptions.STREAM_CODEC
 	);
-	public static final DeferredParticleType<SimpleParticleType> TERMITE = register("termite");
-	public static final DeferredParticleType<SimpleParticleType> COCONUT_SPLASH = register("coconut_splash");
-	public static final DeferredParticleType<SimpleParticleType> SCORCHING_FLAME = register("scorching_flame");
-	public static final DeferredParticleType<SimpleParticleType> UNDERWATER_ASH = register("underwater_ash");
-	public static final DeferredParticleType<SimpleParticleType> PLANKTON = register("plankton");
-	public static final DeferredParticleType<SimpleParticleType> GLOWING_PLANKTON = register("glowing_plankton");
+	public static final DeferredSimpleParticleType TERMITE = register("termite");
+	public static final DeferredSimpleParticleType COCONUT_SPLASH = register("coconut_splash");
+	public static final DeferredSimpleParticleType SCORCHING_FLAME = register("scorching_flame");
+	public static final DeferredSimpleParticleType UNDERWATER_ASH = register("underwater_ash");
+	public static final DeferredSimpleParticleType PLANKTON = register("plankton");
+	public static final DeferredSimpleParticleType GLOWING_PLANKTON = register("glowing_plankton");
 	public static final DeferredParticleType<LeafClusterSeedParticleOptions> LEAF_CLUSTER_SPAWNER = register("leaf_cluster",
 		false,
 		LeafClusterSeedParticleOptions.CODEC,
 		LeafClusterSeedParticleOptions.STREAM_CODEC
 	);
-	public static final DeferredParticleType<SimpleParticleType> CHEST_BUBBLE_SPAWNER = register("chest_bubbles");
-	public static final DeferredParticleType<SimpleParticleType> SHRIEKER_BUBBLE_SPAWNER = register("shrieker_bubbles");
+	public static final DeferredSimpleParticleType CHEST_BUBBLE_SPAWNER = register("chest_bubbles");
+	public static final DeferredSimpleParticleType SHRIEKER_BUBBLE_SPAWNER = register("shrieker_bubbles");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_PEARLESCENT_BLUE = register("hanging_mesoglea_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_PEARLESCENT_BLUE = register("falling_mesoglea_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_PEARLESCENT_BLUE = register("landing_mesoglea_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_PEARLESCENT_BLUE = register("mesoglea_bubble_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_PEARLESCENT_BLUE = register("bubble_column_up_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_PEARLESCENT_BLUE = register("current_down_mesoglea_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_PEARLESCENT_BLUE = register("mesoglea_bubble_pop_pearlescent_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_PEARLESCENT_BLUE = register("mesoglea_splash_pearlescent_blue");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_PEARLESCENT_BLUE = register("hanging_mesoglea_pearlescent_blue");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_PEARLESCENT_BLUE = register("falling_mesoglea_pearlescent_blue");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_PEARLESCENT_BLUE = register("landing_mesoglea_pearlescent_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_PEARLESCENT_BLUE = register("mesoglea_bubble_pearlescent_blue");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_PEARLESCENT_BLUE = register("bubble_column_up_pearlescent_blue");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_PEARLESCENT_BLUE = register("current_down_mesoglea_pearlescent_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_PEARLESCENT_BLUE = register("mesoglea_bubble_pop_pearlescent_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_PEARLESCENT_BLUE = register("mesoglea_splash_pearlescent_blue");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_PEARLESCENT_PURPLE = register("hanging_mesoglea_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_PEARLESCENT_PURPLE = register("falling_mesoglea_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_PEARLESCENT_PURPLE = register("landing_mesoglea_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_PEARLESCENT_PURPLE = register("mesoglea_bubble_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_PEARLESCENT_PURPLE = register("bubble_column_up_mesoglea_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_PEARLESCENT_PURPLE = register("current_down_mesoglea_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_PEARLESCENT_PURPLE = register("mesoglea_bubble_pop_pearlescent_purple");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_PEARLESCENT_PURPLE = register("mesoglea_splash_pearlescent_purple");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_PEARLESCENT_PURPLE = register("hanging_mesoglea_pearlescent_purple");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_PEARLESCENT_PURPLE = register("falling_mesoglea_pearlescent_purple");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_PEARLESCENT_PURPLE = register("landing_mesoglea_pearlescent_purple");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_PEARLESCENT_PURPLE = register("mesoglea_bubble_pearlescent_purple");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_PEARLESCENT_PURPLE = register("bubble_column_up_mesoglea_pearlescent_purple");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_PEARLESCENT_PURPLE = register("current_down_mesoglea_pearlescent_purple");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_PEARLESCENT_PURPLE = register("mesoglea_bubble_pop_pearlescent_purple");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_PEARLESCENT_PURPLE = register("mesoglea_splash_pearlescent_purple");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_PINK = register("hanging_mesoglea_pink");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_PINK = register("falling_mesoglea_pink");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_PINK = register("landing_mesoglea_pink");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_PINK = register("mesoglea_bubble_pink");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_PINK = register("bubble_column_up_mesoglea_pink");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_PINK = register("current_down_mesoglea_pink");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_PINK = register("mesoglea_bubble_pop_pink");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_PINK = register("mesoglea_splash_pink");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_PINK = register("hanging_mesoglea_pink");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_PINK = register("falling_mesoglea_pink");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_PINK = register("landing_mesoglea_pink");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_PINK = register("mesoglea_bubble_pink");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_PINK = register("bubble_column_up_mesoglea_pink");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_PINK = register("current_down_mesoglea_pink");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_PINK = register("mesoglea_bubble_pop_pink");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_PINK = register("mesoglea_splash_pink");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_RED = register("hanging_mesoglea_red");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_RED = register("falling_mesoglea_red");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_RED = register("landing_mesoglea_red");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_RED = register("mesoglea_bubble_red");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_RED = register("bubble_column_up_mesoglea_red");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_RED = register("current_down_mesoglea_red");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_RED = register("mesoglea_bubble_pop_red");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_RED = register("mesoglea_splash_red");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_RED = register("hanging_mesoglea_red");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_RED = register("falling_mesoglea_red");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_RED = register("landing_mesoglea_red");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_RED = register("mesoglea_bubble_red");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_RED = register("bubble_column_up_mesoglea_red");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_RED = register("current_down_mesoglea_red");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_RED = register("mesoglea_bubble_pop_red");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_RED = register("mesoglea_splash_red");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_YELLOW = register("hanging_mesoglea_yellow");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_YELLOW = register("falling_mesoglea_yellow");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_YELLOW = register("landing_mesoglea_yellow");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_YELLOW = register("mesoglea_bubble_yellow");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_YELLOW = register("bubble_column_up_mesoglea_yellow");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_YELLOW = register("current_down_mesoglea_yellow");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_YELLOW = register("mesoglea_bubble_pop_yellow");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_YELLOW = register("mesoglea_splash_yellow");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_YELLOW = register("hanging_mesoglea_yellow");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_YELLOW = register("falling_mesoglea_yellow");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_YELLOW = register("landing_mesoglea_yellow");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_YELLOW = register("mesoglea_bubble_yellow");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_YELLOW = register("bubble_column_up_mesoglea_yellow");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_YELLOW = register("current_down_mesoglea_yellow");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_YELLOW = register("mesoglea_bubble_pop_yellow");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_YELLOW = register("mesoglea_splash_yellow");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_LIME = register("hanging_mesoglea_lime");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_LIME = register("falling_mesoglea_lime");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_LIME = register("landing_mesoglea_lime");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_LIME = register("mesoglea_bubble_lime");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_LIME = register("bubble_column_up_mesoglea_lime");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_LIME = register("current_down_mesoglea_lime");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_LIME = register("mesoglea_bubble_pop_lime");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_LIME = register("mesoglea_splash_lime");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_LIME = register("hanging_mesoglea_lime");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_LIME = register("falling_mesoglea_lime");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_LIME = register("landing_mesoglea_lime");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_LIME = register("mesoglea_bubble_lime");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_LIME = register("bubble_column_up_mesoglea_lime");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_LIME = register("current_down_mesoglea_lime");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_LIME = register("mesoglea_bubble_pop_lime");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_LIME = register("mesoglea_splash_lime");
 
-	public static final DeferredParticleType<SimpleParticleType> HANGING_MESOGLEA_BLUE = register("hanging_mesoglea_blue");
-	public static final DeferredParticleType<SimpleParticleType> FALLING_MESOGLEA_BLUE = register("falling_mesoglea_blue");
-	public static final DeferredParticleType<SimpleParticleType> LANDING_MESOGLEA_BLUE = register("landing_mesoglea_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_BLUE = register("mesoglea_bubble_blue");
-	public static final DeferredParticleType<SimpleParticleType> BUBBLE_COLUMN_UP_MESOGLEA_BLUE = register("bubble_column_up_mesoglea_blue");
-	public static final DeferredParticleType<SimpleParticleType> CURRENT_DOWN_MESOGLEA_BLUE = register("current_down_mesoglea_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_BUBBLE_POP_BLUE = register("mesoglea_bubble_pop_blue");
-	public static final DeferredParticleType<SimpleParticleType> MESOGLEA_SPLASH_BLUE = register("mesoglea_splash_blue");
+	public static final DeferredSimpleParticleType HANGING_MESOGLEA_BLUE = register("hanging_mesoglea_blue");
+	public static final DeferredSimpleParticleType FALLING_MESOGLEA_BLUE = register("falling_mesoglea_blue");
+	public static final DeferredSimpleParticleType LANDING_MESOGLEA_BLUE = register("landing_mesoglea_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_BLUE = register("mesoglea_bubble_blue");
+	public static final DeferredSimpleParticleType BUBBLE_COLUMN_UP_MESOGLEA_BLUE = register("bubble_column_up_mesoglea_blue");
+	public static final DeferredSimpleParticleType CURRENT_DOWN_MESOGLEA_BLUE = register("current_down_mesoglea_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_BUBBLE_POP_BLUE = register("mesoglea_bubble_pop_blue");
+	public static final DeferredSimpleParticleType MESOGLEA_SPLASH_BLUE = register("mesoglea_splash_blue");
 
 	public static final DeferredParticleType<WindClusterSeedParticleOptions> WIND_CLUSTER = register("wind_cluster",
 		false,
@@ -193,11 +194,11 @@ public final class WWParticleTypes {
 		);
 	}
 
-	private static DeferredParticleType<SimpleParticleType> register(String name, boolean overrideLimiter) {
+	private static DeferredSimpleParticleType register(String name, boolean overrideLimiter) {
 		return REGISTER.register(name, overrideLimiter);
 	}
 
-	private static DeferredParticleType<SimpleParticleType> register(String name) {
+	private static DeferredSimpleParticleType register(String name) {
 		return register(name, false);
 	}
 
